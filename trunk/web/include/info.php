a545dcd568b527248f320cade41889dadf735c48
Angelox22@gmail.com <Angelox22@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2011-11-22 12:36:14 +0000

    Some Web stuff from AXClassic.com

---
+++ trunk/web/include/info.php
@@ -0,0 +1,18 @@
+<?php
+/*
+	   TerraNet Website
+		 By DarKiD
+	 Info Variable System
+		    V1.1
+*/
+
+/* About Your Server */
+	$about = "Our server is bla bla bla bla bla bla bla bla bla bla bla bla ";
+
+/* About Antrix */
+	$about_antrix = "Antrix is a server package for World of Warcraft, and written completely in C++.<br />
+	The server has been tested with over 1000 clients without any flaws or missing a beat,And Most game features working!.";
+	
+/* About TerraNet */
+	$about_database = "TerraNet is a Database for Antrix server package, TerraNet Team provide the best DB for Antrix!<br/> Almost all the Quests, Items, Instances, Mobs, Trainers, Vendors and everything works like on Real Blizzard server!";
+?>
---
+++ trunk/web/include/news.php
@@ -0,0 +1,13 @@
+<?php
+
+/* First News Block */
+	$posted_by = "leslamarch";						// Enter writer name
+	$post_date = "4.19.09";						// Enter post date
+	$post_title = "axclassic Game Launcher";						// Enter post title
+	$post_text = "Please Download the AXClassic Game Launcher (from the top Menu) it will do everything for you that you need to play. Just start the install and direct it to your EQ directory.";	// Enter post Text
+/* Second News Block */
+	$posted_by2 = "leslamarch";						// Enter writer name
+	$post_date2 = "04.18.09";						// Enter post date
+	$post_title2 = "login.axclassic.com:5999";					// Enter post title
+	$post_text2 = "Welcome to the AX Classic Account registration page Make sure to set your EQHost.txt file to login.axclassic.com:5999";	// Enter post text
+?>
