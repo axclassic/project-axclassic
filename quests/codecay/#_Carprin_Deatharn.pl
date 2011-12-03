31ad9f2468b41e431c0596bf8e59244aebc3d6de
threeflies@gmail.com <threeflies@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2011-12-03 16:06:04 +0000

    Bertoxxulous flagging quest updates

--- quests/codecay/#_Carprin_Deatharn.pl
+++ quests/codecay/#_Carprin_Deatharn.pl
@@ -3,9 +3,9 @@ sub EVENT_AGGRO
 
 if($spawn == undef) {
 quest::settimer(1,1800);
-quest::spawn2(200036,0,0,270,-47,-59,0); 
-quest::spawn2(200037,0,0,319,-25,,-59,0); 
-quest::spawn2(200038,0,0,319,-149,-59,0); 
+quest::spawn2(200064,0,0,270,-47,-59,0); 
+quest::spawn2(200065,0,0,319,-25,,-59,0); 
+quest::spawn2(200066,0,0,319,-149,-59,0); 
 $spawn=1
 }
 
@@ -13,13 +13,13 @@ $spawn=1
 
 sub EVENT_TIMER
 {
-quest::signal(200036,1); #depop mob 1
-quest::signal(200037,1); #depop mob 2
-quest::signal(200038,1); #depop mob 3
+quest::signal(200064,1); #depop mob 1
+quest::signal(200065,1); #depop mob 2
+quest::signal(200066,1); #depop mob 3
 }
 
 sub EVENT_DEATH
 {
 $spawn=undef;
-quest::spawn2(200035,0,0,408,150,-61.5,135);
+quest::spawn2(200058,0,0,408,150,-61.5,135);
 }
