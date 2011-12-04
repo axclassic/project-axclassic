618f6b7f0c729b8cc417d8079e52b424382eeed1
angelox22@gmail.com <Angelox22@gmail.com>
2011-12-04 13:35:25 +0000

    Some typo fix

--- quests/potranquility/Bor_Wharhammer.pl
+++ quests/potranquility/Bor_Wharhammer.pl
@@ -5,9 +5,9 @@ my $cash;
 
 sub EVENT_SAY {
    if($text=~/hail/i) {
-            quest::say("Greetin's t'ye $name!  Isn't the area 'round 'ere so nice an' quiet?  Such a departure from me old days; slaving o'er the forge t'create masterpieces!  T'be 'onest. at times I do miss the old forge. but after I created me finest breastplate. it seemed as if nothing else I made could ever live up t'it.  So I left me home to'wander the land an' see what I could learn o'the world.  I 'ave learned quite a bit from the elders 'ere an' the skilled craftspeople in New Tanaan.  I 'ave even devised a type o'emblem that will impart the magic o'tranquility into the user t'create planar armors from pieces o'energy found in the planes.");
+            quest::say("Greetin's t'ye $name!  Isn't the area 'round 'ere so nice an' quiet?  Such a departure from me old days; slaving o'er the forge t'create masterpieces!  T'be 'onest. at times I do miss the old forge. but after I created me finest breastplate. it seemed as if nothing else I made could ever live up t'it.  So I left me home to'wander the land an' see what I could learn o'the world.  I 'ave learned quite a bit from the elders 'ere an' the skilled craftspeople in New Tanaan.  I 'ave even devised a type o'[emblem] that will impart the magic o'tranquility into the user t'create planar armors from pieces o'energy found in the planes.");
    }
-   if($text=~/emblem?/i) {
+   if($text=~/emblem/i) {
       if ($ulevel <=54) {
          quest::say("Ye look mighty inexperienced t'be in this area. $name. Come an' seek me out when ye 'ave more knowledge o'the planes!");
       }
