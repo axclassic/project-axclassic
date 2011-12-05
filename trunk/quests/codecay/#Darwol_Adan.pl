0ec31c85e706876ef7aa53aead9586f44f6cb5f4
threeflies@gmail.com <threeflies@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2011-12-05 19:48:39 +0000

    Npc id corrections

--- trunk/quests/codecay/#Darwol_Adan.pl
+++ trunk/quests/codecay/#Darwol_Adan.pl
@@ -1,16 +1,16 @@
 sub EVENT_SPAWN {
 
-quest::spawn2(200042,0,0,($x + 10),$y,$z,$h);
-quest::spawn2(200042,0,0,($x + 10),($y + 10),$z,$h);
-quest::spawn2(200042,0,0,($x - 10),$y,$z,$h);
-quest::spawn2(200042,0,0,($x - 10),($y - 10),$z,$h);
-quest::spawn2(200043,0,0,$x,($y - 10),$z,$h);
+quest::spawn2(200067,0,0,($x + 10),$y,$z,$h);
+quest::spawn2(200067,0,0,($x + 10),($y + 10),$z,$h);
+quest::spawn2(200067,0,0,($x - 10),$y,$z,$h);
+quest::spawn2(200067,0,0,($x - 10),($y - 10),$z,$h);
+quest::spawn2(200068,0,0,$x,($y - 10),$z,$h);
 
 }
 
 sub EVENT_DEATH {
 
-	quest::spawn2(200047,0,0,200,1,-274,0); #west room
+	quest::spawn2(200072,0,0,200,1,-274,0); #west room
 
 }
 
