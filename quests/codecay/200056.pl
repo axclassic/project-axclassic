ab7b253c18b79f8b5510236da11d0f6f1c268cb0
threeflies@gmail.com <threeflies@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2011-12-05 19:15:08 +0000

    Summoner of bertt npc id changes

--- quests/codecay/200056.pl
+++ quests/codecay/200056.pl
@@ -20,27 +20,27 @@ sub EVENT_TIMER {
 
 if($timer == 20) {
 
-	quest::signal(200042);
-	quest::signal(200043);
-	quest::signal(200044);
-	quest::signal(200045);
-	quest::signal(200046);
-	quest::signal(200047);
-	quest::signal(200048);
-	quest::signal(200049);
-	quest::signal(200050);
-	quest::signal(200051);
-	quest::signal(200052);
-	quest::signal(200053);
-	quest::signal(200054);
-	quest::signal(200055);
-	quest::signal(200022);
+	quest::signal(200067);
+	quest::signal(200068);
+	quest::signal(200069);
+	quest::signal(200070);
+	quest::signal(200071);
+	quest::signal(200072);
+	quest::signal(200073);
+	quest::signal(200074);
+	quest::signal(200075);
+	quest::signal(200076);
+	quest::signal(200077);
+	quest::signal(200078);
+	quest::signal(200079);
+	quest::signal(200080);
+	quest::signal(200081);
 	quest::signal(200056);
 
 	}
 if($timer == 21) {
 
-	quest::spawn2(200044,0,0,0,280,-244,0);
+	quest::spawn2(200069,0,0,0,280,-244,0);
         quest::stoptimer(21);
 	}
 }
