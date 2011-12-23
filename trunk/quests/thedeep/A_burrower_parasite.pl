83f708d5784bc98d78d8801380c36caa900d7671
threeflies@gmail.com <threeflies@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2011-12-23 21:08:29 +0000

    Added emote

--- trunk/quests/thedeep/A_burrower_parasite.pl
+++ trunk/quests/thedeep/A_burrower_parasite.pl
@@ -1,8 +1,9 @@
 # signal burrower beast whether success or failure
-#
+#Threeflies added emote.
 
 sub EVENT_SPAWN {
   quest::settimer("countdown",1800);
+quest::emote(quest::emote("The giant burrower begins to tunnel into the ground below it. As it digs, its flesh splits revealing an enormous parasite.");
 }
 
 sub EVENT_TIMER {
