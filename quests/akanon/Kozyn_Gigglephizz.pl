9b05780bfc5079bb117d032128a69fbdc1fb732a
threeflies@gmail.com <threeflies@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2011-12-30 08:11:39 +0000

    Altered and corrected , thank you Angelox

--- quests/akanon/Kozyn_Gigglephizz.pl
+++ quests/akanon/Kozyn_Gigglephizz.pl
@@ -4,7 +4,8 @@
 # NPC ID 55223			              #
 # -17.000000 -21.000000 1.880000              #			
 # Ax_Classic Quests		              #
-# Creator Threeflies		              #
+# Created by Threeflies			      #
+# 		                              #
 # Dec 2011				      #				
 #				              #
 #				              #
@@ -13,62 +14,52 @@
 			
 			
 sub EVENT_SAY {
-# If you want, save this as a template, remove all my explanations and re-upload. 
-# These methods call for two options (what the variable represents, and color), not three. 
-my $Tinmizer = quest::saylink("Tinmizer",0);
-my $Fabulous = quest::saylink("Fabulous compactor",0);
-## variable can't have dots ($T.B.I)
-my $TBI = quest::saylink("T.B.I.",0);
-## this  perl variable doesn't expect a number ($200)
-my $twohundred = quest::saylink("Tinmizer",0);
-my $clockwork = quest::saylink("clockwork dragon",0);
-my $big = quest::saylink("Big Bynn",0); #the two options must be there and no more than two ("Big Bynn", 0)
+
+my $Tinmizer = quest::saylink("who is Tinmizer", 0,"Tinmizer");  
+my $Fabulous = quest::saylink("what fabulous compactor", 0,"Tinmizer's Fabulous compactor");
+my $TBI = quest::saylink("what is the T.B.I", 0,"T.B.I.");
+my $tiz = quest::saylink("where is Tinmizer", 0,"Tinmizer");
+my $clockwork = quest::saylink("what clockwork dragon", 0,"clockwork dragon");
+my $Big = quest::saylink("Big Bynn");
 
 
 
-if($text=~/Hail/i) {
-quest::say("Kozyn Gigglephizz bows politely. Greetings, my friend. Have you come to turn in one of those malfunctioning containers called $Tinmizer's $Fabulous Compactor? I shall gladly exchange it for a nice gem."); 
+if($text=~/Hail/i){
+quest::emote(" bows politely. Greetings, my friend. Have you come to turn in one of those malfunctioning containers called $Fabulous ?,Created of course by that mad as a hatter wizard $Tinmizer. I shall gladly exchange it for a nice gem."); 
 }
-
-if($text=~/Tinmizer/i) {
-quest::say("Kozyn Gigglephizz chortles with glee. 'Ho, ho, ho!! Oh, my! Tinmizer was a bit of a goof when it came to 	tinkering. I am from the $T.B.I. and I was sent to clean up the mess he made. He was tinkering defective containers and selling them. We exiled him and destroyed all his containers. Now we hear the scrapman found some and was using them as	a reward for deeds. We have warned him."); 
+if($text=~/who is Tinmizer/i){
+quest::emote("chortles with glee. 'Ho, ho, ho!! Oh, my! Tinmizer was a bit of a goof when it came to tinkering. I am from the $TBI. and I was sent to clean up the mess he made. He was tinkering defective containers and selling them. We exiled him and destroyed all his containers. Now we hear the scrapman found some and was using them as	a reward for deeds. We have warned him."); 
+}	
+if($text=~/what is the T.B.I/i){
+quest::emote("lowers his voice and leans closer to your ear. 'Ssshhh. This is against regulations, but you look like you could keep a secret. T.B.I. stands for the Tinkerers Bureau of Investigation. We are a clandestine organization reporting directly to King Ak'Anon. We handle all tinkering glitches, if you know what I mean.");
 }
-	
-if($text=~/T.B.I/i) {
-quest::say("Kozyn Gigglephizz lowers his voice and leans closer to your ear. 'Ssshhh. This is against regulations, but you look like you could keep a secret. T.B.I. stands for the Tinkerers Bureau of Investigation. We are a clandestine organization reporting directly to King Ak'Anon. We handle all tinkering glitches, if you know what I mean.");
+if($text=~/What Fabulous Compactor/i){
+quest::say("The compactor is a defective piece of junk. Crafted by the clumsy hands of $tiz. My work shall be done as soon as I exchange all the compactors for precious gems. We take a loss here, you know!!?.");
 }
-
-if($text=~/Fabulous Compactor/i) {
-quest::say("Kozyn Gigglephizz looks sternly at you. 'The compactor is a defective piece of junk. Crafted by the clumsy hands of $200. My work shall be done as soon as I exchange all the compactors for precious gems. We take a loss here, you know!!?.");
+if($text=~/where is Tinmizer/i){
+quest::emote("shakes his head in sorrow. 'Alas, for tinkering defective merchandise, he was exiled from Ak'Anon. First, the $clockwork and now the Fabulous compactor. What could be next? No one has seen nor heard of him since. Probably ended up as a feast for the ogres.");
 }
-
-#Syntax errors all the way down - you forgot the (" after quest::say
-if($text=~/Tinmizer/i) {
-quest::say("/Kozyn Gigglephizz shakes his head in sorrow. 'Alas, for tinkering defective merchandise, he was exiled from Ak'Anon. First, the $clockwork and now the $Fabulous. What could be next? No one has seen nor heard of him since. Probably ended up as a feast for the ogres.");
+if($text=~/What clockwork dragon/i){
+quest::emote("shakes in fear. 'Oh!! I have seen it. A thousand feet tall and smells of burning oil. It was tinkered by Tinmizer. Yep, that's it all righty. Tinkered by the loon. He started out just making a clock, a GIANT ONE!! It was in the form of a wurm. He called it $Big, you know, after his familiar named Bynn. And then... then it escaped. Yeah.. That's it.");
 }
-
-if($text=~/clockwork/i) {
-quest::say("/Kozyn Gigglephizz shakes in fear. 'Oh!! I have seen it. A thousand feet tall and smells of burning oil. It was tinkered by $Tinmizer. Yep, that's it all righty. Tinkered by the loon. He started out just making a clock, a GIANT ONE!! It was in the form of a wurm. He called it $Big, you know, after his familiar named Bynn. And then... then it escaped. Yeah.. That's it.");
-}
-#quotes like " are part of reserved characters for perl, if you want to use them in a phrase you do like this \" (backslash and quote) this tells Perl to ignore the next character after the backslash. 
-if ($text=~/Big Bynn/i) {
-quest::say("/Kozyn Gigglephizz looks you in the eye. \"Some say he has been seen in the mouintains, astride him his creator..... Tinmizer... I'm not saying the story's  true, but I wouldn't like to bet my clockwork pixie on it.\"");
+if ($text=~/Big Bynn/i){
+quest::emote("looks you in the eye. \"Some say he has been seen in the mouintains, astride him his creator..... Tinmizer... I'm not saying the story's  true, but I wouldn't like to bet my clockwork pixie on it.");
   }
 }
 
 
-sub EVENT_ITEM {
+#sub EVENT_ITEM {
 
-if (plugin::check_handin(\%itemcount, 319504 => 1)) { #Tinmizer's Fabulous Compactor
-quest::say("Yes Yes Yes !! , Keep them coming young $race , the more I get the richer you become. Here , take this gem , now be off with you .");
-quest::summonitem(10027);
-quest::ding(); 
-quest::exp(3000);
-}
-else {
-plugin::return_items(\%itemcount);
- }
-}
+#if (plugin::check_handin(\%itemcount, 319504 => 1)) { #Tinmizer's Fabulous Compactor
+#quest::say("Yes Yes Yes !! , Keep them coming young $race , the more I get the richer you become. Here , take this gem , now be #off with you .");
+#quest::summonitem(10027);
+#quest::ding(); 
+#quest::exp(3000);
+#}
+#else {
+#plugin::return_items(\%itemcount);
+# }
+#}
 
 # EOF zone: Akanon ID: 55223 NPC: Kozyn_Gigglephizz
 
