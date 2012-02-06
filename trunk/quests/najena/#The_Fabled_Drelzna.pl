dd440ca12d9d756125cfbbe7646914b29943d851
angelox22@gmail.com <angelox22@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2012-02-06 13:10:43 +0000

    Fix for fabled depopper

--- trunk/quests/najena/#The_Fabled_Drelzna.pl
+++ trunk/quests/najena/#The_Fabled_Drelzna.pl
@@ -1,16 +1,39 @@
 ####Fabled Depoper###
 ### Romell 6-9-09
 
+sub EVENT_SPAWN{
+  my $x;
+  my $y;
+  my $z;
+  my $h;
+  $x = $npc->GetX();
+  $y = $npc->GetY();
+  $z = $npc->GetZ();
+  $h = $npc->GetHeading();
+    quest::set_proximity( $x-100,$x+100,$y-100,$y+100,$z-100,$z+100);
+} 
+
+sub EVENT_ENTER
+{
+	if($ulevel <= 49){
+	 my $x = $npc->GetX();
+    	 my $y = $npc->GetY();
+    	 my $z = $npc->GetZ();
+    	 my $h = $npc->GetHeading();
+	  quest::spawn2(44094,0,0,$x,$y,$z,$h);
+	  quest::depop;	
+	}
+}
+
 sub EVENT_AGGRO
 {
-	if($ulevel <= 39)
-{
-	my $x = $npc->GetX();
-    	my $y = $npc->GetY();
-    	my $z = $npc->GetZ();
-    	my $h = $npc->GetHeading();
-  	quest::spawn2(44094,0,0,$x,$y,$z,$h);
-	quest::shout("We shall meet again $name! When you are a worthy opponent!!");
-	quest::depop;
+	if($ulevel <= 49){
+	 my $x = $npc->GetX();
+    	 my $y = $npc->GetY();
+    	 my $z = $npc->GetZ();
+    	 my $h = $npc->GetHeading();
+  	  quest::spawn2(44094,0,0,$x,$y,$z,$h);
+	  #quest::shout("We shall meet again $name! When you are a worthy opponent!!");
+	  quest::depop;
 	}
 }
