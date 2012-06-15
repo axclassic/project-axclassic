3c6bbffb761c607135e77e8b202006c2a75b5eda
angelox22@gmail.com <Angelox22@gmail.com>
2012-06-15 13:20:37 +0000

    Temp Fix for 2011 database Lavastorm zoning (part 2)

---
+++ quests_ax_peq2011/lavastorm/27129.pl
@@ -0,0 +1,15 @@
+# Zone to safepoint event
+# Zone: Lavastorm
+# AngeloX
+
+sub EVENT_SPAWN
+{
+	$x = $npc->GetX();
+	$y = $npc->GetY();
+	quest::set_proximity($x - 20, $x + 20, $y - 20, $y + 20);
+}
+
+sub EVENT_ENTER
+{
+	quest::movepc(44,851,-9.4,3.1);
+}
