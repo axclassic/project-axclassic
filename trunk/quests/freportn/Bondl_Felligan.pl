4f856a1c1c07b171fd14dea17016d150b6728349
threeflies@gmail.com <threeflies@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2012-06-19 16:35:55 +0000

    Waypoints added , depop added. Fixed.

--- trunk/quests/freportn/Bondl_Felligan.pl
+++ trunk/quests/freportn/Bondl_Felligan.pl
@@ -1,28 +1,45 @@
+# Shaman Epic 1.0
+# Bondl Felligan
+# Freportn
+# Aramid September 2006
+# Modified by threeflies for Ax_Classic 2012
+
 sub EVENT_SAY {
- if($text=~/hail/i){ #Part of Shaman Epic 1.0
+ if($text=~/hail/i) {
   quest::emote("burps loudly in your face and says, 'Bah! Leave me be, fool! You have nothin' I want and I certainly have nothin' you want.");
   }
- elsif($text=~/you can buy booze/i){ #Part of Shaman Epic 1.0
-  quest::emote("suddenly becomes completely sober and says, 'Very well, shaman, please come with me.'");
-  quest::start(20);
- }
+ if(($text=~/booze/i) && ($move == 1)) {
+   quest::emote("suddenly becomes completely sober and says, 'Very well, shaman, please come with me.");
+   quest::spawn2(8117,0,0,64.5,96.8,31.3,6.5);
+   $move = 2;
+   quest::start(20);
+  }
 }
 
 sub EVENT_ITEM {
- if(plugin::check_handin(\%itemcount, 1665 => 1)) { #Part of Shaman Epic 1.0
-  quest::say("WOW, thanks! This must be worth a fortune! I could drink for a month after sellin' this to one of them fool merchants. I'm going to see how much I can get for it right now!");
-  quest::faction(342,100);
-  quest::exp(1000);
-  quest::spawn2(8117,0,0,62,66,32.1,254);
-  #quest::spawn2(8118,0,0,67,103,32.1,508);
-  quest::say("What!? You don't approve of me buyin' some drinks with this gem? Who the heck are you to offer me a gift and order me what to do with it? Is this some kinda conditional kindness? Well? Are you gonna let me buy some booze with this or not?");
- }
+ if (plugin::check_handin(\%itemcount, 1665 => 1)) {
+   quest::say("WOW, thanks! This must be worth a fortune! I could drink for a month after sellin' this to one of them fool merchants. I'm going to see how much I can get for it right now!");
+   quest::faction(342,10);
+    quest::ding(); quest::exp(10000);
+   quest::moveto(555.4,429.4,-20.7,161.8);
+   $move = 1;
+  }
+ else {
+   quest::emote("will not take this item.");
+   plugin::return_items(\%itemcount);
+   return;
+  }
 }
 
-sub EVENT_WAYPOINT_ARRIVE {
-if ($wp == 24) {
+sub EVENT_WAYPOINT {
+  if ($move == 1) {
+   quest::depop();
+   quest::spawn2(8003,0,0,555.4,429.4,-20.7,161.8);
+   quest::say("What!? You don't approve of me buyin' some drinks with this gem? Who the heck are you to offer me a gift and order me what to do with it? Is this some kinda conditional kindness? Well? Are you gonna let me buy some booze with this or not?");
+  }
+ 
+if ($wp == 23) {
 quest::depop();
-}
-}
-
-#Submitted by: Jim Mills
+  
+  
+   }
