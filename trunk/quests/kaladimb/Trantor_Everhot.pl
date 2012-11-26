8ffffcf3bf90c3f4540ebba0f60c714cd9b44096
Angelox22@gmail.com <Angelox22@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2012-11-26 00:49:21 +0000

    Updated Trantor Everhot for the Trueshot bow quest. Also fixed drops and quests for Trushot armor quests (last submission).

--- trunk/quests/kaladimb/Trantor_Everhot.pl
+++ trunk/quests/kaladimb/Trantor_Everhot.pl
@@ -12,17 +12,16 @@ if($text=~/tonics/i){
 
 sub EVENT_ITEM {
 	if($itemcount{12114} == 1) {
-		quest::say('Hey!! Only one?!!');
+		quest::say("Hey!! Only one?!!");
 	} elsif($itemcount{12114} == 2) {
-		quest::say("'Aaah!! I love Tumpy Tonics. They were created by Tumpy Irontoe. He was once a member of the famed Irontoe Brigade. Since they disbanded and left for parts unknown he decided to stay in Kaladim and open Irontoe's, the finest well in Faydwer.");
+		quest::say("Great!! I did not have the time to get down to Irontoe's today. Here. Like I said. A spool of dwarven wire. Now go away. I ain't sharing these tonics.");
 		quest::summonitem(12111);
-		 quest::ding(); quest::exp(1000);
-		#missing factions:
-#	Your faction standing with Storm Guard got better. 
-#Your faction standing with Kazon Stormhammer got better. 
-#Your faction standing with Miners Guild 249 got better. 
-#Your faction standing with Merchants of Kaladim got better. 
-#Your faction standing with Craknek Warriors got worse. 
+		quest::ding(); quest::exp(1000);
+		quest::faction(314,15); # +Storm Guard
+		quest::faction(169,15); # +Kazon Stormhammer
+		quest::faction(219,15); # +Miners Guild 249
+		quest::faction(215,15); # +Merchants of Kaladim
+		quest::faction(57,-15); # -Craknek Warriors
 	}
 }
 #END of FILE Zone:kaladimb  ID:67033 -- Trantor_Everhot 
