3433e9e5cad35dc075a0f6f2592954b02dd20482
Angelox22@gmail.com <Angelox22@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2012-11-28 02:30:18 +0000

    Ranger Trueshot quests will work for all classes (rewards are ranger-only wearable) Maesyn Trueshot will also combine the Trueshot bow for you.

--- trunk/quests/gfaydark/Maesyn_Trueshot.pl
+++ trunk/quests/gfaydark/Maesyn_Trueshot.pl
@@ -17,7 +17,7 @@ my $incarnation = quest::saylink("incarnation", 1);
   elsif($text=~/trueshot/i) {
     $client->Message(14,"The Trueshot Longbow was created by my famed father. Eldin Trueshot. It is quite accurate and takes a ranger's skill to wield. We use our new recruits to $gather materials needed by my father. We shall soon begin to release the formula to good elves so all may fletch such a bow.");
   }
-  elsif(($text=~/gather/i) && ($class eq "Ranger")) {
+  elsif($text=~/gather/i) {
     if($faction < 5) { #Needs better than indifferent
       $client->Message(14,"Take this pack. Go to Kaladim, find Trantor Everhot and ask for dwarven wire. Then go to Freeport to meet Jyle Windshot. Search the inns for him and ask him for treant wood. Then, collect some spiderling silk from spiderlings and finally, in Steamfont, we have the permission of the gnomes to use any micro servos we find while destroying rogue spiders. Combine them all and return the pack to me.");
       quest::summonitem(17951); #Material Pack
@@ -27,7 +27,7 @@ my $incarnation = quest::saylink("incarnation", 1);
     }
   }
   elsif($text=~/correct/i) {
-    $client->Message(14,"Now that I have crafted the Treant Bow Staff, you shall need one Planing Tool, one Treant Bow Staff, one Micro Servo and one spool of Dwarven Wire. These items will be used with your Fletching Kit as all other bows. Be forewarned, only a Master Fletcher can create such a bow and even a master fails from time to time. Good Luck.");
+    $client->Message(14,"Now that I have crafted the Treant Bow Staff, you shall need one Planing Tool, one Treant Bow Staff, one Micro Servo and one spool of Dwarven Wire. These items will be used with your Fletching Kit as all other bows. Be forewarned, only a Master Fletcher can create such a bow and even a master fails from time to time. Good Luck. If you feel you don't have the skill for this, hand me the items and I will make the combine for you.");
   }
   elsif($text=~/incarnation/i) {
     $client->Message(14,"The Trueshot Longbow was once enchanted by the Koada'Dal enchanters.  Once it was enchanted now it is no more. I am sure if you were ask the Koada'Dal [where the enchanted bows] are you will get an answer.");
@@ -37,7 +37,12 @@ my $incarnation = quest::saylink("incarnation", 1);
 sub EVENT_ITEM {
 my $correct = quest::saylink("correct", 1);
 my $incarnation = quest::saylink("incarnation", 1);
-  if(plugin::check_handin(\%itemcount, 12112 => 1)) { #Pack of Materials
+  if ((plugin::check_handin(\%itemcount, 8091 => 1)) && (plugin::check_handin(\%itemcount, 8086 => 1))
+    && (plugin::check_handin(\%itemcount, 12113 => 1))&& (plugin::check_handin(\%itemcount, 12111 => 1))){
+    quest::summonitem(8401); #Trueshot Longbow 
+    $client->Message(14,"Enjoy your bow!");
+  }
+  elsif (plugin::check_handin(\%itemcount, 12112 => 1)) { #Pack of Materials
     $client->Message(14,"I shall see that my father gets the materials. I hope this can be of use to you. It will serve as your starting point toward fletching a Trueshot longbow. It is unfortunate that we are unable to enchant the bow to its next $incarnation, but it is still a fine weapon. You do know the $correct components needed for fletching such a bow, do you not?");
     quest::givecash(0,6,0,0); #6 gold
     quest::summonitem(8091); #Treant Bow Staff
