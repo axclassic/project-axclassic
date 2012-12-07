542e63fbcf4f55fdec237485d76822f6890b391b
Angelox22@gmail.com <Angelox22@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2012-12-07 14:38:45 +0000

    Added highpasshold quests, should be a duplicate of highpass - SoF clients (and higher) will zone into highpasshold, Titanium clients will use highpass.

---
+++ trunk/quests/highpasshold/Captain_Orben.pl
@@ -0,0 +1,19 @@
+sub EVENT_ITEM { 
+if($itemcount{13792} == 1 && $itemcount{6331} == 4){
+quest::givecash("3","6","1","0");
+quest::faction("Not_Found","1");
+quest::faction("Not_Found","1");
+quest::faction("Not_Found","1");
+quest::faction("10102","1");
+quest::faction("Not_Found","1");
+}
+if($itemcount{13792} == 1){
+quest::givecash("6","7","2","0");
+quest::faction("Not_Found","1");
+quest::faction("Not_Found","1");
+quest::faction("Not_Found","1");
+quest::faction("10102","1");
+quest::faction("Not_Found","1"); }
+}
+#END of FILE Zone:highpass  ID:5015 -- Captain_Orben 
+
