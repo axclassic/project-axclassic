ae8c03028509ec067bed947dc9b10053cf395cf7
threeflies@gmail.com <threeflies@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2012-12-23 21:33:51 +0000

    added zoneid

--- trunk/quests/potimeb/Time_Companion.pl
+++ trunk/quests/potimeb/Time_Companion.pl
@@ -11,18 +11,18 @@ quest::say("Please choose [fire] [water] [air] [undead] [earth] as your destinat
 }
 
 if ($text=~ /fire/i){
-quest::say("Good luck $name"); quest::movepc(-59.1,587.1,493.9,64.6);
+quest::say("Good luck $name"); quest::movepc(223,-59.1,587.1,493.9,64.6);
 }
 if ($text=~ /water/i){
-quest::say("Good luck $name"); quest::movepc(-58,884.8,493.7,67.8);
+quest::say("Good luck $name"); quest::movepc(223,-58,884.8,493.7,67.8);
 }
 if ($text=~ /earth/i){
-quest::say("Good luck $name"); quest::movepc(-61.9,1638.6,496.1,65.5);
+quest::say("Good luck $name"); quest::movepc(223,-61.9,1638.6,496.1,65.5);
 }
 if ($text=~ /air/i){
-quest::say("Good luck $name"); quest::movepc(-59.2,1333.4,494.1,56.9);
+quest::say("Good luck $name"); quest::movepc(223,-59.2,1333.4,494.1,56.9);
 }
 if ($text=~ /undead/i){
-quest::say("Good luck $name"); quest::movepc(-36.5,1092.4,495.2,54.6);
+quest::say("Good luck $name"); quest::movepc(223,-36.5,1092.4,495.2,54.6);
 }
 }
