28434e2e590497f9415bcbcf1dce29a1869688ae
Angelox22@gmail.com <Angelox22@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2012-12-28 12:37:19 +0000

    Converted npcids for AXClassic

--- trunk/quests/soldungc/#Fireback_Queen.pl
+++ trunk/quests/soldungc/#Fireback_Queen.pl
@@ -5,9 +5,9 @@ sub EVENT_SPAWN {
 sub EVENT_TIMER {
   if ($timer eq "queen") {
   quest::stoptimer("queen");
-  quest::signalwith(278119,71,0);
-  quest::depopall(278112);
-  quest::depopall(278113);
+  quest::signalwith(278119,71,0); #278119 (same)
+  quest::depopall(278262); #278112
+  quest::depopall(278263); #278113
   quest::depop();
   }
    }
