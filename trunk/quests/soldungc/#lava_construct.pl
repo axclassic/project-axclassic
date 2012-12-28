d7c0c2c8dd0ce9e728070d5d94a4a9539e19c391
threeflies@gmail.com <threeflies@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2012-12-28 17:06:50 +0000

    ID updates

--- trunk/quests/soldungc/#lava_construct.pl
+++ trunk/quests/soldungc/#lava_construct.pl
@@ -4,9 +4,9 @@ quest::settimer("construct",1);
      
 sub EVENT_TIMER { #depop doesn't work well with sub EVENT_SPAWN
 $elemrest = $entity_list->GetMobByNpcTypeID(278124);
-$furiousmagma = $entity_list->GetMobByNpcTypeID(278123);
-$inflamedchamp = $entity_list->GetMobByNpcTypeID(278122);
-$protectorfire = $entity_list->GetMobByNpcTypeID(278109);
+$furiousmagma = $entity_list->GetMobByNpcTypeID(278273);
+$inflamedchamp = $entity_list->GetMobByNpcTypeID(278272);
+$protectorfire = $entity_list->GetMobByNpcTypeID(278259);
 
 if ($timer eq "construct") {
 if ($elemrest || $furiousmagma || $inflamedchamp || $protectorfire) {
