76adbd4a5b8a1ff5b77979814b9b667c284b509a
Angelox22@gmail.com <Angelox22@gmail.com>
2013-01-05 17:29:55 +0000

    Executables Update - See ax_changelog.txt

--- windows_executables/sql_updates/02_AXUpdate.sql
+++ windows_executables/sql_updates/02_AXUpdate.sql
@@ -12,3 +12,14 @@ ALTER TABLE `ax_classic`.`botbank` DROP PRIMARY KEY,
  ADD PRIMARY KEY  USING BTREE(`groupid`, `botid`);
 
 UPDATE ax_classic.botbank SET groupid = 20;
+
+ALTER TABLE `ax_classic`.`group_id` ADD COLUMN `groupid` INT(4)  NOT NULL FIRST,
+ DROP PRIMARY KEY,
+ ADD PRIMARY KEY  USING BTREE(`charid`, `groupid`);
+ 
+ ALTER TABLE `ax_classic`.`botinventory` MODIFY COLUMN `color` INTEGER UNSIGNED NOT NULL;
+ 
+ UPDATE `ax_classic`.`botinventory` SET color = 0;
+ 
+ ALTER TABLE `ax_classic`.`botinventory` MODIFY COLUMN `color` BIGINT UNSIGNED NOT NULL DEFAULT 0;
+ 
