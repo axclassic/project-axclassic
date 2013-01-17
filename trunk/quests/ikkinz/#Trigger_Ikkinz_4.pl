69a6255558ddfbba464abcb008fbdaaa959d5707
Angelox22@gmail.com <Angelox22@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2013-01-17 23:26:07 +0000

    Quests for Taelsoia port

---
+++ trunk/quests/ikkinz/#Trigger_Ikkinz_4.pl
@@ -0,0 +1,19 @@
+sub EVENT_SPAWN {
+  quest::set_proximity($x-50,$x+50,$y-70,$y+70);
+}
+
+sub EVENT_SIGNAL {
+  if($signal == 1) {
+      $counter += 1;
+}
+  if($counter == 2) {
+    quest::signalwith(294611,1,4);
+    }
+}
+
+sub EVENT_PROXIMITY_SAY {
+  if ($text=~/return/i) {
+   $InInstanceIkky7 = quest::GetInstanceID("ikkinz",6);
+    quest::MovePCInstance(294,$InInstanceIkky7,500,-615,-50,186);
+    }
+  }
---
+++ trunk/quests/snpool/#Utandi.pl
@@ -0,0 +1,46 @@
+sub EVENT_SPAWN  {
+$event = 0;
+}
+
+sub EVENT_SAY {
+  if (defined $qglobals{sewers} && $qglobals{sewers} == 5) {
+  if ($text=~/hail/i) {
+  if ($event == 0) {
+    quest::say("I have a problem");
+    }
+  }
+}
+  if (defined $qglobals{sewers} && $qglobals{sewers} == 5) {
+    if ($text=~/what problem/i) {
+    if ($event == 0) {
+  quest::say("I have lost my map.  It shows me how to get home. Can you help me find it?");
+    }
+  }
+}
+  if (defined $qglobals{sewers} && $qglobals{sewers} == 5) {
+  if ($text=~/I will help you find it/i) {
+      if ($event == 0) {
+    quest::say("Thanks!  Try to find a big pile of slime, I think he has my map.");
+    quest::spawn2(285085,0,0,1354,471,-138,187);
+    $event = 1;
+  }
+  }
+  }
+  }
+  sub EVENT_ITEM {
+  if (plugin::check_handin(\%itemcount, 55617 =>1, 55618 =>1, 55619 =>1, 55620 =>1 )) {
+$group = $entity_list->GetGroupByClient($client);
+      if ($group) {
+        for ($count = 0; $count < $group->GroupCount(); $count++) {
+          push (@player_list, $group->GetMember($count)->GetName());
+        }
+}
+foreach $player (@player_list) {
+    $pc = $entity_list->GetClientByName($player);
+     $charid = $pc->CharacterID();
+     quest::targlobal("temp_sewers", 4, "F", 283052, $charid, 283);
+   $pc->Message(4,"You receive a temporary flag!");
+}
+}
+ plugin::return_items(\%itemcount);  
+  }
