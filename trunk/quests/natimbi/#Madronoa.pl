d723f757ab0e7552c2c14c47afe683a04c950622
Caved12@gmail.com <Caved12@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2013-01-19 15:09:58 +0000

    zoned to wrong pot corrected

--- trunk/quests/natimbi/#Madronoa.pl
+++ trunk/quests/natimbi/#Madronoa.pl
@@ -12,7 +12,7 @@ my $travel = quest::saylink("travel", 1);
     $client->Message(14,"Hello $name, how are you today? My name is Madronoa and I have recently returned from the Prayer Grounds of Calling. A group of young adventurers such as yourself led me there some time ago. While there I've learned how to channel the energy to teleport those such as yourself there. If you wish to $travel to Qvic, I shall create a portal for you.");
   }
   if ($text=~/travel/i) {
-            quest::movepc(295,-124,-651,-422,0); #port to qvic
+            quest::movepc(295,-2514.10,767.62,-647.69,0); #port to qvic
          }
       }
 
