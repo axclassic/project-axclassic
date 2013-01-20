32c36dfee91f576ec6bf4f6a46d69ce94c43c010
threeflies@gmail.com <threeflies@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2013-01-20 14:37:15 +0000

    Added wayfarer zone message , combine not tested yet.

--- quests/templates/player.pl
+++ quests/templates/player.pl
@@ -147,4 +147,49 @@ elsif ((defined $Classic2012) && ($Classic2012 > 0)) {
 	$client->Message(15,"Thank you for trying us out!");
       }
     }
+	if($ulevel >= 15 && !defined($qglobals{Wayfarer}) && $client->GetStartZone()!=$zoneid && $zoneid !=50 && $zoneid !=12) {
+    $client->Message(15,"A mysterious voice whispers to you, 'If you can feel me in your thoughts, know this -- something is changing in the world and I reckon you should be a part of it. I do not know much, but I do know that in every home city and the wilds there are agents of an organization called the Wayfarers Brotherhood. They are looking for recruits . . . If you can hear this message, you are one of the chosen. Rush to your home city, or search the West Karanas and Rathe Mountains for a contact if you have been exiled from your home for your deeds, and find out more. Adventure awaits you, my friend.'");
+  }
+}
+
+sub EVENT_COMBINE_SUCCESS
+{
+    if ($recipe_id =~ /^1090[4-7]$/) {
+        $client->Message(1,
+            "The gem resonates with power as the shards placed within glow unlocking some of the stone's power. ".
+            "You were successful in assembling most of the stone but there are four slots left to fill, ".
+            "where could those four pieces be?"
+        );
+    }
+    elsif ($recipe_id =~ /^10(903|346|334)$/) {
+        my %reward = (
+            melee  => {
+                10903 => 67665,
+                10346 => 67660,
+                10334 => 67653
+            },
+            hybrid => {
+                10903 => 67666,
+                10346 => 67661,
+                10334 => 67654
+            },
+            priest => {
+                10903 => 67667,
+                10346 => 67662,
+                10334 => 67655
+            },
+            caster => {
+                10903 => 67668,
+                10346 => 67663,
+                10334 => 67656
+            }
+        );
+        my $type = plugin::ClassType($class);
+        quest::summonitem($reward{$type}{$recipe_id});
+        quest::summonitem(67704);
+        $client->Message(1,"Success");
+    }
+}
+}
+
 }
