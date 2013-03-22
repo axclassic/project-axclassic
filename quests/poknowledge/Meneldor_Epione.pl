143e8780f8d5511eb65307c6a4ebdd281a9e3227
Caved12@gmail.com <Caved12@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2013-03-22 15:27:43 +0000

    Typo + instance from 6 to 8 hrs

--- quests/poknowledge/Meneldor_Epione.pl
+++ quests/poknowledge/Meneldor_Epione.pl
@@ -11,7 +11,7 @@ my $back = quest::saylink("back", 1);
 my $here = quest::saylink("here", 1);
 
   if ($text=~/hail/i && $ulevel >= 64) {
-    $client->Message(14,"Hello $name , I am Meneldor Epione. I am here to tell you about Tier 4 armor, it is called Rathe Armor (Tier 4) it can be found in a special instanced dungeon. You can find all the pieces there exept for the chest pieces. You wil have to bring me 3 The Rathe Server Doll (Tier 4)'s and I wil give you a random Tier 4 chestpiece, either Leather, Plate, Chain or Silk. These dolls drop in this quest dungeon to occasionaly. If you want I can $open a zone instance for you to begin your Tier 4 challenge, it will stay open for 6 hours. I charge 2000pp per zone instance. You can purchase a new instance after the old one has expired.");
+    $client->Message(14,"Hello $name , I am Meneldor Epione. I am here to tell you about Tier 4 armor, it is called Rathe Armor (Tier 4) it can be found in a special instanced dungeon. You can find all the pieces there exept for the chest pieces. You wil have to bring me 3 The Rathe Server Doll (Tier 4)'s and I wil give you a random Tier 4 chestpiece, either Leather, Plate, Chain or Silk. These dolls drop in this quest dungeon to occasionaly. If you want I can $open a zone instance for you to begin your Tier 4 challenge, it will stay open for 8 hours. I charge 2000pp per zone instance. You can purchase a new instance after the old one has expired.");
 $client->Message(12,"In the event that you would die in your Tier 4 quest instance, I can port you $back for as long as the instance is active. In case your instance has already expired, your corpse will end up $here.");
   }
 
@@ -20,10 +20,10 @@ $client->Message(12,"In the event that you would die in your Tier 4 quest instan
   }
 
   if ($text=~/open/i && $ulevel >= 65 && !defined($qglobals{$name."chamberse"})) {
-    $client->Message(14,"Just hand me 2000pp and i will create the instance for your challenge and port you there.");
+    $client->Message(14,"Just hand me 2000pp and I will create the instance for your challenge and port you there.");
   }
   if ($text=~/open/i && $ulevel >= 65 && defined($qglobals{$name."chamberse"})) {
-    $client->Message(14,"You already have an existing Tier 4 zone instance. You can't purchase a newone until it has expired,but I can port you $back to it if you like.");
+    $client->Message(14,"You already have an existing Tier 4 zone instance. You can't purchase a newone until it has expired, but I can port you $back to it if you like.");
 }
   if ($text=~/back/i && $ulevel >= 64) {
    if (defined($qglobals{$name."chamberse"})) {
@@ -56,9 +56,9 @@ if (plugin::check_handin(\%itemcount, 119651 => 3)) {
 elsif (($platinum == 2000) && $ulevel >= 65 && !defined($qglobals{$name."chamberse"})) {
     $client->Message(14,"Thank you $class , you are on the way to your Tier 4 Challenge, good luck!"); #Money handin for instance creation and porting to it.
 
-    my $instanceID = quest::CreateInstance("chamberse", 0, 21600);
+    my $instanceID = quest::CreateInstance("chamberse", 0, 28800);
     quest::AssignToInstance($instanceID);
-    quest::setglobal($name."chamberse",$instanceID,7,H6);
+    quest::setglobal($name."chamberse",$instanceID,7,H8);
     quest::MovePCInstance(308, $instanceID, 0.00, 0.00, -0.21, 150);
 return 1;
  }
