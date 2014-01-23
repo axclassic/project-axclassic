a9635d13de862b0141fabde9219a6fc8f64276ff
Angelox22@gmail.com <Angelox22@gmail.com>
2014-01-23 01:51:57 +0000

    lowered level for Fabled evil eye attack

--- quests/beholder/#The_Fabled_Evil_Eye.pl
+++ quests/beholder/#The_Fabled_Evil_Eye.pl
@@ -3,14 +3,14 @@
 
 sub EVENT_AGGRO
 {
-	if($ulevel <= 22)
+	if($ulevel <= 10)
 {
 	my $x = $npc->GetX();
     	my $y = $npc->GetY();
     	my $z = $npc->GetZ();
     	my $h = $npc->GetHeading();
   	quest::spawn2(16010,0,0,$x,$y,$z,$h);
-	quest::shout("We shall meet again $name! When you are a worthy opponent!!");
+	#quest::shout("We shall meet again $name! When you are a worthy opponent!!");
 	quest::depop;
 	}
 }
