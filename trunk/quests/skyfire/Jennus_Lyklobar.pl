8f9b4f490667d731a9c0f4cdf8bdae8f925c2a5d
Angelox22@gmail.com <Angelox22@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-02-04 12:22:46 +0000

    Typo fix

--- trunk/quests/skyfire/Jennus_Lyklobar.pl
+++ trunk/quests/skyfire/Jennus_Lyklobar.pl
@@ -10,7 +10,7 @@ sub EVENT_SAY {
   if ($text=~/hail/i) {
     quest::say("Hello and well met! A $race like you does not often come here.. Do you come seeking what I possess? Fire, fire, fire and fire is mine! I surround myself with fire, I bathe in fire, I consume fire, I am the Master of Fire! Hahahaha, but I digress. Have you come for the element of Fire, seeking to fall to the same fate as Trilith Magi'kot?");
   }
-  if ($text=~/ielement of fire/i) {
+  if ($text=~/element/i) {
     quest::say("The Element of Fire I can provide you, but you must prove you are able to contain its power. Bring me the Torch of the Elements now kept by a doomed lord of merchants, the burning embers from a notable member of wurmkind who can be found a short journey from here and a blazing wand, which you should be familiar with. And you must surrender the tome Rykas gave you. But I think you would be happier if you stayed with me and learned more of the beauty of the one true element, FIRE! FIRE! Hahahahahahah!");
   }
 }
