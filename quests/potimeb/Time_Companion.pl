be270bb016ece2ef7c54f92418c1dc935cb39a6c
Caved12@gmail.com <Caved12@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-05-05 11:39:38 +0000

    fix

--- quests/potimeb/Time_Companion.pl
+++ quests/potimeb/Time_Companion.pl
@@ -15,7 +15,7 @@ my $air = quest::saylink("air", 1);
 my $undead = quest::saylink("undead", 1);
 
 if ($text=~ /hail/i) {
-$client->Message(14,"Please choose:| $fire | $water | $air | $undead  | $earth |  as your destination");
+$client->Message(14,"Please choose:| $fire | $water | $air | $undead  | $earth |  as your destination.");
 }
 
 ################FIRE#####################################
@@ -23,7 +23,7 @@ if ($text=~/fire/i) {
   if (defined($qglobals{$name."potimeb"})) {
      $client->Message(14,"Good luck $name , porting you inside your instance.");
      my $QGlobalValue = $qglobals{$name."potimeb"};
-     quest::MovePCInstance(223, $QGlobalValue, -59.1, 587.1, 493.9, 150);
+     quest::MovePCInstance(223, $QGlobalValue, -59.1, 587.1, 493.9, [64.6]);
      return 1; 
       }
   else {
@@ -38,7 +38,7 @@ if ($text=~/water/i) {
   if (defined($qglobals{$name."potimeb"})) {
      $client->Message(14,"Good luck $name , porting you inside your instance.");
      my $QGlobalValue = $qglobals{$name."potimeb"};
-     quest::MovePCInstance(223, $QGlobalValue, -58, 884.8, 493.7, 150);
+     quest::MovePCInstance(223, $QGlobalValue, -58, 884.8, 493.7, [67.8]);
      return 1;
       } 
   else {
@@ -53,7 +53,7 @@ if ($text=~/earth/i) {
   if (defined($qglobals{$name."potimeb"})) {
      $client->Message(14,"Going to send you back to your Time B instance now.");
      my $QGlobalValue = $qglobals{$name."potimeb"};
-     quest::MovePCInstance(223, $QGlobalValue, -39.13, 1092.13, 495.53, 150);
+     quest::MovePCInstance(223, $QGlobalValue, -39.13, 1092.13, 495.53, [65.5]);
      return 1;
       }
   else {
@@ -68,7 +68,7 @@ if ($text=~/air/i) {
   if (defined($qglobals{$name."potimeb"})) {
      $client->Message(14,"Good luck $name , porting you inside your instance.");
      my $QGlobalValue = $qglobals{$name."potimeb"};
-     quest::MovePCInstance(223, $QGlobalValue, -59.2, 1333.4, 494.1, 150);
+     quest::MovePCInstance(223, $QGlobalValue, -59.2, 1333.4, 494.1, [56.9]);
      return 1;
       }
   else {
@@ -83,7 +83,7 @@ if ($text=~/undead/i) {
   if (defined($qglobals{$name."potimeb"})) {
      $client->Message(14,"Good luck $name , porting you inside your instance.");
      my $QGlobalValue = $qglobals{$name."potimeb"};
-     quest::MovePCInstance(223, $QGlobalValue, -36.5, 1092.4, 495.2, 150);
+     quest::MovePCInstance(223, $QGlobalValue, -36.5, 1092.4, 495.2, [54.6]);
      return 1;
       }
   else {
