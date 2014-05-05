1f9ff862f680c5718023562dfe7764d12fb4eb9f
Caved12@gmail.com <Caved12@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-05-05 10:47:06 +0000

    fix

--- quests/potimeb/Time_Companion.pl
+++ quests/potimeb/Time_Companion.pl
@@ -3,7 +3,7 @@
 ## Zone: Plane of Time B             ##
 ## Threeflies dec 2012               ##
 ## Revised by Caved for AXClassic    ##
-##                    21-03-2014     ##
+##                    05-05-2014     ##
 ## Trial mover NPC                   ##
 #######################################
 
@@ -24,12 +24,14 @@ if ($text=~/fire/i) {
      $client->Message(14,"Good luck $name , porting you inside your instance.");
      my $QGlobalValue = $qglobals{$name."potimeb"};
      quest::MovePCInstance(223, $QGlobalValue, -59.1, 587.1, 493.9, 150);
+     return 1; 
 }
-else {
- $client->Message(14,"Good luck $name , porting inside regular instance.");
- quest::movepc(223, -59.1, 587.1, 493.9, [64.6]);
+   if (!defined($qglobals{$name."potimeb"})) {
+     $client->Message(14,"Good luck $name , porting inside regular instance.");
+     quest::movepc(223, -59.1, 587.1, 493.9, [64.6]);
+     return 1;
  }
-}
+}hhhhhhh
 
 ################WATER####################################
 if ($text=~/water/i) {
@@ -37,10 +39,12 @@ if ($text=~/water/i) {
      $client->Message(14,"Good luck $name , porting you inside your instance.");
      my $QGlobalValue = $qglobals{$name."potimeb"};
      quest::MovePCInstance(223, $QGlobalValue, -58, 884.8, 493.7, 150);
-}
-else {
-$client->Message(14,"Good luck $name , Porting inside regular instance"); 
-quest::movepc(223, -58, 884.8, 493.7, [67.8]);
+     return 1;
+} 
+   if (!defined($qglobals{$name."potimeb"})) {
+     $client->Message(14,"Good luck $name , Porting inside regular instance"); 
+     quest::movepc(223, -58, 884.8, 493.7, [67.8]);
+     return 1;
  }
 }
 
@@ -50,10 +54,12 @@ if ($text=~/earth) {
      $client->Message(14,"Good luck $name , porting you inside your instance.");
      my $QGlobalValue = $qglobals{$name."potimeb"};
      quest::MovePCInstance(223, $QGlobalValue, -61.9, 1638.6, 496.1, 150);
+     return 1;
 }
-else {
- $client->Message(14,"Good luck $name , Porting inside regular instance"); 
- quest::movepc(223, -61.9, 1638.6, 496.1, [65.5]);
+   if (!defined($qglobals{$name."potimeb"})) {
+     $client->Message(14,"Good luck $name , Porting inside regular instance"); 
+     quest::movepc(223, -61.9, 1638.6, 496.1, [65.5]);
+     return 1;
  }
 }
 
@@ -63,10 +69,12 @@ if ($text=~/air/i) {
      $client->Message(14,"Good luck $name , porting you inside your instance.");
      my $QGlobalValue = $qglobals{$name."potimeb"};
      quest::MovePCInstance(223, $QGlobalValue, -59.2, 1333.4, 494.1, 150);
+     return 1;
 }
-else {
- $client->Message(14,"Good luck $name , Porting inside regular instance"); 
- quest::movepc(223, -59.2, 1333.4, 494.1, [56.9]);
+   if (!defined($qglobals{$name."potimeb"})) {
+     $client->Message(14,"Good luck $name , Porting inside regular instance"); 
+     quest::movepc(223, -59.2, 1333.4, 494.1, [56.9]);
+     return 1;
  }
 }
 
@@ -76,11 +84,12 @@ if ($text=~/undead/i) {
      $client->Message(14,"Good luck $name , porting you inside your instance.");
      my $QGlobalValue = $qglobals{$name."potimeb"};
      quest::MovePCInstance(223, $QGlobalValue, -36.5, 1092.4, 495.2, 150);
+     return 1;
 }
-else {
- $client->Message(14,"Good luck $name , Porting inside regular instance");
- quest::movepc(223, -36.5, 1092.4, 495.2, [54.6]);
+   if (!defined($qglobals{$name."potimeb"})) {
+     $client->Message(14,"Good luck $name , Porting inside regular instance");
+     quest::movepc(223, -36.5, 1092.4, 495.2, [54.6]);
+     return 1;
  }
-}
 }
 }
