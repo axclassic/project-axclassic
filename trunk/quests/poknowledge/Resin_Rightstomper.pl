029e8909e65640d420dc441e0e1b2739fcefced9
resquone@gmail.com <resquone@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-06-06 13:52:56 +0000

    Update Quest zone#

--- trunk/quests/poknowledge/Resin_Rightstomper.pl
+++ trunk/quests/poknowledge/Resin_Rightstomper.pl
@@ -2,8 +2,8 @@
 # Based on input from Rightman and 10th coldain ring #
 # Author: Resqu Miplez #
 # Axclassic Rathe Server #
-# test script 3.7 # 
-#Last issue One one would be marked for zone #
+# test script 3.8 # 
+#Last issue Forgot to Change the Zone number LOL #
 #This is a Forum version to test prior to revising for use#
 
 sub EVENT_SAY {
@@ -11,7 +11,7 @@ sub EVENT_SAY {
 $inpoka = quest::GetInstanceID("greatdivide",0);
 
 if ($text =~/hail/i) {
-	quest::say ("hi");
+	quest::say ("hi this is Second attempt");
 
 	if($inpoka == 0) {
 		$Instance = quest::CreateInstance("greatdivide", 0, 64800);
@@ -21,7 +21,7 @@ if ($text =~/hail/i) {
 
 		quest::setglobal($name."greatdivide",$Instance,7,"H18");
 		quest::setglobal($name."zone",$Instance,7,"H18");
-		quest::MovePCInstance(55, $Instance, -965,-7720,-557);
+		quest::MovePCInstance(118, $Instance, -965,-7720,-557);
 		quest::say("I'd move you, but Cy has it commented out currently.");
 
 	} else {
@@ -31,10 +31,11 @@ if ($text =~/hail/i) {
 
 }
 if ($text =~/return/i) {
-		quest::MovePCInstance(55, $inpoka, -965,-7720,-557);
+		quest::MovePCInstance(118, $inpoka, -965,-7720,-557);
 	}
 if ($text =~/clearme/i) {
 		quest::DestroyInstance(56);
+		$inpoka == 0;
 		quest::say("Clearing your instance stuff");
 	}
 }
