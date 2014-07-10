ca551d0fca8fbb25ee1d250f676cbec520973354
Caved12@gmail.com <Caved12@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-07-10 19:49:39 +0000

    update

--- quests/poknowledge/#The_Rathe_Server_Casino.pl
+++ quests/poknowledge/#The_Rathe_Server_Casino.pl
@@ -11,3 +11,26 @@ sub EVENT_SAY {
     $client->Message(14,"Hello $name , WELCOME to the Casino, in the future I will roll the diece for you so you can win grand prices. You will need to give me 1 Small Gold Token (you can buy them from the vendor on the right) to spin the diece. But not yet my friend be patient I am not yet functional! My friend Caved is teaching me how to run the Casino as we speak.");
   }
 }
+
+sub EVENT_ENTER
+{
+	$npc->SetAppearance(1);
+	my $random_result = int(rand(100));
+	if ($random_result<=10){
+	quest::shout("Come place your bets next to the main bank, give me 1 small gold token and lets roll. Do you have what it takes? ARE you a WINNER? You KNOW you want me to roll the diece... GREAT prices PLAY now!");
+	}
+else{
+	#Do Nothing
+ }
+}
+
+
+sub EVENT_SIGNAL {
+    my $random_result = int(rand(100));
+    if($random_result<=20){
+    quest::shout("Come place your bets next to the main bank, give me 1 small gold token and lets roll. Do you have what it takes? ARE you a WINNER? You KNOW you want me to roll the diece... GREAT prices PLAY now!");
+    }
+else{
+    #say nothing
+ }
+}
