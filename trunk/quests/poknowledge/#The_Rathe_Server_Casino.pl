924c40e5738582ed3487e12130c064e219faae76
Caved12@gmail.com <Caved12@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-07-11 01:09:03 +0000

    new things

--- trunk/quests/poknowledge/#The_Rathe_Server_Casino.pl
+++ trunk/quests/poknowledge/#The_Rathe_Server_Casino.pl
@@ -5,21 +5,24 @@
 ## Created by Caved for AXClassic    ##
 ## The Rathe Server     10-07-2014   ##
 #######################################
-sub EVENT_SAY {
+sub EVENT_SAY { 
 
   if ($text=~/hail/i) {
-    $client->Message(14,"Hello $name , WELCOME to the Casino, in the future I will roll the diece for you so you can win grand prices. You will need to give me 1 Small Gold Token (you can buy them from the vendor on the right) to spin the diece. But not yet my friend be patient I am not yet functional! My friend Caved is teaching me how to run the Casino as we speak.");
+    $client->Message(14,"Hello $name , WELCOME to the Casino, in the future I will roll the diece for you so you can win grand prices. You will need to give me 1 small gold token (you can buy them from the vendor on the right) to spin the diece. I am now fully functional!");
   }
+  else {
+   return 1;
+}
 }
 
 sub EVENT_ENTER {
 	$npc->SetAppearance(1);
 	my $random_result = int(rand(100));
-	
-if ($random_result<=10) {
+
+	if ($random_result<=10) {
 	quest::shout("Come place your bets next to the main bank, give me 1 small gold token and lets roll. Do you have what it takes? ARE you a WINNER? You KNOW you want me to roll the diece... GREAT prices PLAY now!");
 	}
-else {
+else{
 	#Do Nothing
  }
 }
@@ -28,11 +31,40 @@ else {
 sub EVENT_SIGNAL {
     my $random_result = int(rand(100));
     
-if($random_result<=20){
+if($random_result<=20) {
     quest::shout("Come place your bets next to the main bank, give me 1 small gold token and lets roll. Do you have what it takes? ARE you a WINNER? You KNOW you want me to roll the diece... GREAT prices PLAY now!");
     }
 else{
     #say nothing
  }
 }
+
+sub EVENT_ITEM {
+if (plugin::check_handin(\%itemcount, 59970 => 1)) {
+    $client->Message(14,"Thank you $class , ROLLING the Diece..."); #Small Gold Token Handin
+    my $random_result = int(rand(1000));
+     if ($random_result <= 950) {
+        $client->Message(14,"BAD luck $name , unfortunatly you didnt win anything. better luck next time...");
+        return 1;
+        } 
+     if ($random_result > 950) {
+        $client->Message(14,"Good roll $name , you are a winner!");
+         my $random_result = int(rand(30));
+
+    
+
+
+
+quest::summonitem(119544);
+    quest::exp(5000);
+    quest::ding();
+    return 1;
+ }
+ 
+ 
+else {
+    $client->Message(14,"I don't need this $name. Take it back.");
+    plugin::return_items(\%itemcount);
+    return 1;
+ }
 }
