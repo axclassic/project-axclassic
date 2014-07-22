70f6bba47bbd42dcc69a120c916add554ae73558
Angelox22@gmail.com <Angelox22@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-07-22 23:30:53 +0000

    Can't use quest:spawn2 and have the same npc-id spawning from the database

--- trunk/quests/griegsend/163473.pl
+++ trunk/quests/griegsend/163473.pl
@@ -1,21 +1,21 @@
-sub EVENT_SPAWN
-{
-my $x;
-my $y;
-my $z;
-my $h;
-
-$x = $npc->GetX();
-$y = $npc->GetY();
-$z = $npc->GetZ();
-$h = $npc->GetHeading();
-
-
- 	quest::set_proximity( $x-10,$x+10,$y-10,$y+10,$z-10,$z+10);
-	}
-sub EVENT_ENTER {
-	quest::spawn2(163006,0,0,3383.0,-79.7,-6.2,254.1);
-	quest::depop;
-    }
- 
-
+# sub EVENT_SPAWN
+# {
+# my $x;
+# my $y;
+# my $z;
+# my $h;
+# 
+# $x = $npc->GetX();
+# $y = $npc->GetY();
+# $z = $npc->GetZ();
+# $h = $npc->GetHeading();
+# 
+# 
+#  	quest::set_proximity( $x-10,$x+10,$y-10,$y+10,$z-10,$z+10);
+# 	}
+# sub EVENT_ENTER {
+# 	quest::spawn2(163006,0,0,3383.0,-79.7,-6.2,254.1);
+# 	quest::depop;
+#     }
+#  
+# 
