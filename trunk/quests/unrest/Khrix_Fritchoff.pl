2d7dcb377d33580445eaa1e845773100fdffbe7e
Angelox22@gmail.com <Angelox22@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-07-22 22:39:30 +0000

    syntax corrections

--- trunk/quests/unrest/Khrix_Fritchoff.pl
+++ trunk/quests/unrest/Khrix_Fritchoff.pl
@@ -7,14 +7,14 @@ sub EVENT_SAY {
 
 sub EVENT_ITEM {
  if($itemcount{10400} == 1) {
-	quest::say('At last I may leave this place!! Good luck destroying my abomination, you will surely need it against my most powerful yet uncontrolable creation!');
+    quest::say('At last I may leave this place!! Good luck destroying my abomination, you will surely need it against my most powerful yet uncontrolable creation!');
     quest::ding();
-	quest::faction(71,-10)
-    quest::faction(91,10)
-    quest::faction(115,10)
-    quest::faction(76,10) 
+    quest::faction(71,-10);
+    quest::faction(91,10);
+    quest::faction(115,10);
+    quest::faction(76,10) ;
     quest::exp(2500);	
-	quest::spawn2(63098,0,0,60.9,597.3,19.8,124.9);
-	quest::depop();
+    quest::spawn2(63098,0,0,60.9,597.3,19.8,124.9);
+    quest::depop();
  }
 }
