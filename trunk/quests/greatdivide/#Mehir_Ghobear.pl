8ad4fadcf464297c82a437eedbdcf4142c0a447b
Caved12@gmail.com <Caved12@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-07-30 08:32:25 +0000

    new script prestige quest

--- trunk/quests/greatdivide/#Mehir_Ghobear.pl
+++ trunk/quests/greatdivide/#Mehir_Ghobear.pl
@@ -6,8 +6,6 @@
 ## EQWorld Server     29-07-2014     ##
 #######################################
 sub EVENT_SAY {
-my $prestige = quest::saylink("prestige", 1); 
-
 
   if ($text=~/hail/i && $ulevel >= 65) {
     $client->Message(14,"$name , please go away I dont need people like you skrewing up my hideout. I am trying to catch some vultures here and you are NOT helping my case at all!");
@@ -21,11 +19,15 @@ my $prestige = quest::saylink("prestige", 1);
 
 sub EVENT_ITEM {
 if (plugin::check_handin(\%itemcount, 119933 => 1) && $ulevel == 65) {
-    $client->Message(14,"Very well $class , I see you are here to help me catch those damned vultures. I need you to bring me a coin, 1 from each of them. Were they are exacly? i dont know. But I am confident that you will find them.")
+    $client->Message(14,"Very well $class , I see you are here to help me catch those damned vultures. I need you to bring me a coin, 1 from each of them. These coins will be the proof that you killed them. Were they are exacly? Noone knows. But I am confident that you will find them.");
+    quest::spawn("118170","0","0","-1961.229980","-6358.689941","-411.079987");
+    quest::spawn("118171","0","0","1572.400024","-1835.869995","137.740005");
+    quest::spawn("118172","0","0","-1643.349976","-2462.590088","186.240005");
+    quest::spawn("118173","0","0","2286.250000","-5496.649902","-162.460007");
  }
 elsif (plugin::check_handin(\%itemcount, 119928 => 1, 119929 => 1, 119930 => 1, 119931 => 1) && $ulevel == 65) {
-    $client->Message(14,"Wow $sname , You really DID pull it off I commend you. Please seek out .. .. in the Karanas to proceed!");
-quest::summonitem("");
+    $client->Message(14,"Wow $sname , You really DID pull it off I commend you. Please seek out Andy in the Karanas to proceed! Give him this token together with:  ");
+    quest::summonitem("119934");
  }
 else {
     $client->Message(14,"I don't need this $name. Take it back.");
