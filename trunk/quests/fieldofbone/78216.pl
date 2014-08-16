ecc1dacab66733d00e188f91b8fc5cb1a11f3ece
Veccrakk@gmail.com <Veccrakk@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-08-16 04:28:29 +0000

    flavor

--- trunk/quests/fieldofbone/78216.pl
+++ trunk/quests/fieldofbone/78216.pl
@@ -1,4 +1,4 @@
 sub EVENT_DEATH {
-quest::emote("nods and grins at you as you release its essence and  falls. As its hands rest on the ground, one unfurls, releasing an essence vial.");
+quest::emote("nods and grins at you as you release its essence and its body shudders for a split second before falling to the ground. As its hands rest on the ground, one unfurls, releasing an essence vial. You notice a new patch of grass form, is this a sign of new future or has evil taken a new appearance?");
 quest::spawn2(78218,0,0,920,518,175,65);
  }
