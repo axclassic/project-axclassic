26807a6c8b4588558f875ab0fad45dc58c5e5778
Veccrakk@gmail.com <Veccrakk@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-09-05 17:40:26 +0000

    Gnoll power

--- trunk/quests/halas/Lysbith_McNaff.pl
+++ trunk/quests/halas/Lysbith_McNaff.pl
@@ -1,14 +1,18 @@
 sub EVENT_SAY {
+my $serve = quest::saylink("serve", 1);
+my $orc = quest::saylink("orc", 1);
+my $goblins = quest::saylink("goblins", 1);
+
 if($text=~/Hail/i){
-quest::say("Hail! Ye've come to [serve Halas]. have ye not? We're the Wolves o' the North and it is our task to defend our city from harm.");
+quest::say("Hail! Ye've come to $serve Halas?. have ye not? We're the Wolves o' the North and it is our task to defend our city from harm.");
 }
-if($text=~/serve halas/i){
-quest::say("Halas is surrounded by barren arctic tundra. We've many foes. Among them are the [orc troopers]. [ice goblins] and the ever-present polar bears.");
+if($text=~/serve/i){
+quest::say("Halas is surrounded by barren arctic tundra. We've many foes. Among them are the $orc troopers,ice $goblins and the ever-present polar bears.");
 }
-if($text=~/what orc troopers/i){
+if($text=~/orc/i){
 quest::say("So. Ye think yerself strong enough to battle the snow orc troopers?  I pray to the Tribunal that ye are.  If ye can return to me four wrath orc wristbands  from the troopers' bodies. I'll reward ye with the [Seax].");
 }
-if($text=~/what ice goblins/i){
+if($text=~/goblins/i){
 quest::say("The ice goblins have plagued our community fer some time now.  At times. they even manage to get inside our walls. Fer the most part. they prey on travelers who pass through Everfrost Peaks. Since they're a weak race. we employ our youngest warriors to [protect the pass].");
 }
 if($text=~/what seax/i){
@@ -55,11 +59,12 @@ sub EVENT_ITEM {
  # Gnoll Bounty - Return 3 Gnoll Fangs
    if  (plugin::check_handin(\%itemcount, 13915 => 3)){
    quest::say("Fine work, fine work!  The gnoll threat must be extinguished before it can ever fully grow.  Ye've done yer part to aid our cause.  Please allow me to repay ye with a few provisions and a wee bit o' coin.  Then, continue with yer good deeds.");
-    quest::ding(); quest::exp(3800);
+   	my @items = (50011,50018,50039);
+			my $total = $items[ rand @items ];
+		  quest::summonitem($total);
+    quest::ding(); quest::exp(40000);
    quest::givecash("0","0","5","0");
-   # Reward: Iron Ration
-   quest::summonitem(13005);
-   # Wolves of the North
+     # Wolves of the North
    quest::faction("361","10");
    # Shamen of Justice
    quest::faction("294","10");
