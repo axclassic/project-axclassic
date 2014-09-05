b715bd20aafadf7b63e9eb325e411b7a4dc8e5a6
Veccrakk@gmail.com <Veccrakk@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-09-05 02:16:07 +0000

    x

--- quests/qeynos/Jusean_Evanesque.pl
+++ quests/qeynos/Jusean_Evanesque.pl
@@ -1,9 +1,12 @@
 sub EVENT_SAY {
- if($text =~ /Hail/i) {
-    quest::say("Why, hello there, $name! I am Jusean, loyal member of the League of Antonican Bards. You look like the reliable sort, maybe you could help me out for a bit, huh? It's about time for our field agents to turn in their [watch reports], and I need someone to go pick them up for me."); 
+my $watch = quest::saylink("watch reports", 1);
+my $AandB = quest::saylink("Anehan and Behroe", 1);
+my $LandQ = quest::saylink("Leanon and Quinon", 1);
+if($text =~ /Hail/i) {
+    quest::say("Why, hello there, $name! I am Jusean, loyal member of the League of Antonican Bards. You look like the reliable sort, maybe you could help me out for a bit, huh? It's about time for our field agents to turn in their $watch, and I need someone to go pick them up for me."); 
  }
  if ($text=~/watch reports/i) {
-   quest::say("We always have someone stationed on watch duty at the two main entries to the city to keep an eye on what is happening around Qeynos. We have [Anehan and Behroe] down at the docks, and [Leanon and Quinon] working the North Gate.");
+   quest::say("We always have someone stationed on watch duty at the two main entries to the city to keep an eye on what is happening around Qeynos. We have $AndB down at the docks, and $LandQ working the North Gate.");
  }
  if ($text=~/Anehan and Behroe/i) {
    quest::say("Here, if you take this to Anehan or Behroe down at the docks, and then return their report to me as soon as you can, I will give you a small reward.");
