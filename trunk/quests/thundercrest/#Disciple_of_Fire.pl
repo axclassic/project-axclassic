51b6ae5f4ec26ff933d9774c275f65bd94baaff4
BeatnikAardil@gmail.com <BeatnikAardil@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-09-27 02:29:36 +0000

    something wrong here trying to reload folder

--- trunk/quests/thundercrest/#Disciple_of_Fire.pl
+++
@@ -1,8 +0,0 @@
-sub EVENT_SIGNAL {
-if($signal == 109) { #signal 109 signal from controller to speak#
-quest::shout("I make no apologies. I shall burn your hearts out!");
-}
-}
-sub EVENT_DEATH {
-quest::signalwith(340049,113,0); 
-}
--- trunk/quests/thundercrest/#Master_of_Deceit.pl
+++
@@ -1,9 +0,0 @@
-sub EVENT_SIGNAL {
-if($signal == 109) { #signal 109 signal from controller to speak#
-quest::emote(' smiles broadly');
-quest::shout("You certainly will find Rage difficult to stop. I see it boiling up inside many of you. Allow it to take over, and Rage will ruin you.");
-}
-}
-sub EVENT_DEATH {
-quest::signalwith(340049,113,0); 
-}
