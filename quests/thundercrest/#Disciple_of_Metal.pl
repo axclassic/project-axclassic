4e949c4bf11b615b5e66b7972d630cc64d72aa6e
BeatnikAardil@gmail.com <BeatnikAardil@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-09-27 02:56:15 +0000

    trying to get the folder to work

--- quests/thundercrest/#Disciple_of_Metal.pl
+++
@@ -1,9 +0,0 @@
-sub EVENT_SIGNAL {
-if($signal == 109) { #signal 109 signal from controller to speak#
-quest::emote(' In a clear, ringing voice');
-quest::say("Please forgive my poor humor, but I am very eager to test your mettle.");
-}
-}
-sub EVENT_DEATH {
-quest::signalwith(340049,113,0); 
-}
