027906cabd34122363c40561f17064cdb195f3ff
BeatnikAardil@gmail.com <BeatnikAardil@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-09-27 02:29:36 +0000

    something wrong here trying to reload folder

--- quests/thundercrest/#Disciple_of_Earth.pl
+++
@@ -1,8 +0,0 @@
-sub EVENT_SIGNAL {
-if($signal == 109) { #signal 109 signal from controller to speak#
-quest::say("When we have defeated you, I shall see that your corpses rot in the soil as is proper.");
-}
-}
-sub EVENT_DEATH {
-quest::signalwith(340049,113,0); 
-}
--- quests/thundercrest/#Disciple_of_Wood.pl
+++
@@ -1,8 +0,0 @@
-sub EVENT_SIGNAL {
-if($signal == 109) { #signal 109 signal from controller to speak#
-quest::say("May our efforts here earn favor with those that watch over us all.");
-}
-}
-sub EVENT_DEATH {
-quest::signalwith(340049,113,0); 
-}
