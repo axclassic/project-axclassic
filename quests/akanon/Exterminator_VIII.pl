3f9bb9c3dcfd68e49ea2d4cf41a991c53490c857
Veccrakk@gmail.com <Veccrakk@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-11-01 21:36:19 +0000

    Reek the gnome

--- quests/akanon/Exterminator_VIII.pl
+++ quests/akanon/Exterminator_VIII.pl
@@ -1,21 +1,27 @@
 sub EVENT_SAY {
   if($text=~/Hail/i){ #Start of Exterminate the Vermin Quest(Scripted By: Fatty Beerbelly)
-    quest::say("Greetings, citizen. Welcome to Akanon. *whirr whirr* Mission *Terminate Vermin* initiated. Bring four clockwork rat leg sections from defective cleaners for reward. All defective models must be eradicated *bzzzt whirr*.");
+    quest::say("Greetings, citizen. Welcome to Akanon. *whirr whirr* Mission *Terminate Vermin* initiated. Bring one clockwork rat leg sections from defective cleaners for reward. All defective models must be eradicated *bzzzt whirr*.");
   }
 }
 
 sub EVENT_ITEM {
   #Handin: 4x Clockwork Rat Leg Section 
-  if(plugin::check_handin(\%itemcount, 10967 => 4)){ #End of Exterminate the Vermin(Scripted By: Fatty Beerbelly)
-    quest::say("Four Clockwork Rat Leg Sections recieved *bzzzt* processing reward. Reward mode initiated. Coins given. Mission *Terminate Vermin* reset one second *bzzt whirr*.");
+  if(plugin::check_handin(\%itemcount, 10967 => 1)){ #End of Exterminate the Vermin(Scripted By: Fatty Beerbelly)
+    quest::say("One Clockwork Rat Leg Sections recieved *bzzzt* processing reward. Reward mode initiated. Coins given. Mission *Terminate Vermin* reset one second *bzzt whirr*. Thank Reek the great gnome if you see him.");
     quest::faction(176,10); # +King Ak'Anon
     quest::faction(91,10); # +Eldritch Collective
     quest::faction(115,10); # +Gem Choppers
     quest::faction(71,-10); # -Dark Reflection
     quest::faction(209,-10); # -Meldrath
-     quest::ding(); quest::exp(5);
-    quest::givecash(1,1,1,0);
+     quest::ding(); quest::exp(1000);
+    quest::givecash(3,1,1,0);
   }
+   else{
+    $client->Message(14,"Sorry, I can't use this.");
+    plugin::return_items(\%itemcount);
+     if($platinum != 0 || $gold !=0 || $silver != 0 || $copper != 0) {
+        quest::givecash($copper, $silver, $gold, $platinum);
+     }
 } 
 
 
