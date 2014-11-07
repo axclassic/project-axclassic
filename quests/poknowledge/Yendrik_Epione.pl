faf271d752e42cbd80cc40cecf3207ed79af5b2b
Caved12@gmail.com <Caved12@gmail.com@cf3d5988-41f8-b8ae-7f76-8d792267d49b>
2014-11-07 00:00:49 +0000

    fix

--- quests/poknowledge/Yendrik_Epione.pl
+++ quests/poknowledge/Yendrik_Epione.pl
@@ -16,17 +16,17 @@ my $aatotal = $client->GetSpentAA();
     $client->Message(14,"Hello $name , I am Yendrik Epione. I am here to tell you about Tier 2 armor, it is called Rathe Armor (Tier 2) it can be found in a special instanced dungeon. You can find all the pieces there exept for the chest pieces. You wil have to bring me 3 The Rathe Server Doll (Tier 2)'s and I wil give you a random Tier 2 chestpiece, either Leather, Plate, Chain or Silk. These dolls drop in this quest dungeon to occasionaly. If you are lvl 65 and have spend 75 AA points I can $open a zone instance for you to begin your Tier 2 challenge, it will stay open for 3.5 hours. I charge 1000pp per zone instance. You can purchase a new instance after the old one has expired.");
 $client->Message(12,"In the event that you would die in your Tier 2 quest instance, I can port you $back for as long as the instance is active. In case your instance has already expired, your corpse will end up $here.");
 return 1;
-  }
+}
 
   if ($text=~/hail/i && $ulevel < 65 || $aatotal < 75) {
     $client->Message(14,"Hello $name , the Tier 2 server challenge and quest I am offering is for people who are lvl 65 and have spend in all 75 AA points as a minimum. Come back when you meet those requirements.");
 return 1;
-  }
+}
 
   if ($text=~/open/i && $ulevel >= 65 && !defined($qglobals{$name."chambersf"}) && $aatotal >= 75) {
     $client->Message(14,"Just hand me 1000pp and I will create the instance for your challenge and port you there.");
 return 1;
-  }
+}
   if ($text=~/open/i && $ulevel >= 65 && defined($qglobals{$name."chambersf"}) && $aatotal >= 75) {
     $client->Message(14,"You already have an existing Tier 2 zone instance. You can't purchase a newone until it has expired, but I can port you $back to it if you like.");
 return 1;
@@ -38,15 +38,11 @@ return 1;
      quest::MovePCInstance(309, $QGlobalValue, 0.00, 0.00, -0.21);
 return 1;
 }
-else {
- $client->Message(14,"You seem to have no Tier 2 instance.");
-}
-return 1;
-}
-if ($text=~/here/i && $ulevel >= 64 && $aatotal >= 75) {
-$client->Message(14,"Going to send you to the corpse recovery instance now. Remember there is only 1 player allowed in this instance, if there already is a player you will have to wait until this player leaves. Your corpse will end up here after your instance has expired.");
-quest::movepc(309, 0.00, 0.00, -0.21);
 
+  if ($text=~/here/i && $ulevel >= 64 && $aatotal >= 75) {
+     $client->Message(14,"Going to send you to the corpse recovery instance now. Remember there is only 1 player allowed in this instance, if there already is a player you will have to wait until this player      leaves. Your corpse will end up here after your instance has expired.");
+     quest::movepc(309, 0.00, 0.00, -0.21);
+return 1;
 
 }
 }
@@ -62,19 +58,18 @@ if (plugin::check_handin(\%itemcount, 119592 => 3)) {
 
 elsif (($platinum == 1000) && $ulevel >= 65 && !defined($qglobals{$name."chambersf"}) && $aatotal >= 75) {
     $client->Message(14,"Thank you $class , you are on the way to your Tier 2 Challenge, good luck!"); #Money handin for instance creation and porting to it.
-
     my $instanceID = quest::CreateInstance("chambersf", 0, 12800);
     quest::AssignToInstance($instanceID);
     quest::setglobal($name."chambersf",$instanceID,7,M213);
     quest::MovePCInstance(309, $instanceID, 0.00, 0.00, -0.21, 150);
 return 1;
  }
- elsif (($platinum == 1000) && $ulevel >= 65 && defined($qglobals{$name."chambersf"}) && $aatotal >= 75) {
+elsif (($platinum == 1000) && $ulevel >= 65 && defined($qglobals{$name."chambersf"}) && $aatotal >= 75) {
     $client->Message(14,"You already have a Tier 2 instance that hasn't expired yet, here is your money back $name."); #Instance exists Money back.
     quest::givecash(0,0,0,1000);
 return 1;
  }
- elsif (($platinum == 1000) && $ulevel >= 65 && !defined($qglobals{$name."chambersf"}) && $aatotal <= 74) {
+elsif (($platinum == 1000) && $ulevel >= 65 && !defined($qglobals{$name."chambersf"}) && $aatotal <= 74) {
     $client->Message(14,"You lack the AA requirement to enter Tier 2 $name."); #Instance exists Money back.
     quest::givecash(0,0,0,1000);
 return 1;
