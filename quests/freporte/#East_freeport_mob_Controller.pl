0f50b1932d0b0417ea60eefe50076b98e98de0ed
Aardil <aardil@msn.com>
2015-04-25 16:42:57 -0500

    Delete #East_freeport_mob_Controller.pl
    did not work needed to be named by ID number

--- quests/freporte/#East_freeport_mob_Controller.pl
+++
@@ -1,6 +0,0 @@
-#East_freeport_mob_Controller#
-sub EVENT_SPAWN {
-    my $a = quest::ChooseRandom(10156,10157,10160,10163,10164,10179,10186);
-  quest::spawn2($a,0,0,-855,-236,-55,-246.5);
-      quest::depop();
-  }
