c6ae3aaa24ae89353e84fe75fea44b2520f18789
Aardil <aardil@msn.com>
2015-07-14 18:25:01 -0500

    Update Kerr_Tiparr.pl

--- quests/hollowshade/Kerr_Tiparr.pl
+++ quests/hollowshade/Kerr_Tiparr.pl
@@ -1,6 +1,14 @@
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Welcome to our humble outpost. traveller. Let me know if you see anything you need. I consider it an honor to sell here. for doing so enables Vah Shir in training to stay in the field."); }
+  if($text=~/Hail/i){
+   quest::say("Welcome to our humble outpost. traveller. Let me know if you see anything you need. I consider it an honor to sell here. for doing so enables Vah Shir in training to stay in the field.");
+   }
 }
+
+sub EVENT_ITEM {
+   if(plugin::check_handin(\%itemcount,5566 => 1)){
+    quest::say("Thank you $name! That was Purrrfect!!");
+    quest::summonitem(5567);
+    }
+}    
 #END of FILE Zone:hollowshade  ID:166011 -- Kerr_Tiparr 
 
