8341a85f125f775d2954989ebdcae28113a6b3ed
patrikpatrik <axclassiceq@gmail.com>
2015-07-31 07:51:00 -0700

    color correction

--- quests/poknowledge/#Champion_Lightwater.pl
+++ quests/poknowledge/#Champion_Lightwater.pl
@@ -19,14 +19,14 @@ sub EVENT_EXIT {
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
 	quest::doanim(24);
-	$client->Message(15,"Champion Lightwater says, 'Can't you see I'm busy here? I'm coping with my losses.'");
+	$client->Message(14,"Champion Lightwater says, 'Can't you see I'm busy here? I'm coping with my losses.'");
 	$client->Message(263,"Champion Lightwater looks somewhat startled and agigated at the same time.");
 	}
 }
 sub EVENT_ITEM {
 #If king card is handed in
 	if (plugin::check_handin(\%itemcount, 22298 => 1)) {
-	$client->Message(15,"Champion Lightwater says, 'My God, you've found it! I haven't seen one of these in a long time!'");
+	$client->Message(14,"Champion Lightwater says, 'My God, you've found it! I haven't seen one of these in a long time!'");
 	$client->Message(263,"Champion Lightwater waves her hand emphatically as a random item materializes out of thin air.");	
 		my $randomroll = int(rand(100));
 		if ($randomroll < '34') {
@@ -47,7 +47,7 @@ sub EVENT_ITEM {
 			quest::summonitem(14009);
 			quest::summonitem($randombag);# big bag of platinum
 			$client->Message(263,"$name stumbles as the weight of the bag falls into your hands.");
-			$client->Message(15,"Champion Lightwater says, 'Farewell!'");
+			$client->Message(14,"Champion Lightwater says, 'Farewell!'");
 			}
 		elsif ($randomroll < '68') {
 			my @epicarray = (10650, 8495, 14341, 11050, 20490, 68299, 28034, 14383, 10651, 10652, 11057, 20488, 5532, 20542, 10908, 20544);
@@ -59,19 +59,19 @@ sub EVENT_ITEM {
 				else{
 				quest::summonitem($randomepic);
 				}
-			$client->Message(15,"Champion Lightwater says, 'Farewell!'");
+			$client->Message(14,"Champion Lightwater says, 'Farewell!'");
 		}
 		else {
 		quest::we(13, "Champion Lightwater shouts, 'Congratulations to $name for a risky proposition and came out victorious!'");
 		quest::summonitem(120216); # rathe server is 120216 test is 120199
-		$client->Message(15,"Champion Lightwater says, 'Farewell!'");
+		$client->Message(14,"Champion Lightwater says, 'Farewell!'");
 		}
 		
 	quest::ding();
 	quest::depop(202389); #rathe & test is 202389
 	}
 	else {
-	$client->Message(15,"Champion Lightwater says, 'Now what would I do with this?'");
+	$client->Message(14,"Champion Lightwater says, 'Now what would I do with this?'");
 	plugin::return_items(\%itemcount); #returns item
 	}
 }
