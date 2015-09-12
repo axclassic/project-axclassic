f147d96b12d320c07d584c05ddefc6138b13ee56
Aardil <aardil@msn.com>
2015-09-12 16:17:34 -0500

    Update 300097.pl

--- quests/wallofslaughter/300097.pl
+++ quests/wallofslaughter/300097.pl
@@ -6,7 +6,7 @@ sub EVENT_SPAWN
 {
 	$x = $npc->GetX();
 	$y = $npc->GetY();
-	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+	quest::set_proximity($x - 25, $x + 25, $y - 25, $y + 25);
 }
 
 sub EVENT_ENTER
