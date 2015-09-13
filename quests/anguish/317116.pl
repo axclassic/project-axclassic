a9d45368d13912e272825a33699a8bcaf76350ee
Aardil <aardil@msn.com>
2015-09-12 20:33:02 -0500

    Update 317116.pl

--- quests/anguish/317116.pl
+++ quests/anguish/317116.pl
@@ -8,7 +8,8 @@ sub EVENT_SPAWN
 	$y = $npc->GetY();
 	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
 }
+
 sub EVENT_ENTER
 {
-	quest::movepc(300,1322,1617.2,122.4,141.1);
+	quest::movepc(300,1322.0,1705.9,123.0);
 }
