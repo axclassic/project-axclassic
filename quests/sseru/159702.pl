3dbf2c8632d1d12e62ccfc5cd27a2b41aee6a159
Aardil <aardil@msn.com>
2015-09-12 19:30:53 -0500

    Update 159702.pl

--- quests/sseru/159702.pl
+++ quests/sseru/159702.pl
@@ -2,6 +2,7 @@ sub EVENT_SPAWN{
 $x = $npc->GetX();
 	$y = $npc->GetY();
 	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+	quest::settimer(1,1);
 	}
 sub EVENT_ENTER{
 if($ulevel <= 60){
@@ -13,6 +14,14 @@ quest::spawn2(159703,0,0,$x,$y,$z,$h);
 quest::shout("We shall meet again $name! When you are a worthy opponent!!");
 quest::depop();
 }
+}
+sub EVENT_TIMER {
+ my $x = $npc->GetX();
+ my $y = $npc->GetY();
+ if($timer == 1 && ($x < -353 || $x > -109 || $y < -549 || $y > -310)) {
+    quest::shout("No! I must not leave the time chamber! If I do, I'll age and die!");
+    $npc->GMMove(-231.464005,-432.937469,202.375946,.125);
+ }
 }
 sub EVENT_AGGRO{
 	if($ulevel <= 60){
@@ -27,4 +36,5 @@ sub EVENT_AGGRO{
 }
 sub EVENT_DEATH {
 quest::signalwith(159704,159,0);
+quest::stoptimer(1);
 }
