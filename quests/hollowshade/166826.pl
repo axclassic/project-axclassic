55a5a9173eaeb345ed898589419b982d2c82a586
Aardil <aardil@msn.com>
2016-01-16 00:01:30 -0600

    Update 166826.pl

--- quests/hollowshade/166826.pl
+++ quests/hollowshade/166826.pl
@@ -1,8 +1,13 @@
+
 my %DeathText = (
   1 => "coughs up a bit of blood. 'You'll never escape us. . .'",
-  2 => "screams as it collapses, 'Brothers and sisters, show no mercy to this outlander.'",
+  2 => "screams as it collapses.",
   3 => "staggers for a moment and then collapses."
- );
+);
+
+sub EVENT_AGGRO { 
+   quest::emote(" Brothers and sisters, show no mercy to this outlander.");
+}
 
 sub EVENT_DEATH{
 my $RandomNumber = int(rand(3)) + 1;
