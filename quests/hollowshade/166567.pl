848a61c1a3d0aaeaf48e77fb96cd7ea6bbc935e2
Aardil <aardil@msn.com>
2016-01-16 19:52:48 -0600

    Update 166567.pl

--- quests/hollowshade/166567.pl
+++ quests/hollowshade/166567.pl
@@ -1,14 +1,30 @@
+my %AggroText = (
+  1 => "Don't run. Make this easier on both of us.",
+  2 => "Don't worry, this will be quick.",
+  3 => "I've always wanted to eat someone like you.",
+  4 => "Interfere with our fishing will you? If you want to be eaten, you'll have to wait your turn!"
+);
+
 my %DeathText = (
   1 => "coughs up a bit of blood. 'You'll never escape us. . .'",
-  2 => "screams as it collapses.",
+  2 => "screams as it collapses.'",
   3 => "staggers for a moment and then collapses."
 );
 
-sub EVENT_AGGRO { 
-   quest::say("Brothers and sisters, show no mercy to this outlander.");
+sub EVENT_SPAWN {
+ $x = $npc->GetX();
+	$y = $npc->GetY();
+	quest::set_proximity($x - 20, $x + 20, $y - 20, $y + 20);
+	}
+sub EVENT_ENTER {
+    quest::say("Tribesmen, we are under siege!");
+        }
+sub EVENT_AGGRO {
+  my $RandomNumber = int(rand(4)) + 1;
+  quest::emote("$AggroText{$RandomNumber}");
 }
 
 sub EVENT_DEATH{
 my $RandomNumber = int(rand(3)) + 1;
   quest::emote("$DeathText{$RandomNumber}");
- }
+}
