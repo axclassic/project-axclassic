3d28d83a0c6463e81d2c5f33010245ad521d018a
Aardil <aardil@msn.com>
2016-01-16 21:04:04 -0600

    Update 166403.pl

--- quests/hollowshade/166403.pl
+++ quests/hollowshade/166403.pl
@@ -1,7 +1,6 @@
 my %AggroText = (
   1 => "growls as it senses the presence of an outsider.",
-  2 => "lunges forward, saliva dripping from its fanged jaw.",
-
+  2 => "sniffs at the ground, picking up the scent of a threat."
 );
 
 my %DeathText = (
@@ -9,18 +8,22 @@ my %DeathText = (
   2 => "defiantly tries to make one final swipe at you with its claws.",
   3 => "whines pitifully as an attack cuts his life short."
 );
+
 sub EVENT_SPAWN {
  $x = $npc->GetX();
 	$y = $npc->GetY();
 	quest::set_proximity($x - 20, $x + 20, $y - 20, $y + 20);
 	}
 sub EVENT_ENTER {
-   my $RandomNumber = int(rand(2)) + 1;
+    quest::emote(" lunges forward, saliva dripping from its fanged jaw.");
+        }
+sub EVENT_AGGRO {
+  my $RandomNumber = int(rand(2)) + 1;
   quest::emote("$AggroText{$RandomNumber}");
 }
 
-sub EVENT_DEATH {
-  my $RandomNumber = int(rand(3)) + 1;
+sub EVENT_DEATH{
+my $RandomNumber = int(rand(3)) + 1;
   quest::emote("$DeathText{$RandomNumber}");
   quest::signalwith(166661,350,0);
  }
