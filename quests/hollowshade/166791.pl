8a7795fb9a7018ed981fb59cb5d39328e52090c0
Aardil <aardil@msn.com>
2016-01-17 22:38:59 -0600

    Update 166791.pl

--- quests/hollowshade/166791.pl
+++ quests/hollowshade/166791.pl
@@ -3,6 +3,7 @@ sub EVENT_SPAWN {
 	$y = $npc->GetY();
 	quest::set_proximity($x - 75, $x + 75, $y - 75, $y + 75);
         quest::signalwith(166970,812,0);
+        quest::signalwith(166968,810,0);
 	}
 sub EVENT_ENTER {
     quest::shout("I see you $name! Nothing but death awaits you here!");
@@ -13,4 +14,3 @@ sub EVENT_DEATH{
 quest::signalwith(166962,607,0); #Boss_Dead_Owlbears
 quest::signalwith(166799,10,0);
 }
-
