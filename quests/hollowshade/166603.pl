95a13358a8b2b0a62c4ffcd697a250df417fb891
Aardil <aardil@msn.com>
2016-01-18 00:21:38 -0600

    Update 166603.pl

--- quests/hollowshade/166603.pl
+++ quests/hollowshade/166603.pl
@@ -2,6 +2,8 @@ sub EVENT_SPAWN {
  $x = $npc->GetX();
 	$y = $npc->GetY();
 	quest::set_proximity($x - 75, $x + 75, $y - 75, $y + 75);
+	quest::spawn2(166970,0,0,-3028.7,636.2,73.2,97.9);
+	quest:;spawn2(166969,0,0,-3028.7,621.7,72.7,91.2);
 	quest::signalwith(166970,812,0);
         quest::signalwith(166969,811,0);
 	}
