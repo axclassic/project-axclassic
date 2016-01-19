f914eccea76eb65e9e9622931547440cc7658e77
Aardil <aardil@msn.com>
2016-01-19 16:32:57 -0600

    Update 166410.pl

--- quests/hollowshade/166410.pl
+++ quests/hollowshade/166410.pl
@@ -1,11 +1,7 @@
 sub EVENT_SPAWN{
-  quest::depop(166972);
-  quest::depop(166973);
-  quest::depop(166971);
-  quest::spawn2(166971,0,0,1721.8,3552.1,188.9,90);
-  quest::spawn2(166973,0,0,1727,3583.3,158.4,91.4);
    quest::signalwith(166971,816,0);
   quest::signalwith(166972,818,0);
+  quest::signalwith(166985,1036,0);
 }
 sub EVENT_DEATH{
 quest::signalwith(166582,408,250000);
