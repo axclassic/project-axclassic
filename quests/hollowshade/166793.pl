b26e99901d3dc552ee89977bc2bb1860a80b87a5
Aardil <aardil@msn.com>
2016-01-19 21:55:15 -0600

    Update 166793.pl

--- quests/hollowshade/166793.pl
+++ quests/hollowshade/166793.pl
@@ -1,10 +1,7 @@
 sub EVENT_SPAWN {
  $x = $npc->GetX();
 	$y = $npc->GetY();
-	quest::set_proximity($x - 75, $x + 75, $y - 75, $y + 75);
-	}
-sub EVENT_ENTER {
-    quest::shout("I see you $name! Nothing but death awaits you here!");
+  quest::set_proximity($x - 75, $x + 75, $y - 75, $y + 75);
   quest::depop(166972);
   quest::depop(166973);
   quest::depop(166971);
@@ -13,7 +10,10 @@ sub EVENT_ENTER {
   quest::signalwith(166973,817,0);
   quest::signalwith(166972,818,0);
   quest::signalwith(166582,2001,2500);
-  quest::signalwith(166963,305,0);
+	}
+sub EVENT_ENTER {
+    quest::shout("I see you $name! Nothing but death awaits you here!");
+    quest::signalwith(166963,305,0);
   }
 
 sub EVENT_DEATH{
