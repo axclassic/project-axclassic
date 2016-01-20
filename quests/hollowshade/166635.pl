1de26b8e7cfc04e1e813acd31b2a5959edb4c428
Aardil <aardil@msn.com>
2016-01-20 12:38:43 -0600

    Update 166635.pl

--- quests/hollowshade/166635.pl
+++ quests/hollowshade/166635.pl
@@ -1,6 +1,7 @@
 sub EVENT_SPAWN{
-  quest::signalwith(166970,812,0);
   quest::signalwith(166969,811,0);
+  quest::signalwith(166970,812,0);
+  quest::signalwith(166988,4039,0);
 }
 sub EVENT_DEATH{
 quest::signalwith(166582,445,250000);
