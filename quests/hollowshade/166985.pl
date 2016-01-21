92683686b4feeb68d9f670efc92a4962a2053565
Aardil <aardil@msn.com>
2016-01-20 23:08:56 -0600

    Update 166985.pl

--- quests/hollowshade/166985.pl
+++ quests/hollowshade/166985.pl
@@ -1,7 +1,7 @@
 ##North Cave Delay respawn check against Owlbears##
 sub EVENT_SIGNAL{
 if ($signal == 1001) {
-   quest::settimer("delay_owlbears_north_cave_timer",180);
+   quest::settimer("delay_owlbears_north_cave_timer",300);
   }
 if($signal == 1002){
    quest::depop(166413);
