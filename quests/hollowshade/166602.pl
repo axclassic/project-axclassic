deb5d36d77778d6af55500e44bd870701129ff75
Aardil <aardil@msn.com>
2016-01-22 00:46:20 -0600

    Update 166602.pl

--- quests/hollowshade/166602.pl
+++ quests/hollowshade/166602.pl
@@ -6,9 +6,9 @@ sub EVENT_SPAWN {
   quest::depop(166969);
   quest::depop(166970);
   quest::spawn2(166969,0,0,-3027.7,510.1,72,61.9);
-  quest::spawn2(166970,0,0,-3027.6,502,72,52.4);
+  quest::spawn2(166968,0,0,-3027.6,502,72,52.4);
   quest::signalwith(166969,811,0);
-  quest::signalwith(166970,812,0);
+  quest::signalwith(166968,812,0);
   quest::signalwith(166582,623,25000);
 	}
 sub EVENT_ENTER {
