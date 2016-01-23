f406c40f4f7b0d5e9fc9ea5271fff9cac95e0afa
Aardil <aardil@msn.com>
2016-01-22 21:51:17 -0600

    Create 166597.pl

---
+++ quests/hollowshade/166597.pl
@@ -0,0 +1,112 @@
+##Signal for what attackers and what defenders to spawn##
+sub EVENT_SIGNAL{
+if ($signal == 1001){
+  quest::depop(166487);
+  quest::depop(166585);
+  quest::depop(166586);
+  quest::depop(166587);
+  quest::depop(166588);
+   quest::spawn2(166311,0,0,1186.9,2198.8,178.2,189.8); #owlbear defender
+   quest::spawn2(166311,0,0,1187.5,2177,173.8,191.5); #owlbear defender
+   quest::spawn2(166311,0,0,1190,2159.2,171,185.6); #owlbear defender
+   quest::spawn2(166311,0,0,1195,2139.7,169.7,191.6); #owlbear defender
+   quest::spawn2(166311,0,0,1194.6,2117.5,167.9,194.8); #owlbear defender
+  quest::spawn2(166406,498123,0,-2676.5,720.7,39.7,54.8); #a sonic wolf fiend
+  quest::spawn2(166406,498123,0,-2674.6,712.5,39.7,51.6); #a sonic wolf fiend
+  quest::spawn2(166406,498123,0,-2671.9,706.1,39.7,49.8); #a sonic wolf fiend
+  quest::spawn2(166406,498123,0,-2669.6,699.4,39.7,47.6); #a sonic wolf fiend
+  quest::spawn2(166406,498123,0,-2666.9,691.7,39.7,50.4); #a sonic wolf fiend
+   quest::depop();
+   }
+if ($signal == 1002){
+   quest::depop(166487);
+   quest::depop(166585);
+   quest::depop(166586);
+   quest::depop(166587);
+   quest::depop(166588);
+   quest::depopall(166311);
+   quest::spawn2(166574,498121,0,-748.1,-2525.6,-30.2,251.5); #gremling_invader
+   quest::spawn2(166574,498121,0,-760,-2527.9,-29.3,0.9); #gremling_invader
+   quest::spawn2(166574,498121,0,-772.8,-2531.9,-27.4,249.8); #gremling_invader
+   quest::spawn2(166574,498121,0,-784,-2536.9,-26.2,248.1); #gremling_invader
+   quest::spawn2(166574,498121,0,-793.4,-2541.9,-24.7,241.6); #gremling_invader
+   quest::spawn2(166575,0,0,1186.9,2198.8,178.2,189.8); #owlbear defender
+   quest::spawn2(166575,0,0,1187.5,2177,173.8,191.5); #owlbear defender
+   quest::spawn2(166575,0,0,1190,2159.2,171,185.6); #owlbear defender
+   quest::spawn2(166575,0,0,1195,2139.7,169.7,191.6); #owlbear defender
+   quest::spawn2(166575,0,0,1194.6,2117.5,167.9,194.8); #owlbear defender
+   quest::depop();
+  }
+if ($signal == 1003){
+   quest::depop(166650);
+   quest::depop(166651);
+   quest::depop(166652);
+   quest::depop(166653);
+   quest::depop(166654);
+   quest::spawn2(166139,498120,0,1072.7,2206,160.7,134.2); #a furious owlbear
+   quest::spawn2(166139,498120,0,1060.2,2204.5,160.3,128); #a furious owlbear
+   quest::spawn2(166139,498120,0,1047.6,2204.2,160.9,128.6); #a furious owlbear
+   quest::spawn2(166139,498120,0,1035.1,2203.4,161,130.1); #a furious owlbear
+   quest::spawn2(166139,498120,0,1023.7,2203.4,161.1,129.9); #a furious owlbear
+   quest::spawn2(166403,0,0,-2741.9,587.7,48.4,48.8); #a sonic defender
+   quest::spawn2(166403,0,0,-2741.8,572.1,48.4,51); #a sonic defender
+   quest::spawn2(166403,0,0,-2738.8,557,48.1,32.1); #a sonic defender
+   quest::spawn2(166403,0,0,-2743.9,536.6,48,77); #a sonic defender
+   quest::spawn2(166403,0,0,-2741.7,520.5,47.6,79.8); #a sonic defender
+   quest::depop(); 
+  }
+if ($signal == 1004){
+   quest::depop(166650);
+   quest::depop(166651);
+   quest::depop(166652);
+   quest::depop(166653);
+   quest::depop(166654);
+   quest::spawn2(166256,498122,0,-748.1,-2525.6,-30.2,251.5); #gremling_invader
+   quest::spawn2(166256,498122,0,-760,-2527.9,-29.3,0.9); #gremling_invader
+   quest::spawn2(166256,498122,0,-772.8,-2531.9,-27.4,249.8); #gremling_invader
+   quest::spawn2(166256,498122,0,-784,-2536.9,-26.2,248.1); #gremling_invader
+   quest::spawn2(166256,498122,0,-793.4,-2541.9,-24.7,241.6); #gremling_invader
+   quest::spawn2(166668,0,0,-2741.9,587.7,48.4,48.8); #a sonic defender
+   quest::spawn2(166668,0,0,-2741.8,572.1,48.4,51); #a sonic defender
+   quest::spawn2(166668,0,0,-2738.8,557,48.1,32.1); #a sonic defender
+   quest::spawn2(166668,0,0,-2743.9,536.6,48,77); #a sonic defender
+   quest::spawn2(166668,0,0,-2741.7,520.5,47.6,79.8); #a sonic defender
+   quest::depop(); 
+  }
+if ($signal == 1005){
+   quest::depop(166567);
+   quest::depop(166568);
+   quest::depop(166569);
+   quest::depop(166570);
+   quest::depop(166571);
+   quest::spawn2(166827,498119,0,-748.1,-2525.6,-30.2,251.5); #a furious owlbear
+   quest::spawn2(166827,498119,0,-760,-2527.9,-29.3,0.9); #a furious owlbear
+   quest::spawn2(166827,498119,0,-772.8,-2531.9,-27.4,249.8); #a furious owlbear
+   quest::spawn2(166827,498119,0,-784,-2536.9,-26.2,248.1); #a furious owlbear
+   quest::spawn2(166827,498119,0,-793.4,-2541.9,-24.7,241.6); #a furious owlbear
+   quest::spawn2(166826,0,0,-668.7,-2622.6,11.8,229.4); #a grimling_defender
+   quest::spawn2(166826,0,0,-659.2,-2614.8,12,230.5); #a grimling_defender
+   quest::spawn2(166826,0,0,-645.9,-2603.7,11.8,237); #a grimling_defender
+   quest::spawn2(166826,0,0,-636.8,-2596.4,9.6,245.1); #a grimling_defender
+   quest::spawn2(166826,0,0,-618.5,-2583.2,5.3,242.4); #a grimling_defender
+   quest::depop(); 
+  }
+if ($signal == 1006){
+   quest::depop(166567);
+   quest::depop(166568);
+   quest::depop(166569);
+   quest::depop(166570);
+   quest::depop(166571);
+   quest::spawn2(166298,498124,0,-2676.5,720.7,39.7,54.8); #a sonic wolf fiend
+   quest::spawn2(166298,498124,0,-2674.6,712.5,39.7,51.6); #a sonic wolf fiend
+   quest::spawn2(166298,498124,0,-2671.9,706.1,39.7,49.8); #a sonic wolf fiend
+   quest::spawn2(166298,498124,0,-2669.6,699.4,39.7,47.6); #a sonic wolf fiend
+   quest::spawn2(166298,498124,0,-2666.9,691.7,39.7,50.4); #a sonic wolf fiend
+   quest::spawn2(166565,0,0,-668.7,-2622.6,11.8,229.4); #a grimling_defender
+   quest::spawn2(166565,0,0,-659.2,-2614.8,12,230.5); #a grimling_defender
+   quest::spawn2(166565,0,0,-645.9,-2603.7,11.8,237); #a grimling_defender
+   quest::spawn2(166565,0,0,-636.8,-2596.4,9.6,245.1); #a grimling_defender
+   quest::spawn2(166565,0,0,-618.5,-2583.2,5.3,242.4); #a grimling_defender
+   quest::depop(); 
+  }
+}
