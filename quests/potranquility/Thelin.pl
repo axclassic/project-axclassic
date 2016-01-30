0c33192e811fc785cb1efd5368a126c0484eab6f
Aardil <aardil@msn.com>
2016-01-30 02:36:27 -0600

    Delete Thelin.pl

--- quests/potranquility/Thelin.pl
+++
@@ -1,9 +0,0 @@
-sub EVENT_SPAWN {
-   $npc->SetAppearance(3);#sleeping
-}
-sub EVENT_SIGNAL{
-   if($signal == 1501){
-   quest::emote(" tosses and turns violently on the floor, then is still.");
-  }
- } 
-
