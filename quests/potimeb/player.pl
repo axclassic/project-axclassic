ca3dfc9ff0667b5781a04be7c8ea170f83e5e953
patrikpatrik <axclassiceq@gmail.com>
2016-06-07 14:42:16 -0700

    Clean-up debug removed

--- quests/potimeb/player.pl
+++ quests/potimeb/player.pl
@@ -53,7 +53,7 @@ sub EVENT_ZONE{
 	  #This happens when a player doesnt retrive his corpse and dies again so no corpse is moved and nothing is deleted, could be buggy!
 	  #updated to move to here when you zone in and someone is still here. you need to go.
 	  #quest::ze(15, "You left but you had a reco which means you quit while in corpse recovery.");
-	  quest::ze(15, "You left because either you logged in this zone and someone was here or tried to leave and come back later to continue, thats a no no.");
+	  #quest::ze(15, "You left because either you logged in this zone and someone was here or tried to leave and come back later to continue, thats a no no.");
 	  #quest::delglobal($name."pobcorpse");
 	  quest::delglobal("outout");
 	}
@@ -66,10 +66,10 @@ sub EVENT_ENTERZONE { #Upon entering zone through portals it will spawn mobs in 
 		quest::ze(15, "a GM has entered the zone.");
 		#quest::delglobal("blockout");
 	} elsif ((defined $qglobals{"blockout"}) && (defined $qglobals{$name."reco"})) {
-		quest::ze(15, "Was sent by Udunir, retrieving corpse."); #corpse run so bypass qglobals and youll be in graveyard
+		#quest::ze(15, "Was sent by Udunir, retrieving corpse."); #corpse run so bypass qglobals and youll be in graveyard
 		quest::setglobal($name."reco2", 1, 7, "H20"); #20 hours
 	} elsif ((defined $qglobals{"blockout"}) && (defined $globals{$name."reco2"}) || (defined $globals{$name."reco"})) {
-		quest::ze(15, "you must have logged out from a corpse run so bypass qglobals and no depop!");
+		#quest::ze(15, "you must have logged out from a corpse run so bypass qglobals and no depop!");
 	
 		#quest::ze(15, "successful entering zone. blockout qglobal DISABLED!");
 	} elsif((defined $qglobals{"blockout"}) && (defined $qglobals{"blockout2"})) { #The blockout2 is an extra check involved for when someone disconnects in the zone.
@@ -92,12 +92,12 @@ sub EVENT_ENTERZONE { #Upon entering zone through portals it will spawn mobs in 
 			#quest::signalwith(223211,23,0); #send signal to main trigger to start hour time limit
 	} elsif ((defined $qglobals{"blockout"}) && (!defined $qglobals{"blockout2"}) && (!defined $qglobals{$name."reco"}) && (!defined $qglobals{$name."reco2"})) {
 		quest::setglobal("outout", 1, 7, "F"); #triggers outout
-		quest::ze(15, "This means you came here when someone else is here so you must be zoned out asap OR you logged out in this zone and must be zoned out.");
+		#quest::ze(15, "This means you came here when someone else is here so you must be zoned out asap OR you logged out in this zone and must be zoned out.");
 		
 		quest::ze(13, "An unknown voice shouts, 'You do not belong here, begone!'");
 		quest::movepc(9, 335, 181, -25); #WC default 
 	} else {
-			quest::ze(15, "You entered and came across the else function. You must have quit and come back when no ones here.");
+			#quest::ze(15, "You entered and came across the else function. You must have quit and come back when no ones here.");
 			quest::repopzone(); #no ones in here? just reset anyway
 			#$client->Message(15, "corpse fetching and or gm spectating my status is $status.");
 		#When qglobals blockout is not defined nothing happens i.e corpse fetching in graveyard or gm intervention.
