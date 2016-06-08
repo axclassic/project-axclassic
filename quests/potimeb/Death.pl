b53300d65ca38cda5cc40fae78bfc89d97700a0c
patrikpatrik <axclassiceq@gmail.com>
2016-06-08 09:43:40 -0700

    update pl

--- quests/potimeb/Death.pl
+++ quests/potimeb/Death.pl
@@ -9,10 +9,48 @@ if($status == 255){
 	quest::doanim(15);
 	}
 }
-
 if($text=~/hail/i) {
 	quest::say("Yes?");
 	quest::doanim(5);
 	}
 
 }#END sub EVENT_SAY
+
+sub EVENT_WAYPOINT {
+	if ($wp == 2) {
+		my $rolls = int(rand(200));
+		if($rolls =< 2) {
+		quest::emote("pats his pockets and says, 'Hmm, now where did I put my keys?'");
+		}
+	}
+	if ($wp == 4) {
+	my $rolls = int(rand(200));
+		if($rolls =< 2) {
+		quest::emote("pats his pockets and says, 'Hmm, now where did I put my keys?'");
+		}
+	} 
+	if ($wp == 6) {
+	my $rolls = int(rand(200));
+		if($rolls =< 2) {
+		quest::emote("pats his pockets and says, 'Hmm, now where did I put my keys?'");
+		}
+	}
+	if ($wp == 8) {
+	my $rolls = int(rand(200));
+		if($rolls =< 2) {
+		quest::emote("pats his pockets and says, 'Hmm, now where did I put my keys?'");
+		}
+	}
+	if ($wp == 10) {
+	my $rolls = int(rand(200));
+		if($rolls =< 2) {
+		quest::emote("pats his pockets and says, 'Hmm, now where did I put my keys?'");
+		}
+	}
+	if ($wp == 12) {
+	my $rolls = int(rand(200));
+		if($rolls =< 2) {
+		quest::emote("pats his pockets and says, 'Hmm, now where did I put my keys?'");
+		}
+	}
+}#END sub EVENT_WAYPOINT
