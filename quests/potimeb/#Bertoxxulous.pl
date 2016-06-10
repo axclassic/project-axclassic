38dc69e8ad358e858b7e1202b9fb0af1e7f829c6
Aardil <aardil@msn.com>
2016-06-09 20:32:10 -0500

    Update #Bertoxxulous.pl

--- quests/potimeb/#Bertoxxulous.pl
+++ quests/potimeb/#Bertoxxulous.pl
@@ -6,9 +6,9 @@ sub EVENT_DEATH {
         quest::signalwith(223158,13010,0);
         quest::say("Defeat by mortals. . . impossible. The power of decay still beckons for you. . . someday. . .");
 }
-
-sub EVENT_SIGNAL {
+}
+#sub EVENT_SIGNAL {
   # if ($signal == 66){
      # quest::depop();
 # }
- }
+# }
