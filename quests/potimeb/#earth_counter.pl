f0b0f620762f05a81f18858efdb8ec16a0672aad
patrikpatrik <axclassiceq@gmail.com>
2016-06-10 14:18:08 -0700

    2nd revision.

--- quests/potimeb/#earth_counter.pl
+++ quests/potimeb/#earth_counter.pl
@@ -9,7 +9,7 @@ sub EVENT_SPAWN { #This spawns 1st mob group for Phase 2
 	quest::spawn2(223102,0,0,242.0,1644.0,491.5,192.5);
 	quest::spawn2(223133,0,0,252.0,1649.0,491.3,192.5);
 	quest::spawn2(223125,0,0,252.0,1639.0,491.0,192.5);
-	#quest::spawn2(233133,0,0,232.0,1639.0,491.6,192.5); #This is the problem! try 133
+	quest::spawn2(223133,0,0,232.0,1639.0,491.6,192.5);
 	quest::spawn2(223110,0,0,242.0,1634.0,491.2,192.5);
 	quest::spawn2(223125,0,0,252.0,1629.0,491.0,192.5);
 }
@@ -19,7 +19,7 @@ sub EVENT_SIGNAL { #like earth_event.pl starts a counter for when all are dead, 
 		$earthc = $enpccounter;
 		#quest::ze(15, "okay i signaled and my counter is now $earthc.");
 	}	
-	if ($enpccounter == 8) { # phase_trigger script npcid - 223191
+	if ($enpccounter == 9) { # phase_trigger script npcid - 223191
 		quest::signalwith(223191, 14034, 0);
 		$enpccounter = 0;
 		quest::depop();
--- quests/potimeb/#earth_trigger.pl
+++ quests/potimeb/#earth_trigger.pl
@@ -3,7 +3,13 @@
 
 sub EVENT_SPAWN { #This spawns 2nd grp + boss for phase 2
     quest::spawn2(223110,0,0,252.0,1659.0,491.0,192.5);
+<<<<<<< .mine
+	quest::spawn2(223125,0,0,242.0,1654.0,491.6,192.5);
+||||||| .r4282
+	quest::spawn2(233125,0,0,242.0,1654.0,491.6,192.5); #Doesnt spawn for some reason
+=======
 	quest::spawn2(223125,0,0,242.0,1654.0,491.6,192.5); 
+>>>>>>> .r4299
 	quest::spawn2(223102,0,0,232.0,1649.0,491.9,192.5);
 	quest::spawn2(223125,0,0,252.0,1649.0,491.3,192.5);
 	quest::spawn2(223125,0,0,242.0,1644.0,491.5,192.5);
@@ -20,7 +26,7 @@ sub EVENT_SIGNAL { #like earth_counter.pl starts a counter for when all are dead
 		$earthc = $enpccounter;
 		#quest::ze(15, "okay i signaled and my counter is now $earthc.");
 	}	
-	if ($enpccounter == 9) { # phase_trigger script npcid - 223191
+	if ($enpccounter == 10) { # phase_trigger script npcid - 223191
 		quest::signalwith(223191, 14035, 0);
 		$enpccounter = 0;
 		quest::depop();
