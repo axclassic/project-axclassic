00b11b73eec22bfd1ccdeda9b46dd1db0bbd0672
patrikpatrik <axclassiceq@gmail.com>
2016-06-11 15:43:01 -0700

    Revision 3.

--- quests/potimeb/player.pl
+++ quests/potimeb/player.pl
@@ -31,10 +31,12 @@ sub EVENT_ZONE{
 			quest::delglobal("portal3"); #remember to put this in so it deletes upon dying, zoning etc
 			quest::delglobal("portal4");
 			quest::delglobal("portal5");
-			quest::depopzone(1); #improved reset
-			quest::repopzone();
+			quest::stoptimer("opendoors");
+			quest::stoptimer("twoopens");
 			quest::delglobal("blockout");
 			quest::delglobal($name."reco");
+			quest::depopzone(1); #improved reset
+			quest::repopzone();
 			#quest::stoptimer($timer);
 			#fetch corpse
 			@corpse = $entity_list->GetCorpseList();
@@ -74,6 +76,8 @@ sub EVENT_ENTERZONE { #Upon entering zone through portals it will spawn mobs in 
 	
 		#quest::ze(15, "successful entering zone. blockout qglobal DISABLED!");
 	} elsif((defined $qglobals{"blockout"}) && (defined $qglobals{"blockout2"})) { #The blockout2 is an extra check involved for when someone disconnects in the zone.
+			quest::stoptimer("opendoors");
+			quest::stoptimer("twoopens");
 			quest::depopzone(1); #improved reset
 			quest::repopzone();
 			#$client->Message(15, "blockout is here so spawns should pop.");
