b46088b2d7598b77bf819375c22e56616381f307
patrikpatrik <axclassiceq@gmail.com>
2016-07-14 15:50:28 -0700

    Correct depopping script left in.

---
+++ quests/karnor/#The_Fabled_Skeletal_Captain.pl
@@ -0,0 +1,27 @@
+# #The_Fabled_Skeletal_Captain (102131)
+
+
+sub EVENT_SPAWN{
+	$x = $npc->GetX();
+	$y = $npc->GetY();
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+}
+
+sub EVENT_ENTER{
+	if($ulevel <= 40){
+		quest::shout("We shall meet again $name! When you are a worthy opponent!!");
+
+		quest::depop();
+	}
+}
+
+sub EVENT_AGGRO{
+	if($ulevel <= 40){
+		quest::shout("We shall meet again $name! When you are a worthy opponent!!");
+
+		quest::depop();
+	}
+}
+sub EVENT_DEATH {
+
+}
---
+++ quests/karnor/#The_Fabled_Venril_Sathir.pl
@@ -0,0 +1,26 @@
+# #The_Fabled_Venril_Sathir (102133)
+
+sub EVENT_SPAWN{
+	$x = $npc->GetX();
+	$y = $npc->GetY();
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+}
+
+sub EVENT_ENTER{
+	if($ulevel <= 40){
+		quest::shout("We shall meet again $name! When you are a worthy opponent!!");
+
+		quest::depop();
+	}
+}
+
+sub EVENT_AGGRO{
+	if($ulevel <= 40){
+		quest::shout("We shall meet again $name! When you are a worthy opponent!!");
+
+		quest::depop();
+	}
+}
+sub EVENT_DEATH {
+
+}
