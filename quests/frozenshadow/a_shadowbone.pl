64d7bd562a3260ff7fe634371b288a9e15434dae
Angelox <axclassic@users.noreply.github.com>
2016-10-27 20:38:27 -0400

    Increased spawn on death rate on first floor

--- quests/frozenshadow/a_shadowbone.pl
+++ quests/frozenshadow/a_shadowbone.pl
@@ -6,7 +6,7 @@ quest::say("Areeeeewwwww"); }
 #Angelox
 sub EVENT_DEATH {
 	$gnoll = int(rand(100));
-	if ($gnoll <= 9) {
+	if ($gnoll <= 18) {
 		$spawngnoll = quest::spawn2(111158,0,0,$x,$y,$z,0);
 		$attack = $entity_list->GetMobID($spawngnoll);
 		$gnollattack = $attack->CastToNPC();
