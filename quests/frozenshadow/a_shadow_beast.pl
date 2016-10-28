e8019d51932dd0c7227c7bd4915e226c0bda23ba
Angelox <axclassic@users.noreply.github.com>
2016-10-28 19:06:19 -0400

    Re adjustment

--- quests/frozenshadow/a_shadow_beast.pl
+++ quests/frozenshadow/a_shadow_beast.pl
@@ -3,7 +3,7 @@
 #Angelox
 sub EVENT_DEATH {
 	$gnoll = int(rand(100));
-	if ($gnoll <= 18) {
+	if ($gnoll <= 10) {
 		$spawngnoll = quest::spawn2(111158,0,0,$x,$y,$z,0);
 		$attack = $entity_list->GetMobID($spawngnoll);
 		$gnollattack = $attack->CastToNPC();
