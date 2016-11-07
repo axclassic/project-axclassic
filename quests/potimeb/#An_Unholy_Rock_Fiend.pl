ec0ba977ae68a8feba0f66c5b230f6e62e567d6f
patrikpatrik <axclassiceq@gmail.com>
2016-11-07 09:10:43 -0800

    Syntax fix.

--- quests/potimeb/#An_Unholy_Rock_Fiend.pl
+++ quests/potimeb/#An_Unholy_Rock_Fiend.pl
@@ -5,7 +5,7 @@ sub EVENT_DEATH {
   quest::signalwith(223178,14028,0); #Triggers earth_counter.pl
   quest::signalwith(223169,14035,0); #Triggers earth_trigger.pl
 }
-}
+
 #sub EVENT_SIGNAL {
   # if ($signal == 66){
      # quest::depop();
