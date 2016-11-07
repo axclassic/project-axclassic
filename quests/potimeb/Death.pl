6a6482bf5eee03181e7f9e8e37f7050cb001c503
patrikpatrik <axclassiceq@gmail.com>
2016-11-07 09:07:58 -0800

    Syntax fix.

--- quests/potimeb/Death.pl
+++ quests/potimeb/Death.pl
@@ -23,38 +23,38 @@ if($text=~/hail/i) {
 
 sub EVENT_WAYPOINT {
 	if ($wp == 2) {
-		my $rolls = int(rand(200));
-		if($rolls =< 2) {
+	my $rolls = int(rand(200));
+		if($rolls <= 2) {
 		quest::emote("pats his pockets and says, 'Hmm, now where did I put my keys?'");
 		}
 	}
 	if ($wp == 4) {
 	my $rolls = int(rand(200));
-		if($rolls =< 2) {
+		if($rolls <= 2) {
 		quest::emote("pats his pockets and says, 'Hmm, now where did I put my keys?'");
 		}
 	} 
 	if ($wp == 6) {
 	my $rolls = int(rand(200));
-		if($rolls =< 2) {
+		if($rolls <= 2) {
 		quest::emote("pats his pockets and says, 'Hmm, now where did I put my keys?'");
 		}
 	}
 	if ($wp == 8) {
 	my $rolls = int(rand(200));
-		if($rolls =< 2) {
+		if($rolls <= 2) {
 		quest::emote("pats his pockets and says, 'Hmm, now where did I put my keys?'");
 		}
 	}
 	if ($wp == 10) {
 	my $rolls = int(rand(200));
-		if($rolls =< 2) {
+		if($rolls <= 2) {
 		quest::emote("pats his pockets and says, 'Hmm, now where did I put my keys?'");
 		}
 	}
 	if ($wp == 12) {
 	my $rolls = int(rand(200));
-		if($rolls =< 2) {
+		if($rolls <= 2) {
 		quest::emote("pats his pockets and says, 'Hmm, now where did I put my keys?'");
 		}
 	}
