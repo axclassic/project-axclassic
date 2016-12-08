a1d8a9f6bbfae0ba9ea32b39f18edd38f65b40a9
patrikpatrik <axclassiceq@gmail.com>
2016-12-08 10:53:56 -0800

    testing server wide messages upon rare loots.

---
+++ quests/karnor/player.pl
@@ -0,0 +1,16 @@
+#####################################
+## Server-wide messages upon rare loots
+## Karnor's Castle
+#####################################
+
+sub EVENT_LOOT{
+	$templ = $looted_id;
+	if($templ == 82731) { #Fabled_Jade_mace
+		$item = quest::varlink(82731);
+	} elsif ($templ == 82734) { #Fabled_Tranquil staff
+		$item = quest::varlink(82734);
+	} elsif ($templ == 6639) { # Tranquil staff
+		$item = quest::varlink(6639);
+	}
+		quest::we(14, "$name, the $class has just looted the -$item-!");
+}
