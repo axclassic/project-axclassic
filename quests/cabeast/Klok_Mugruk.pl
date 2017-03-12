82f7dcbc28fee795a0b299f19f05fe14718f4753
patrikpatrik <axclassiceq@gmail.com>
2017-03-11 23:11:43 -0800

    addition of sparring armor quest. associated with Master Bain.

--- quests/cabeast/Klok_Mugruk.pl
+++ quests/cabeast/Klok_Mugruk.pl
@@ -1,23 +1,44 @@
+#
+# Quest update: 3/10/2017
+# Addition of Monk Training Bag
+# associated with Master Bain sparring armor quest
+# by patrikpatrik
+
 my $bristle = quest::saylink("bristle", 1);
 my $armor = quest::saylink("armor", 1);
 my $service = quest::saylink("service", 1);
 my $perform = quest::saylink("perform", 1);
 sub EVENT_SAY { 
   if($text=~/Hail/i){
-    quest::say("Welcome to my shop. Only the finessst I have. I make for you jussst as I make for the emperor's harem. Many $bristle silk garmentsss do they need. Ssss.. Lucky man, the emperor isss.");
+    quest::say("Welcome to my shop. Only the finessst I have. I make for you jussst as I 
+	make for the emperor's harem. Many $bristle silk garmentsss do they need. Ssss.. Lucky 
+	man, the emperor isss.");
   }
   if($text=~/bristle/i){
-    quest::say("What is bristle silk?!! Huh? Where have you been living? Under the rocksss.. Nahh. I sssee. You are a broodling from the common ssstock. You could never afford bristle silk nor the affectionss of those who drape themselves with it. Still, commons have much ssservice to offer. Maybe you would like to earn some curscale $armor. Hmm?");
+    quest::say("What is bristle silk?!! Huh? Where have you been living? Under the rocksss.. 
+	Nahh. I sssee. You are a broodling from the common ssstock. You could never afford bristle 
+	silk nor the affectionss of those who drape themselves with it. Still, commons have much 
+	ssservice to offer. Maybe you would like to earn some curscale $armor. Hmm?");
   }
   if($text=~/armor/i){
-    quest::say("Curscale armor? I make thisss crude armor from the broodlings of the scaled wolves. I use the ssskins of the pupsss and cubs. Those ssskins are ssstill young and thin and are bessst for the young adventurers. I sssupply them to the Legion of Cabilisss, but for a small $service I shall make them available to you.");
+    quest::say("Curscale armor? I make thisss crude armor from the broodlings of the scaled 
+	wolves. I use the ssskins of the pupsss and cubs. Those ssskins are ssstill young and 
+	thin and are bessst for the young adventurers. I sssupply them to the Legion of Cabilisss, 
+	but for a small $service I shall make them available to you.");
   }
   if($text=~/service/i){
-    quest::say("I am in need of pup and cub scaled wolf skins to create more curscale armor. Many Legionnaires have died in battle. The Baron demands more sssuits. I mussst have more ssskins or it is MY HIDE!! I shall reward pieces of the curscale armor to any who $perform a small service.");
+    quest::say("I am in need of pup and cub scaled wolf skins to create more curscale armor.
+	Many Legionnaires have died in battle. The Baron demands more sssuits. I mussst have more 
+	ssskins or it is MY HIDE!! I shall reward pieces of the curscale armor to any who $perform 
+	a small service.");
   }
   #Gives the empty curscale pack.
   if($text=~/perform/i){
-    quest::say("Good. You take thisss pack and fill it all with scaled curskins. Only the pupsss and cubs of the scaled wolves shall do. They are not much for one sssuch as you. Combine all those ssskins and return the full curskin pack and I shall reward you with any available piece of curskin armor and a sssmall amount of coin. Mossst important is that you shall be proving your allegiance to all of Cabilisss.");
+    quest::say("Good. You take thisss pack and fill it all with scaled curskins. Only the 
+	pupsss and cubs of the scaled wolves shall do. They are not much for one sssuch as you. 
+	Combine all those ssskins and return the full curskin pack and I shall reward you with 
+	any available piece of curskin armor and a sssmall amount of coin. Mossst important is 
+	that you shall be proving your allegiance to all of Cabilisss.");
     quest::summonitem(17992);
   }
 }
@@ -40,6 +61,10 @@ sub EVENT_ITEM {
     quest::faction(193,10);
      quest::ding(); quest::exp(10000);
     
+  } elsif(plugin::check_handin(\%itemcount,12685=>1)) { #accepts Full Pouch of Leech Husks
+	quest::say("Excellent, here is your training bag husk. Combine three pile of granite pebbles
+	to tailor a monk training bag for your caste. Return this back to Master Bain when you're done.");
+	quest::summonitem(12687); #Training Bag Husk
   }
   else {
   qust::say("I have no need for this.. Take it back!");
