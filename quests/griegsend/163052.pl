fa6a1fc619d0c2bea7a8496b20de4d344cbd9507
patrikpatrik <axclassiceq@gmail.com>
2017-03-19 02:10:37 -0700

    update info npc name & npcid

--- quests/griegsend/163052.pl
+++ quests/griegsend/163052.pl
@@ -1,3 +1,5 @@
+# an_ancient_necromantic_shade (163052)
+
 sub EVENT_DEATH {
-   quest::spawn2(163056,0,0,1784.2,-626.3,-74,85.8);
+   quest::spawn2(163056,0,0,1784.2,-626.3,-74,85.8); # a_chest (163056)
    }
