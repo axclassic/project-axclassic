aef69eb4d5ab6e6cdf0b36e9057cb3652b78499c
Aardil <aardil@msn.com>
2017-03-20 16:06:07 -0500

    Update 316043.pl

--- quests/provinggrounds/316043.pl
+++ quests/provinggrounds/316043.pl
@@ -11,9 +11,10 @@ my $Duram = quest::saylink("Duram", 1);
   quest::say("Duram sent you? Unbelievable. Before I go anywhere, I need my sword back, it was taken from me earlier. That beast in there took it and knocked me down. . .he thought I was dead because I didn't move. Defeat him and bring me back my sword and I will leave.");
   quest::spawn2(316063,0,0,-1861,-742,-309.8,146.5);
    }
- } 
+ }
+ 
   sub EVENT_ITEM {
-    if (plugin::check_handin(\%itemcount, 69974 => 1)) {
+    (plugin::check_handin(\%itemcount, 69974 => 1)) {
     quest::say("'My sword, you have brought it back to me. Thank you $name, you truly are heroic.");
     quest::say("'At last I am free again! Thank you so much for saving me from the horrors of this vile place. You have my gratitude. I can find my way out from here friend. Here take this note. I am sure you will find a use for it.");
     quest::summonitem(69973);
