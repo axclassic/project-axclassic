3fabcd2eaafde5fd79cd985a3ad63316372b8b0f
Aardil <aardil@msn.com>
2017-04-01 20:07:41 -0500

    Update Seilaen.pl

--- quests/ponightmare/Seilaen.pl
+++ quests/ponightmare/Seilaen.pl
@@ -34,6 +34,7 @@ sub EVENT_SIGNAL {
   quest::shout("Oh thank you! Thank you! I don't feel the trees watching me anymore! And.. Let me try Mother's Locket again, I think it might work..");
   quest::depopall(204351);
   quest::depopall(204352);
+  quest::depop(204353);
   quest::depop();
   }
  }  
