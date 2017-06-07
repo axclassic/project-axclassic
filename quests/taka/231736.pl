8d8d22db149ca5e4faf9faff345dd78aadc15bd6
Aardil <aardil@msn.com>
2017-06-07 10:11:31 -0500

    Delete 231736.pl

--- quests/taka/231736.pl
+++
@@ -1,7 +0,0 @@
-##test 231736##
-sub EVENT_SAY {
-if ($text=~/hail/i && $ulevel <= 65 && $ulevel >= 60){
-quest::say("Sending you to the Sunken Library.");
-quest::setglobal("Takaten", 1, 5, "90M");
-}
-}
