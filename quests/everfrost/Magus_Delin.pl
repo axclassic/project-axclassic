5614737ebea784f784549398a64f4e4ab2e05151
Congdar <congdar@hotmail.com>
2017-06-09 18:31:50 -0700

    Use Moonstones

--- quests/everfrost/Magus_Delin.pl
+++ quests/everfrost/Magus_Delin.pl
@@ -6,27 +6,19 @@ sub EVENT_SAY {
   if($ulevel >= 15) {
     if(plugin::check_hasitem($client, 41000) || $client->KeyRingCheck(41000)) { #Adventurer's Stone
       if($text=~/hail/i) {
-        quest::say("You're going to need to have a fair amount of faith in me and my friends. I make it a policy to tell all of the travelers that come to me that this is not a perfected magic and probably never will be. The magic we are using is, in my opinion, not of Norrathian origins. Or perhaps I'm simply an eccentric who hopes there is a greater force out there that is granting us the use of new magics. Anyway, enough chatter. Tell me where you would like to go and I will send you there. I can send you to any of the other camps in [Butcherblock], [Commonlands], [Nedaria's Landing], [North Ro], or [South Ro]. Please be still as I do this.");
-      }
-      if($text=~/commonlands/i) {
-        quest::emote("begins to cast a spell.");
-        quest::movepc(22,-105,-1640,5,0); #East Commonlands
-      }
-      if($text=~/butcherblock/i) {
-        quest::emote("begins to cast a spell.");
-        quest::movepc(68,-2500,-1105,1,0); #Butcherblock Mountains
-      }
-      if($text=~/nedaria/i) {
-        quest::emote("begins to cast a spell.");
-        quest::selfcast(4580); #Teleport Nedaria
-      }
-      if($text=~/north ro/i) {
-        quest::emote("begins to cast a spell.");
-        quest::movepc(34,900,2650,-24,0); #Northern Desert of Ro
-      }
-      if($text=~/south ro/i) {
-        quest::emote("begins to cast a spell.");
-        quest::movepc(35,1030,-1440,-23,0); #Southern Desert of Ro
+         my $random_result = int(rand(100));
+         if($random_result<=45) {
+            quest::say("I hear that you use Moonstones for travel around here.");
+         }
+         elsif($random_result<=60) {
+            quest::emote("Looks toward the mountains dreaming of the after work skiing trip.");
+         }
+         elsif($random_result<=85) {
+            quest::say("Sure, Arindri gets the sunny beach gig and I am stuck in the frozen tundra.");
+         }
+         else {
+            quest::say("I hear that you use Moonstones for travel around here.");
+         }
       }
     }
     else { #PC does not have Adventurer's Stone
--- quests/nro/Magus_Arindri.pl
+++ quests/nro/Magus_Arindri.pl
@@ -3,39 +3,31 @@
 #
 
 sub EVENT_SAY {
-  if($ulevel >= 15) {
-    if(plugin::check_hasitem($client, 41000) || $client->KeyRingCheck(41000)) { #Adventurer's Stone
-      if($text=~/hail/i) {
-        quest::say("You endured the burning heat of the desert to come and use our magic! I'm so excited. We have been getting a lot of customers. We've only lost a few. I sometimes wonder if Vayzl has incorrectly used the spell on purpose to see the odd traveler explode into a spray of magic. I do respect her, but I just don't understand dark elves, I guess. I am far too precise and careful to make any mistakes . . . Well, not a second time. Tell me where you would like to go and I will send you there. I can send you to any of the other camps in [Butcherblock], [Commonlands], [Everfrost], [Nedaria's Landing], or [South Ro].");
+   if($ulevel >= 15) {
+      if(plugin::check_hasitem($client, 41000) || $client->KeyRingCheck(41000)) { #Adventurer's Stone
+         if($text=~/hail/i) {
+            my $random_result = int(rand(100));
+            if($random_result<=45) {
+               quest::say("I hear that you use Moonstones for travel around here.");
+            }
+            elsif($random_result<=60) {
+               quest::say("I can hear the sound of the Ocean from here, can you?");
+            }
+            elsif($random_result<=85) {
+               quest::say("All this sand... it gets into everywhere.");
+            }
+            else {
+               quest::say("I hear that you use Moonstones for travel around here.");
+            }
+         }
       }
-      if($text=~/commonlands/i) {
-        quest::emote("begins to cast a spell.");
-        quest::selfcast(4176); #Teleport East Commons
+      else { #PC does not have Adventurer's Stone
+         quest::say("You will have to excuse me, I am quite busy studying this Farstone and the possibility of using the magic stored inside of it. Perhaps you should talk to another at this camp to see if they have any use for you.");
       }
-      if($text=~/butcherblock/i) {
-        quest::emote("begins to cast a spell.");
-        quest::selfcast(4179); #Teleport Butcherblock
-      }
-      if($text=~/nedaria/i) {
-        quest::emote("begins to cast a spell.");
-        quest::selfcast(4580); #Teleport Nedaria
-      }
-      if($text=~/everfrost/i) {
-        quest::emote("begins to cast a spell.");
-        quest::selfcast(4180); #Teleport Everfrost
-      }
-      if($text=~/south ro/i) {
-        quest::emote("begins to cast a spell.");
-        quest::selfcast(4178); #Teleport South Ro
-      }
-    }
-    else { #PC does not have Adventurer's Stone
-      quest::say("You will have to excuse me, I am quite busy studying this Farstone and the possibility of using the magic stored inside of it. Perhaps you should talk to another at this camp to see if they have any use for you.");
-    }
-  }   
-  else { #PC is not at appropriate level yet
-    quest::say("You will have to excuse me, I am quite busy."); #Text made up
-  }
+   }
+   else { #PC is not at appropriate level yet
+      quest::say("You will have to excuse me, I am quite busy."); #Text made up
+   }
 }
 
 sub EVENT_ITEM {
