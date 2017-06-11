b4206c8c5a69e1f99816415c6e6dfa8a05ab82c8
Angelox <axclassic@users.noreply.github.com>
2017-06-10 20:04:21 -0400

    Obsolete - look for PrivateRepos

--- ax_classic_blackwater/templates/intro.tmpl.php
+++
@@ -1,5 +0,0 @@
-       <center>
-        <div class="page_title"><span><br>AX-Classic Online Editing Interface</span></div>
-        <img src="images/dontpanic.jpg">
-        <div class="page_title"><span>Boomba The Big Edition: Updated October, 2011</span></div>
-       </center>
