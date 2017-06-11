d2898e9eed4bff03479014305d5e525074de54ac
Aardil <aardil@msn.com>
2017-06-11 09:09:47 -0500

    Create Vision_of_Emperor_Csarth.pl

--- quests/hohonora/Vision_of_Emperor_Csarth.pl
+++ quests/hohonora/Vision_of_Emperor_Csarth.pl
@@ -1,4 +1,4 @@
-##Emperor_Csarth 211120##
+##Vision_of_Emperor_Csarth 211120##
 sub EVENT_SPAWN {
 quest::signalwith(211151,357,0);
 quest::ze(15,"The walls seem to bend and break as Csarth marches down from his throne.");
