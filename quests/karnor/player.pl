9c5529b08a166fee804bed2492b02d0824b88727
patrikpatrik <axclassiceq@gmail.com>
2017-07-26 16:29:32 -0700

    rev 3.

--- quests/karnor/player.pl
+++ quests/karnor/player.pl
@@ -1,22 +1,17 @@
 #####################################
 ## Server-wide messages upon rare loots
 ## Karnor's Castle
-## Revision 2.0 by patrikpatrik 7/25/2017
+## Revision 3 by patrikpatrik 7/26/2017
 #####################################
 
-sub EVENT_LOOT{
-	$tempa = $looted_id;
-	#$statement2 = quest::ze(15, "looted_id is $tempa.");
-	if($tempa == 82731) { #Fabled_Jade_mace
-		$item = quest::varlink(82731);
+sub EVENT_LOOT {
+	my @item_list = (82731, 82734, 6639);
+	# 82731 Fabled_Jade_mace
+	# 82734 Fabled_Tranquil staff
+	# 6639 Tranquil staff
+
+	if(grep(/^$looted_id$/, @item_list)) {
+		my $item = quest::varlink($looted_id);
 		quest::we(14, "The Rathe server congratulates $name, the uber $class for looting an extremely rare -$item-!");
-	} elsif ($tempa == 82734) { #Fabled_Tranquil staff
-		$item = quest::varlink(82734);
-		quest::we(14, "The Rathe server congratulates $name, the uber $class for looting an extremely rare -$item-!");
-	} elsif ($tempa == 6639) { # Tranquil staff
-		$item = quest::varlink(6639);
-		quest::we(14, "The Rathe server congratulates $name, the uber $class for looting an extremely rare -$item-!");
-	} else {
-		# Do nothing!
 	}
 }
