40104c075dc1401858d1e577f85584d5212a4665
patrikpatrik <axclassiceq@gmail.com>
2017-07-26 16:31:39 -0700

    rev 3.

--- quests/sebilis/player.pl
+++ quests/sebilis/player.pl
@@ -1,22 +1,17 @@
 #####################################
 ## Server-wide messages upon rare loots
 ## Sebilis
-## Revision 2.0 by patrikpatrik 7/25/2017
+## Revision 3 by patrikpatrik 7/26/2017
 #####################################
 
-sub EVENT_LOOT{
-	$tempa = $looted_id;
-	#$statement2 = quest::ze(15, "looted_id is $tempa.");
-	if($tempa == 2735) { #Fungus covered scale tunic
-		$item = quest::varlink(2735);
+sub EVENT_LOOT {
+	my @item_list = (2735, 1619, 1620);
+	# 2735 Fungus covered scale tunic
+	# 1619 siblisian berserker cloak
+	# 1620 Runebranded girdle
+
+	if(grep(/^$looted_id$/, @item_list)) {
+		my $item = quest::varlink($looted_id);
 		quest::we(14, "The Rathe server congratulates $name, the uber $class for looting an extremely rare -$item-!");
-	} elsif ($tempa == 1619) { #siblisian berserker cloak
-		$item = quest::varlink(1619);
-		quest::we(14, "The Rathe server congratulates $name, the uber $class for looting an extremely rare -$item-!");
-	} elsif ($tempa == 1620) { # Runebranded girdle
-		$item = quest::varlink(1620);
-		quest::we(14, "The Rathe server congratulates $name, the uber $class for looting an extremely rare -$item-!");
-	} else {
-		# Do nothing
 	}
 }
