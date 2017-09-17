a0c80ea54801caa0b922e711121f02524002a3b0
patrikpatrik <axclassiceq@gmail.com>
2017-09-17 12:12:19 -0700

    Emeleo Leafwalker will tell you if an Event is currently running.

--- quests/cshome/Emeleo_Leafwalker.pl
+++ quests/cshome/Emeleo_Leafwalker.pl
@@ -1,6 +1,9 @@
 #Zone:cshome
 #Angelox
 #Script for setting or unsetting special events
+#Update 9/17/2017
+#Will now tell you if an event is up before setting it - patrikpatrik
+##########################################################
 
 sub EVENT_SAY {
 my $start = quest::saylink("start", 1);
@@ -12,9 +15,33 @@ my $startBloodhunt = quest::saylink("Start Bloodhunt", 1);
 my $startPitchblack = quest::saylink("Start Pitch Black", 1);
 my $startMoonstone = quest::saylink("Start Random Moonstone", 1);
 my $startOrc = quest::saylink("Start Band dislikes Orcs", 1); #this one already here, saved for template to next quest.
+#array of existing events in database. event1 - event8
+my @eventarr = ($qglobals{'event1'},$qglobals{'event2'},$qglobals{'event3'},
+$qglobals{'event4'},$qglobals{'event5'},$qglobals{'event6'},$qglobals{'event7'},
+$qglobals{'event8'});
+#naming the events
+my $event1 = "New Year's Event";
+my $event2 = "Easter Event";
+my $event3 = "Bloodhunt Event";
+my $event4 = "Pitch Black Event";
+my $event5 = "Random Moonstone Event";
+my $event6 = "Christmas Event";
+my $event7 = "Band Dislikes Orcs";
+my $event8 = "Unknown";
+my @currevent = ($event1, $event2, $event3, $event4, $event5, $event6, $event7, $event8);
 
 if($text=~/Hail/i){
   $client->Message(14,"Hello $name\! Would you like to $start or $terminate an event?");
+  	# Events currently running if active
+	 for (my $i = 0; $i < scalar(@eventarr); $i++) {
+			my $active = $eventarr[$i];
+			if($active == 1) {
+			$enow = $currevent[$i]; # Setting indices to be equal to each other!
+				my $p = $i + 1;
+				my $eventnow = "Event$p";
+				$client->Message(14, "$enow or $eventnow is currently running!");
+			}
+		} 
  }
 elsif($text=~/Start New Year/i){ #event1
  $client->Message(14,"Very well then, The NewYear event has started.");
