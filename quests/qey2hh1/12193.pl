5aa853a4370de1d0851206956f393e98571d9974
patrikpatrik <axclassiceq@gmail.com>
2017-10-01 22:58:24 -0700

    controller disabled for now.

--- quests/qey2hh1/12193.pl
+++ quests/qey2hh1/12193.pl
@@ -1,14 +1,16 @@
-#Vilnius_controller#
-sub EVENT_SPAWN {
-$x = $npc->GetX();
-	$y = $npc->GetY();
-	quest::set_proximity($x - 150, $x + 150, $y - 150, $y + 150);
-    }
-sub EVENT_ENTER {
-    my $a = quest::ChooseRandom(12019,12123,12139);
-    quest::depop(12019);
-    quest::depop(12123);
-    quest::depop(12139);
-  quest::spawn2($a,17,0,-6695.9,166.9,260.9,209.5);
-    quest::depop();
-  }
+# Vilnius_controller#
+# 10/2/2017 Controller disabled for now - patrikpatrik
+
+# sub EVENT_SPAWN {
+# $x = $npc->GetX();
+	# $y = $npc->GetY();
+	# quest::set_proximity($x - 150, $x + 150, $y - 150, $y + 150);
+    # }
+# sub EVENT_ENTER {
+    # my $a = quest::ChooseRandom(12019,12123,12139);
+    # quest::depop(12019);
+    # quest::depop(12123);
+    # quest::depop(12139);
+  # quest::spawn2($a,17,0,-6695.9,166.9,260.9,209.5);
+    # quest::depop();
+  # }
