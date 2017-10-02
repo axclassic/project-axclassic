01a7d8f746a6b8db70b48d633212517d35755fef
patrikpatrik <axclassiceq@gmail.com>
2017-10-01 22:57:42 -0700

    Updated day & night spawn conditions for Vilnius & ph.

--- quests/qey2hh1/12179.pl
+++ quests/qey2hh1/12179.pl
@@ -1,24 +1,40 @@
 #qey2hh1 night and day, spawns night or day NPCs.
 #Angelox
-sub EVENT_SPAWN{
-if (($zonetime >= 0)&&($zonetime <= 800)){           #nighttime
-	quest::spawn_condition("qey2hh1", 2,0);      #Day is 2
-	quest::spawn_condition("qey2hh1", 1,1);}     #Night is 1
-elsif (($zonetime >= 2000)&&($zonetime <= 2359)){    #nighttime
-	quest::spawn_condition("qey2hh1", 2,0);      #Day is 2
-	quest::spawn_condition("qey2hh1", 1,1);}     #night is 1
-else{		#daytime
-	quest::spawn_condition("qey2hh1",2,1);
-	quest::spawn_condition("qey2hh1",1,0);}}
+#Update- 10/2/2017 patrikpatrik
+#Added spawn_conditions for a_brigand and Vilnius the Small (rogue epic)
+sub EVENT_SPAWN {
+	if (($zonetime >= 0)&&($zonetime <= 800)) {      	 #nighttime
+		quest::spawn_condition("qey2hh1", 2,0);      	 #Day is 2
+		quest::spawn_condition("qey2hh1", 1,1);      	 #Night is 1
+	} elsif (($zonetime >= 0)&&($zonetime <= 600)) {	 # 12am - 6am
+		quest::spawn_condition("qey2hh1", 3,1);			 # vilnius is up
+	} elsif (($zonetime >= 2200)&&($zonetime <= 2359)) { # 10pm - 12am
+		quest::spawn_condition("qey2hh1", 3,1);          # vilnius is up
+	} elsif (($zonetime >= 2000)&&($zonetime <= 2359)) { #nighttime
+		quest::spawn_condition("qey2hh1", 2,0);      	 #Day is 2
+		quest::spawn_condition("qey2hh1", 1,1);      	 #night is 1
+	} else {											 #daytime
+		quest::spawn_condition("qey2hh1",2,1);
+		quest::spawn_condition("qey2hh1",1,0);
+		quest::spawn_condition("qey2hh1",3,0); 			 #vilnius is NOT up
+	}
+}
 
-sub EVENT_WAYPOINT{
-if (($zonetime >= 0)&&($zonetime <= 800)){           #nighttime
-	quest::spawn_condition("qey2hh1", 2,0);   #Day is 2
-	quest::spawn_condition("qey2hh1", 1,1);}  #Night is 1
-elsif (($zonetime >= 2000)&&($zonetime <= 2359)){    #nighttime
-	quest::spawn_condition("qey2hh1", 2,0);   #Day is 2
-	quest::spawn_condition("qey2hh1", 1,1);}  #Night is 1
-else{		#daytime
-	quest::spawn_condition("qey2hh1",2,1);
-	quest::spawn_condition("qey2hh1",1,0);}}
+sub EVENT_WAYPOINT {
+	if (($zonetime >= 0)&&($zonetime <= 800)) {          #nighttime
+		quest::spawn_condition("qey2hh1", 2,0);   		 #Day is 2
+		quest::spawn_condition("qey2hh1", 1,1);  		 #Night is 1
+	} elsif (($zonetime >= 0)&&($zonetime <= 600)) {	 # 12am - 6am
+		quest::spawn_condition("qey2hh1", 3,1);			 # vilnius is up
+	} elsif (($zonetime >= 2200)&&($zonetime <= 2359)) { # 10pm - 12am
+		quest::spawn_condition("qey2hh1", 3,1);          # vilnius is up	
+	} elsif (($zonetime >= 2000)&&($zonetime <= 2359)) { #nighttime
+		quest::spawn_condition("qey2hh1", 2,0);   		 #Day is 2
+		quest::spawn_condition("qey2hh1", 1,1);  		 #Night is 1
+	} else {											 #daytime
+		quest::spawn_condition("qey2hh1",2,1);
+		quest::spawn_condition("qey2hh1",1,0);
+		quest::spawn_condition("qey2hh1",3,0); 			 #vilnius is NOT up
+	}
+}
 ## End of script
