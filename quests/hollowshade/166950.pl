a4f53b3e44c59d203d9ed7059d32986e40c7c94e
Angelox <axclassic@users.noreply.github.com>
2017-10-05 08:11:20 -0400

    Hollowshade work

--- quests/hollowshade/166950.pl
+++ quests/hollowshade/166950.pl
@@ -1,11 +1,9 @@
-##Angelox: This wasn't working before and problems started after Pat corrected
-##So maybe Aardil made a new script that worked and is the why for the multi spawns
-##I'm quoting it out
-# sub EVENT_SPAWN{
-#   quest::signalwith(166971,816,0);
-#   quest::signalwith(166972,818,0);
-#   quest::signalwith(166987,3070,0);
-# }
-# sub EVENT_DEATH{
-# quest::signalwith(166582,516,250000);
-# }
+sub EVENT_SPAWN{
+   quest::signalwith(166971,816,0); ## North_Cave_Respawn_check_wolves
+   quest::signalwith(166972,818,0); ## North_Cave_Respawn_check_owlbears
+   quest::signalwith(166987,3070,0); ## North_Cave_Respawn_check_Delay_grimlings
+ }
+ sub EVENT_DEATH{
+ quest::signalwith(166582,516,250000);#War_Event_Respawner
+ }
+ ## #Wiknak_Grimglom script
