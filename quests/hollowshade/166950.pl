6095c3069bb36ea8084253f4c6b7f7cf266ae8b0
Angelox <axclassic@users.noreply.github.com>
2017-10-04 21:05:23 -0400

    May have found the problem.

--- quests/hollowshade/166950.pl
+++ quests/hollowshade/166950.pl
@@ -1,8 +1,11 @@
-sub EVENT_SPAWN{
-  quest::signalwith(166971,816,0);
-  quest::signalwith(166972,818,0);
-  quest::signalwith(166987,3070,0);
-}
-sub EVENT_DEATH{
-quest::signalwith(166582,516,250000);
-}
+##Angelox: This wasn't working before and problems started after Pat corrected
+##So maybe Aardil made a new script that worked and is the why for the multi spawns
+##I'm quoting it out
+# sub EVENT_SPAWN{
+#   quest::signalwith(166971,816,0);
+#   quest::signalwith(166972,818,0);
+#   quest::signalwith(166987,3070,0);
+# }
+# sub EVENT_DEATH{
+# quest::signalwith(166582,516,250000);
+# }
