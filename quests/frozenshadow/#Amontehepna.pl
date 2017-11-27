e19d5e9ee2317423b178306f92ed4e0b566dc3f4
Angelox <axclassic@users.noreply.github.com>
2017-11-27 09:58:19 -0500

    Angelox:Fix for Fabled Omica to warn when de-popping.

--- quests/frozenshadow/#Amontehepna.pl
+++ quests/frozenshadow/#Amontehepna.pl
@@ -4,8 +4,12 @@
 sub EVENT_DEATH{
 	my $random_result = int(rand(100));
     if ($omica>1){ #Omica not up yet
-	if (($random_result < 20) && ($ulevel > 39)) { #4
+	if (($random_result <= 20) && ($ulevel >= 40)) { #4
 		quest::spawn2(111177,0,0,199.12,984.39,186.16,135);  #Spawn Fabled Omica
+	}
+	elsif (($random_result <= 20) && ($ulevel <= 39)) { # Should have been Fabled, so warn and spawn normal Omica
+		quest::shout("We shall meet again $name! When you are a worthy opponent!!");
+		quest::spawn2(111163,0,0,199.12,984.39,186.16,135); 
 	}
 	else { 
 		quest::spawn2(111163,0,0,199.12,984.39,186.16,135); #Spawn normal Omica
