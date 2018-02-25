7fa82f11b93dac968177327ec0a388d183928547
Congdar <congdar@hotmail.com>
2018-02-25 15:44:15 -0800

    multiple changes

--- quests/sseru/#Lord_Inquisitor_Seru_.pl
+++ quests/sseru/#Lord_Inquisitor_Seru_.pl
@@ -1,16 +1,31 @@
-sub EVENT_SPAWN {
-  quest::settimer(1,1);
+sub EVENT_AGGRO {
+   quest::settimer("LeashLIS",5);
 }
 
+my $LISLocation = 1;
+my $distanceCHK = 0;
+my $distanceLOC = 50;
 sub EVENT_TIMER {
- my $x = $npc->GetX();
- my $y = $npc->GetY();
- if($timer == 1 && ($x < -353 || $x > -109 || $y < -549 || $y > -310)) {
-    quest::shout("No! I must not leave the time chamber! If I do, I'll age and die!");
-    $npc->GMMove(-231.464005,-432.937469,202.375946,.125);
- }
+   if($timer eq "LeashLIS") {
+      if($LISLocation == 1) {
+         $distanceCHK = $npc->CalculateDistance(-232.1,-431.6,201.1);
+      }
+      if($LISLocation == 2) {
+         $distanceCHK = $npc->CalculateDistance(-199.2, -432.7, 196.1);
+      }
+      if($distanceCHK >= $distanceLOC) {
+         quest::shout("No! I must not leave the time chamber! If I do, I'll age and die!");
+         $npc->GMMove(-199.2, -432.7, 196.1,255);
+         if($LISLocation == 2) {
+            $npc->HateSummon(1);
+            $distanceLOC = 20;
+         }
+         $LISLocation = 2;
+      }
+   }
 }
 
 sub EVENT_DEATH {
-  quest::stoptimer(1);
+   quest::stoptimer("LeashLIS");
 }
+
---
+++ quests/sseru/#The_Fabled_Lord_Inquisitor_Seru.pl
@@ -0,0 +1,31 @@
+sub EVENT_AGGRO {
+   quest::settimer("LeashFLIS",5);
+}
+
+my $LISLocation = 1;
+my $distanceCHK = 0;
+my $distanceLOC = 50;
+sub EVENT_TIMER {
+   if($timer eq "LeashFLIS") {
+      if($LISLocation == 1) {
+         $distanceCHK = $npc->CalculateDistance(-232.1,-431.6,201.1);
+      }
+      if($LISLocation == 2) {
+         $distanceCHK = $npc->CalculateDistance(-199.2, -432.7, 196.1);
+      }
+      if($distanceCHK >= $distanceLOC) {
+         quest::shout("No! I must not leave the time chamber! If I do, I'll age and die!");
+         $npc->GMMove(-199.2, -432.7, 196.1,255);
+         if($LISLocation == 2) {
+            $npc->HateSummon(1);
+            $distanceLOC = 20;
+         }
+         $LISLocation = 2;
+      }
+   }
+}
+
+sub EVENT_DEATH {
+   quest::stoptimer("LeashFLIS");
+}
+
