c0580426ed165ba7926a32b6635d7d45b746565a
Congdar <congdar@hotmail.com>
2018-03-01 15:58:06 -0800

    wisp stone for vex thal key

--- quests/items/CHRMNegFaction306.pl
+++ quests/items/CHRMNegFaction306.pl
@@ -1,9 +1,10 @@
 sub EVENT_SCALE_CALC {
-  my $scale = $client->GetModCharacterFactionLevel(279);
-    
-  # set the scale based on where faction is in the range [-1500, 0]
-  if($scale > 0) {
-    $scale = 0;
-  }
-  $questitem->SetScale($scale/-1500);
+   my $scale = $client->GetModCharacterFactionLevel(279); # Sabertooths of Blackburrow
+
+   # set the scale based on where faction is in the range [-1500, 0]
+   if($scale > 0) {
+      $scale = 0;
+   }
+   $questitem->SetScale($scale/-1500);
 }
+
--- quests/poknowledge/Arch_Mage_Olera.pl
+++ quests/poknowledge/Arch_Mage_Olera.pl
@@ -1,26 +1,36 @@
 #Arch_Mage_Olera.pl
 #Planar Armor Exchange/Magician Tomes
 
-sub EVENT_SAY { 
-	if($text=~/Hail/i){
-		quest::emote("gives an almost unearthly graceful gesture in warm welcoming of $name. 'May the light of honor, valor, and untainted passion guide you through the dark turmoil of this dreaded era. I am very pleased to see you before us, friend, for the children of the light must all come together in this dark hour of Norrath and the universe's fate. If you have need of guidance in the ways of the elements, then what humble knowledge I have is at your disposal, my friend.'");
-	}
+sub EVENT_SAY {
+   if($text=~/Hail/i) {
+      quest::emote("gives an almost unearthly graceful gesture in warm welcoming of $name. 'May the light of honor, valor, and untainted passion guide you through the dark turmoil of this dreaded era. I am very pleased to see you before us, friend, for the children of the light must all come together in this dark hour of Norrath and the universe's fate. If you have need of guidance in the ways of the elements, then what humble knowledge I have is at your disposal, my friend.'");
+   }
+   if($text=~/wisp stone/i) {
+      quest::say("I will gladly perform this service for you.  Just hand me 100 platinum.");
+   }
 }
+
 sub EVENT_ITEM {
-	if((plugin::check_handin(\%itemcount, 1239 => 1)) ||
-		(plugin::check_handin(\%itemcount, 1240 => 1)) ||
-		(plugin::check_handin(\%itemcount, 1241 => 1)) ||
-		(plugin::check_handin(\%itemcount, 1242 => 1)) ||
-		(plugin::check_handin(\%itemcount, 1243 => 1)) ||
-		(plugin::check_handin(\%itemcount, 1244 => 1)) ||
-		(plugin::check_handin(\%itemcount, 1245 => 1))) {#Apothic Armor
-    	quest::say("Thank you, $name.");#Text made up
-    	quest::summonitem(quest::ChooseRandom(10028, 10037, 22503, 15981));#Random gem: Peridot, Diamond, Blue Diamond, Raw Diamond
-    	quest::exp(698775);
-  }
-  else {
-		plugin::try_tome_handins(\%itemcount, $class, 'Magician');
-		plugin::return_items(\%itemcount);
-  }
+   if($platinum > 99) {
+      quest::selfcast(321); # Summon Wisp Stone
+   }
+   elsif((plugin::check_handin(\%itemcount, 1239 => 1)) ||
+      (plugin::check_handin(\%itemcount, 1240 => 1)) ||
+      (plugin::check_handin(\%itemcount, 1241 => 1)) ||
+      (plugin::check_handin(\%itemcount, 1242 => 1)) ||
+      (plugin::check_handin(\%itemcount, 1243 => 1)) ||
+      (plugin::check_handin(\%itemcount, 1244 => 1)) ||
+      (plugin::check_handin(\%itemcount, 1245 => 1)))
+   {#Apothic Armor
+      quest::say("Thank you, $name.");#Text made up
+      quest::summonitem(quest::ChooseRandom(10028, 10037, 22503, 15981));#Random gem: Peridot, Diamond, Blue Diamond, Raw Diamond
+      quest::exp(698775);
+   }
+   else {
+      plugin::try_tome_handins(\%itemcount, $class, 'Magician');
+      plugin::return_items(\%itemcount);
+   }
 }
 #END of FILE Zone:poknowledge  ID:202240 -- Arch_Mage_Olera
+
+
