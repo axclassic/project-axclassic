e5a993ca36aa50c8a7ce2e237d11166d1228427f
Congdar <congdar@hotmail.com>
2018-04-04 17:05:00 -0700

    aerindar and pure mana updates

--- quests/poknowledge/Illusionist_Acored.pl
+++ quests/poknowledge/Illusionist_Acored.pl
@@ -1,6 +1,20 @@
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Why. hello there. $name. and welcome to New Tanaan. In the past. we were rarely graced with the visits of Norrath's brave adventurers. but now it seems that your presence is delivered in a most gracious and unpredicted volume. Oh but I do not complain. my friend! I am merely excited and bound in wonder at the true. underlying tale of it all -- why. something most extraordinary must be on the heels of history's most recent climax for such interest to be garnered by Norrath's worldly scholars and travelers. We of New Tanaan understand these things most well. indeed. and our guidance is extended to all should they need it. I personally am one whose affinity is that of enchantments. and any who share my niche are more than welcome to browse what spells I carry and purchase them at their will and desire."); }
+    if($text=~/Hail/i) {
+        quest::say("Why. hello there. $name. and welcome to New Tanaan. In the past. we were rarely graced with the visits of Norrath's brave adventurers. but now it seems that your presence is delivered in a most gracious and unpredicted volume. Oh but I do not complain. my friend! I am merely excited and bound in wonder at the true. underlying tale of it all -- why. something most extraordinary must be on the heels of history's most recent climax for such interest to be garnered by Norrath's worldly scholars and travelers. We of New Tanaan understand these things most well. indeed. and our guidance is extended to all should they need it. I personally am one whose affinity is that of enchantments. and any who share my niche are more than welcome to browse what spells I carry and purchase them at their will and desire.");
+    }
+    if($text=~/Clear Mana/i) {
+       quest::say("I will gladly perform this service for you.  Just hand me an Emerald, a Poison Vial and 100 platinum.");
+    }
+}
+
+sub EVENT_ITEM {
+   if(($platinum>99) && plugin::check_handin(\%itemcount, 10029 => 1) && plugin::check_handin(\%itemcount, 16965 => 1)) {#Emerald and Poison Vial
+      quest::selfcast(540); #Clarify Mana
+   }
+   else {
+      quest::say("I don't need this."); #text made up
+      plugin::return_items(\%itemcount);
+   }
 }
 #END of FILE Zone:poknowledge  ID:202210 -- Illusionist_Acored 
 
--- quests/poknowledge/Illusionist_Lobaen.pl
+++ quests/poknowledge/Illusionist_Lobaen.pl
@@ -1,6 +1,17 @@
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Good day to you. $name. Despite your purpose among us in New Tanaan. know that we bear no ill will toward any for mere distinctions of race or religious preference. We are all equals -- children of the gods and children of the earth alike. It has been a very long time since I have been among adventurers. and I admit that this is truly an experience I have not craved. The nymphs were persecuted in our day. though not through intended force. but in the expansion of your cultures and the erection of great elven and human cities. Aaah. but I do dwell on matters of the past. What is done is done. Now. I exist to aid you should you wish it. friend. I have adopted the mortal art of enchantment and what knowledge I have of it is yours should it be of use."); }
+    if($text=~/Hail/i) {
+        quest::say("Good day to you. $name. Despite your purpose among us in New Tanaan. know that we bear no ill will toward any for mere distinctions of race or religious preference. We are all equals -- children of the gods and children of the earth alike. It has been a very long time since I have been among adventurers. and I admit that this is truly an experience I have not craved. The nymphs were persecuted in our day. though not through intended force. but in the expansion of your cultures and the erection of great elven and human cities. Aaah. but I do dwell on matters of the past. What is done is done. Now. I exist to aid you should you wish it. friend. I have adopted the mortal art of enchantment and what knowledge I have of it is yours should it be of use.");
+    }
+    if($text=~/Clear Mana/i) {
+        quest::say("I will gladly perform this service for you.  Just hand me an Emerald, a Poison Vial and 100 platinum.");
+    }
+}
+
+sub EVENT_ITEM {
+    if(($platinum>99) && plugin::check_handin(\%itemcount, 10029 => 1) && plugin::check_handin(\%itemcount, 16965 => 1)) {
+        #Emerald and Poison Vial
+        quest::selfcast(540); #Clarify Mana
+    }
 }
 #END of FILE Zone:poknowledge  ID:202211 -- Illusionist_Lobaen 
 
