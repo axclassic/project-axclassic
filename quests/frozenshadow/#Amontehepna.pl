94b61d93aa1dff3a01fff432e521f8deafb23c1b
Congdar <congdar@hotmail.com>
2018-08-13 09:08:50 -0700

    update #Amontehepna.pl #Cara_Omica.pl

--- quests/frozenshadow/#Amontehepna.pl
+++ quests/frozenshadow/#Amontehepna.pl
@@ -1,21 +1,15 @@
 #npc - Spawns #Cara_Omica (111163) upon Amontehepna's death.
 #zone - FrozenShadow
 #Angelox
-sub EVENT_DEATH{
-	#quest::say("Your level is $ulevel");
-	my $random_result = int(rand(100));
-    if ($omica>1){ #Omica not up yet
-	if (($random_result <= 20) && ($ulevel >= 40)) { #4
-		quest::spawn2(111177,0,0,199.12,984.39,186.16,135);  #Spawn Fabled Omica
-	}
-	elsif (($random_result <= 20) && ($ulevel <= 39)) { # Should have been Fabled, so warn and spawn normal Omica
-		quest::shout("We shall meet again $name! When you are a worthy opponent!!");
-		quest::spawn2(111163,0,0,199.12,984.39,186.16,135); 
-	}
-	else { 
-		quest::spawn2(111163,0,0,199.12,984.39,186.16,135); #Spawn normal Omica
-	}
+sub EVENT_DEATH {
+    if(!$entity_list->GetMobByNpcTypeID(111163)) { #Omica not up yet
+        my $random_result = int(rand(100));
+        if(($random_result <= 20) && ($ulevel >= 40)) {
+            quest::spawn2(111177,0,0,199.12,984.39,186.16,135);  #Spawn Fabled Omica
+        }
+        else {
+            quest::spawn2(111163,0,0,199.12,984.39,186.16,135); #Spawn normal Omica
+        }
     }
-  #Amontehepna died and should reset back to 2 after Omica death.  
-  quest::setglobal("omica",1,3,"F");
 }
+
--- quests/sleeper/Not_Found.pl
+++
@@ -1,6 +0,0 @@
-sub EVENT_SAY{
-if($text=~/Ahhh, I feel much better now.../i){
-quest::say("One has performed well.  An intruder is stopped."); }
-}
-#END of FILE Zone:sleeper  ID:Not_Found -- an_ancient_sentry 
-
