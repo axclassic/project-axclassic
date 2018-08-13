29fa96a1a70e55089f89305a2f71d0a2be9259ce
Congdar <congdar@hotmail.com>
2018-08-13 09:23:51 -0700

    update #Amontehepna.pl #The_Fabled_Cara_Omica.pl

--- quests/frozenshadow/#Amontehepna.pl
+++ quests/frozenshadow/#Amontehepna.pl
@@ -2,7 +2,7 @@
 #zone - FrozenShadow
 #Angelox
 sub EVENT_DEATH {
-    if(!$entity_list->GetMobByNpcTypeID(111163)) { #Omica not up yet
+    if(!$entity_list->GetMobByNpcTypeID(111163)&&!$entity_list->GetMobByNpcTypeID(111177)) { #Omica not up yet
         my $random_result = int(rand(100));
         if(($random_result <= 20) && ($ulevel >= 40)) {
             quest::spawn2(111177,0,0,199.12,984.39,186.16,135);  #Spawn Fabled Omica
--- quests/frozenshadow/#The_Fabled_Cara_Omica.pl
+++ quests/frozenshadow/#The_Fabled_Cara_Omica.pl
@@ -1,7 +1,4 @@
 # #The_Fabled_Cara_Omica NPCID - 111177
-sub EVENT_SPAWN{
-	$npc->SetAppearance(1); #Sitting
-}
-sub EVENT_DEATH{
- quest::setglobal("omica",2,3,"F"); #set global back to 2 which resets Amontehepna script
+sub EVENT_SPAWN {
+    $npc->SetAppearance(1); #Sitting
 }
