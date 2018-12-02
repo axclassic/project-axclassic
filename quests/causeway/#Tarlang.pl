10f630390b7bf1897998b8791bb80f432e17ab86
Aardil <aardil@msn.com>
2018-12-02 13:13:56 -0600

    Update #Tarlang.pl

--- quests/causeway/#Tarlang.pl
+++ quests/causeway/#Tarlang.pl
@@ -1,15 +1,18 @@
 #BeginFile: causeway\#Tarlang.pl 
-#Quest file for Nobles Causeway - Tarlang (Active): Necromancer Epic 1.5 (Soulwhisper) 
+#Quest file for Nobles Causeway - Tarlang Bot Necromancer Epic 2.0 (Deathwhisper)
 
 sub EVENT_SPAWN { 
-  quest::settimer("TarlangDepop",1500); #Despawn after 25 minutes 
-} 
+  quest::shout("Fool! You think you have what it takes to destroy me? I shall feast on your bones!");
+  quest::settimer("TarlangDepop",3600); 
+  } 
 
 sub EVENT_TIMER {
 if($timer eq "TarlangDepop") { 
-  quest::stoptimer("TarlangDepop"); 
+  quest::stoptimer("TarlangDepop");
+  quest::depop(303119);
+  quest::depop(303046);  
   quest::depop(); 
-} 
+ } 
 }
 
 #EndFile: causeway\#Tarlang.pl 
