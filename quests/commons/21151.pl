63139d4217da8bf78db32299cc5f62ae17b92965
Angelox <axclassic@users.noreply.github.com>
2019-11-22 08:24:59 -0500

    More changes to npc condition scripts

--- quests/commons/21151.pl
+++ quests/commons/21151.pl
@@ -1,13 +1,15 @@
 #commons night and day, spawns night or day NPCs.
+#This set of NPCs already has condition 2 assigned to Live
+#and condition 1 assigned to Undead, so has to be like this
 #Angelox, Qadar
 sub EVENT_SPAWN {
    if(($zonetime >= 0)&&($zonetime <= 800)) {        #nighttime
-      quest::spawn_condition("commons", 1,1);        #live are 1
-      quest::spawn_condition("commons", 2,0);        #undead are 2
+      quest::spawn_condition("commons", 1,0);        #Undead has Condition 1, so 0 spawns them
+      quest::spawn_condition("commons", 2,1);        #Live have Condition 2
    }
    elsif(($zonetime >= 2000)&&($zonetime <= 2359)) { #nighttime
-      quest::spawn_condition("commons", 1,1);        #live are 1
-      quest::spawn_condition("commons", 2,0);        #undead are 2
+      quest::spawn_condition("commons", 1,0);        #Undead has Condition 1, so 0 spawns them
+      quest::spawn_condition("commons", 2,1);        #Live have Condition 2
    }
    else {                                            #daytime
       quest::spawn_condition("commons",1,1);
@@ -17,12 +19,12 @@ sub EVENT_SPAWN {
 
 sub EVENT_WAYPOINT {
    if(($zonetime >= 0)&&($zonetime <= 800)) {        #nighttime
-      quest::spawn_condition("commons", 1,1);        #live are 1
-      quest::spawn_condition("commons", 2,0);        #undead are 2
+      quest::spawn_condition("commons", 1,0);        #Undead has Condition 1, so 0 spawns them
+      quest::spawn_condition("commons", 2,1);        #Live have Condition 2
    }
    elsif(($zonetime >= 2000)&&($zonetime <= 2359)) { #nighttime
-      quest::spawn_condition("commons", 1,1);        #live are 1
-      quest::spawn_condition("commons", 2,0);        #undead are 2
+      quest::spawn_condition("commons", 1,0);        #Undead has Condition 1, so 0 spawns them
+      quest::spawn_condition("commons", 2,1);        #Live have Condition 2
    }
    else {                                            #daytime
       quest::spawn_condition("commons",1,1);
--- quests/lakerathe/51150.pl
+++ quests/lakerathe/51150.pl
@@ -1,12 +1,14 @@
 # lakerathe night and day spawns
+#This set of NPCs already has condition 2 assigned to Live
+#and condition 1 assigned to Undead, so has to be like this
 #Angelox, Qadar
 sub EVENT_SPAWN {
    if(($zonetime >= 0)&&($zonetime <= 800)) {        #nighttime
-      quest::spawn_condition("lakerathe", 1,0);      #Undead had Condition 1, so 0 spawns them
+      quest::spawn_condition("lakerathe", 1,0);      #Undead has Condition 1, so 0 spawns them
       quest::spawn_condition("lakerathe", 2,1);      #Live have Condition 2
    }
    elsif(($zonetime >= 2000)&&($zonetime <= 2359)) { #nighttime
-      quest::spawn_condition("lakerathe", 1,0);      #Undead had Condition 1, so 0 spawns them
+      quest::spawn_condition("lakerathe", 1,0);      #Undead has Condition 1, so 0 spawns them
       quest::spawn_condition("lakerathe", 2,1);      #Live have Condition 2
    }
    else {                                            # default to daytime
@@ -17,11 +19,11 @@ sub EVENT_SPAWN {
 
 sub EVENT_WAYPOINT {
    if(($zonetime >= 0)&&($zonetime <= 800)) {        #nighttime
-      quest::spawn_condition("lakerathe", 1,0);      #Undead had Condition 1, so 0 spawns them
+      quest::spawn_condition("lakerathe", 1,0);      #Undead has Condition 1, so 0 spawns them
       quest::spawn_condition("lakerathe", 2,1);      #Live have Condition 2
    }
    elsif(($zonetime >= 2000)&&($zonetime <= 2359)) { #nighttime
-      quest::spawn_condition("lakerathe", 1,0);      #Undead had Condition 1, so 0 spawns them
+      quest::spawn_condition("lakerathe", 1,0);      #Undead has Condition 1, so 0 spawns them
       quest::spawn_condition("lakerathe", 2,1);      #Live have Condition 2
    }
    else {                                            # default to daytime
