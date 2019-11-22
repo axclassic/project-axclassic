ac99efe07c75dab34571942481163b2897044d5e
Congdar <congdar@hotmail.com>
2019-11-21 17:00:33 -0800

    restore perl based spawn conditions

--- quests/kithicor/20250.pl
+++ quests/kithicor/20250.pl
@@ -1,24 +1,32 @@
 # # kithicor night and day spawns
 # #Angelox, Qadar
- sub EVENT_SPAWN{
- if (($zonetime >= 0)&&($zonetime <= 800)){           #nighttime
- 	quest::spawn_condition("kithicor", 1,0);      #live are 1
- 	quest::spawn_condition("kithicor", 2,1);}     #undead are 2
- elsif (($zonetime >= 2000)&&($zonetime <= 2359)){    #nighttime
- 	quest::spawn_condition("kithicor", 1,0);      #live are 1
- 	quest::spawn_condition("kithicor", 2,1);}     #undead are 2
- else{		#daytime
- 	quest::spawn_condition("kithicor",1,1);
- 	quest::spawn_condition("kithicor",2,0);}}
- 
- sub EVENT_WAYPOINT{
- if (($zonetime >= 0)&&($zonetime <= 800)){           #nighttime
- 	quest::spawn_condition("kithicor", 1,0); #live are 1
- 	quest::spawn_condition("kithicor", 2,1);} #undead are 2
- elsif (($zonetime >= 2000)&&($zonetime <= 2359)){    #nighttime
- 	quest::spawn_condition("kithicor", 1,0); #live are 1
- 	quest::spawn_condition("kithicor", 2,1);} #undead are 2
- else{		#daytime			#day
- 	quest::spawn_condition("kithicor",1,1);
- 	quest::spawn_condition("kithicor",2,0);}}
- ## End of spawn script
+sub EVENT_SPAWN {
+   if(($zonetime >= 0)&&($zonetime <= 800)) {        #nighttime
+      quest::spawn_condition("kithicor", 1,0);       #live are 1
+      quest::spawn_condition("kithicor", 2,1);       #undead are 2
+   }
+   elsif(($zonetime >= 2000)&&($zonetime <= 2359)) { #nighttime
+      quest::spawn_condition("kithicor", 1,0);       #live are 1
+      quest::spawn_condition("kithicor", 2,1);       #undead are 2
+   }
+   else {                                            #daytime
+      quest::spawn_condition("kithicor",1,1);
+      quest::spawn_condition("kithicor",2,0);
+   }
+}
+
+sub EVENT_WAYPOINT {
+   if(($zonetime >= 0)&&($zonetime <= 800)) {        #nighttime
+      quest::spawn_condition("kithicor", 1,0);       #live are 1
+      quest::spawn_condition("kithicor", 2,1);       #undead are 2
+   }
+   elsif(($zonetime >= 2000)&&($zonetime <= 2359)) { #nighttime
+      quest::spawn_condition("kithicor", 1,0);       #live are 1
+      quest::spawn_condition("kithicor", 2,1);       #undead are 2
+   }
+   else {                                            #daytime
+      quest::spawn_condition("kithicor",1,1);
+      quest::spawn_condition("kithicor",2,0);
+   }
+}
+## End of spawn script
