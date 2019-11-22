258a7d374c4443bbd3e4c7957f0485318fde5275
Angelox <axclassic@users.noreply.github.com>
2019-11-22 07:24:32 -0500

    Correction for commons night day script

--- quests/commons/21151.pl
+++ quests/commons/21151.pl
@@ -2,12 +2,12 @@
 #Angelox, Qadar
 sub EVENT_SPAWN {
    if(($zonetime >= 0)&&($zonetime <= 800)) {        #nighttime
-      quest::spawn_condition("commons", 1,0);        #live are 1
-      quest::spawn_condition("commons", 2,1);        #undead are 2
+      quest::spawn_condition("commons", 1,1);        #live are 1
+      quest::spawn_condition("commons", 2,0);        #undead are 2
    }
    elsif(($zonetime >= 2000)&&($zonetime <= 2359)) { #nighttime
-      quest::spawn_condition("commons", 1,0);        #live are 1
-      quest::spawn_condition("commons", 2,1);        #undead are 2
+      quest::spawn_condition("commons", 1,1);        #live are 1
+      quest::spawn_condition("commons", 2,0);        #undead are 2
    }
    else {                                            #daytime
       quest::spawn_condition("commons",1,1);
@@ -17,12 +17,12 @@ sub EVENT_SPAWN {
 
 sub EVENT_WAYPOINT {
    if(($zonetime >= 0)&&($zonetime <= 800)) {        #nighttime
-      quest::spawn_condition("commons", 1,0);        #live are 1
-      quest::spawn_condition("commons", 2,1);        #undead are 2
+      quest::spawn_condition("commons", 1,1);        #live are 1
+      quest::spawn_condition("commons", 2,0);        #undead are 2
    }
    elsif(($zonetime >= 2000)&&($zonetime <= 2359)) { #nighttime
-      quest::spawn_condition("commons", 1,0);        #live are 1
-      quest::spawn_condition("commons", 2,1);        #undead are 2
+      quest::spawn_condition("commons", 1,1);        #live are 1
+      quest::spawn_condition("commons", 2,0);        #undead are 2
    }
    else {                                            #daytime
       quest::spawn_condition("commons",1,1);
