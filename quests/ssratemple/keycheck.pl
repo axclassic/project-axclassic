f66abe4b98ef5e479fa35a5596951a082f155618
Congdar <congdar@hotmail.com>
2020-09-19 17:37:15 -0700

    ssratemple updates

--- quests/ssratemple/keycheck.pl
+++ quests/ssratemple/keycheck.pl
@@ -1,13 +1,15 @@
 # keycheck (162569)
 
 sub EVENT_SPAWN {
-	quest::set_proximity($x - 208, $x + 208, $y - 242, $y + 242, $z - 5, $z + 400);
+	quest::set_proximity($x - 135, $x + 135, $y - 135, $y + 135, $z - 5, $z + 400);
 }
 
 sub EVENT_ENTER {
 	if($status < 80) {
 		if(!plugin::check_hasitem($client, 19719) && !$client->KeyRingCheck(19719)) {
-			quest::movepc(162,0,0,2.2,65); # Zone in: ssratemple
+			quest::movepc(162,0,0,2.2,65); # Zone: ssratemple
+            $client->Message(14, "You must have a Ring of the Shissar to enter this chamber.");
 		}
 	}
 }
+
