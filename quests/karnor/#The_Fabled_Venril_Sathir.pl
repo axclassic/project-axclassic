fa76067bb3e95efe3d5975eaa91b97db3b0a130d
Congdar <congdar@hotmail.com>
2020-12-21 09:09:51 -0800

    Updates to Ranger/Druid Epic 1.0 quest for non-Druid/Rangers

--- quests/karnor/#The_Fabled_Venril_Sathir.pl
+++ quests/karnor/#The_Fabled_Venril_Sathir.pl
@@ -1,26 +1,22 @@
 # #The_Fabled_Venril_Sathir (102133)
 
 sub EVENT_SPAWN{
-	$x = $npc->GetX();
-	$y = $npc->GetY();
+	my $x = $npc->GetX();
+	my $y = $npc->GetY();
 	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
 }
 
-sub EVENT_ENTER{
-	if($ulevel <= 55){
+sub EVENT_ENTER {
+    if($ulevel <= 59) {
 		quest::shout("We shall meet again $name! When you are a worthy opponent!!");
-
 		quest::depop();
 	}
 }
 
-sub EVENT_AGGRO{
-	if($ulevel <= 55){
+sub EVENT_AGGRO {
+	if($ulevel <= 59){
 		quest::shout("We shall meet again $name! When you are a worthy opponent!!");
-
 		quest::depop();
 	}
 }
-sub EVENT_DEATH {
 
-}
