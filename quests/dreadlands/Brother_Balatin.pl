496cd24b371367ebcb7fb23e3883b2ad3a29524b
Congdar <congdar@hotmail.com>
2021-01-28 10:10:56 -0800

    Brother Balatin faction fixes

--- quests/dreadlands/Brother_Balatin.pl
+++ quests/dreadlands/Brother_Balatin.pl
@@ -1,34 +1,37 @@
+# Brother_Balatin (86022, 86113, 86114, 86136)
+
 my $drape = quest::saylink("drape", 1);
 sub EVENT_SAY {
- if($text=~/hail/i){
-  # ROTWF [Part 1]
-  quest::me("Brother Balatin turns to you. A transparent human face materalizes upon the skull. As he speaks the smell of rotted rat corpses rushes into your face. You must fight the urge to vomit.");
-  quest::say("What discipline do you follow?");
- }
- elsif($text=~/circle/i){
-  # ROTWF [Part 2]
-  quest::say("Then we are one. The rebirth of our circle shall start with you. I have waited decades for your arrival. You should seek the living brothers within Tunaria. When you have earned your garb then you will be ready for the true $drape.");
- }
- elsif($text=~/drape/i){
-  # ROTWF [Part 3]
-  quest::say("Seek the broken flute of our master. Taken from us by the Iksar twins of Vistrei. To me they will come and to me your robe and you will come the dawning of the new brotherhood.");
- }
+    if($text=~/hail/i) {
+        # ROTWF [Part 1]
+        quest::me("Brother Balatin turns to you. A transparent human face materalizes upon the skull. As he speaks the smell of rotted rat corpses rushes into your face. You must fight the urge to vomit.");
+        quest::say("What discipline do you follow?");
+    }
+    elsif($text=~/circle/i) {
+        # ROTWF [Part 2]
+        quest::say("Then we are one. The rebirth of our circle shall start with you. I have waited decades for your arrival. You should seek the living brothers within Tunaria. When you have earned your garb then you will be ready for the true $drape.");
+    }
+    elsif($text=~/drape/i) {
+        # ROTWF [Part 3]
+        quest::say("Seek the broken flute of our master. Taken from us by the Iksar twins of Vistrei. To me they will come and to me your robe and you will come the dawning of the new brotherhood.");
+    }
 }
 
 sub EVENT_ITEM {
- #Handin: A Metal Pipe (12979) & A Metal Pipe (12980) & Robe of the Lost Circle (12256)
- if(plugin::check_handin(\%itemcount, 12979 => 1, 12980 => 1, 12256 => 1)){
-  # ROTWF [Part 4][End]
-  quest::emote("holds his arms out and a fine robe materializes before your eyes. You grab it just in time to see the skeleton fade from existence.");
-  #Summon: Robe of the Whistling Fist (12970)
-  quest::summonitem(12970);
-  quest::faction("359","1");
-  quest::faction("50","1");
-  quest::faction("317","-1");
- } else {
-   quest::say("I have no need for this item $name, you can have it back.");
-   plugin::return_items(\%itemcount); 
- } 
+    #Handin: A Metal Pipe (12979) & A Metal Pipe (12980) & Robe of the Lost Circle (12256)
+    if(plugin::check_handin(\%itemcount, 12979 => 1, 12980 => 1, 12256 => 1)) {
+        # ROTWF [Part 4][End]
+        quest::emote("holds his arms out and a fine robe materializes before your eyes. You grab it just in time to see the skeleton fade from existence.");
+        #Summon: Robe of the Whistling Fist (12970)
+        quest::summonitem(12970);
+        quest::faction("359","200");
+        quest::faction("50","50");
+        quest::faction("317","-50");
+    }
+    else {
+        quest::say("I have no need for this item $name, you can have it back.");
+        plugin::return_items(\%itemcount); 
+    } 
 }
 
 
--- quests/karnor/#The_Fabled_Drolvarg_Warlord.pl
+++ quests/karnor/#The_Fabled_Drolvarg_Warlord.pl
@@ -3,28 +3,38 @@
 # by patrikpatrik 07/21/16
 # Update 8/30/17 Timer stops if engaged and restarts on exit.
 
-sub EVENT_SPAWN{
-	$x = $npc->GetX();
-	$y = $npc->GetY();
-	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
-	
-	$tempk = $npc->GetNPCTypeID();
-	
-	if ($tempk == 102128) {
-		quest::settimer("fwarlord", 600); #will remain for 10 minutes
-	}
+sub EVENT_SPAWN {
+    $x = $npc->GetX();
+    $y = $npc->GetY();
+    quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+
+    $tempk = $npc->GetNPCTypeID();
+
+    if($tempk == 102128) {
+        #will remain for 10 minutes
+        quest::settimer("fwarlord", 600);
+    }
 }
 
 sub EVENT_COMBAT {
-	if($npc->IsEngaged()) {
-		quest::stoptimer($timer);
-	} else {
-		quest::settimer("fwarlord", 600);
-	}
+    if($npc->IsEngaged()) {
+        quest::stoptimer("fwarlord");
+    }
+    else {
+        quest::settimer("fwarlord", 600);
+    }
+}
+
+sub EVENT_AGGRO {
+    if($ulevel < 60) {
+        quest::shout("We shall meet again $name! When you are a worthy opponent!!");
+        quest::stoptimer("fwarlord");
+        quest::depop();
+    }
 }
 
 sub EVENT_TIMER {
-	if($timer eq "fwarlord") {
-		quest::depop();
-	}
+    if($timer eq "fwarlord") {
+        quest::depop();
+    }
 }
