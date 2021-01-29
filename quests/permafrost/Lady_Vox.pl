4342ba8659cda694bafc0f8582cab1ed36184099
Congdar <congdar@hotmail.com>
2021-01-29 10:58:58 -0800

    Fix Nagafen and Vox banish code.

--- quests/permafrost/Lady_Vox.pl
+++ quests/permafrost/Lady_Vox.pl
@@ -1,11 +1,33 @@
-# Attempt to limit players level 53 and over from engaging nagafen and vox.
-# Player will be banished to just outside the permafrost zone in everfrost.
+# Attempt to limit players level 53 and over from engaging Lord Nagafen and Lady Vox.
 
+# This is for bots and pets and players that do physical attack damage
 sub EVENT_ATTACK {
-   if($ulevel >= 53) {
-      quest::shout("I will not fight you, but I will banish you!");
-      quest::movepc(30,-7021.9,2020.1,-59.8,190.1);
-   }
+    # Dragon attacker
+    $DragonsTarget = $entity_list->GetMobID($userid);
+
+    # Check if the attacker is a Pet and get the owner if true
+    $DragonsTargetPetOwner = $entity_list->GetMobID($DragonsTarget->GetOwnerID());
+    if($DragonsTargetPetOwner) {
+        $DragonsTarget = $DragonsTargetPetOwner;
+    }
+
+    if($DragonsTarget && ($DragonsTarget->GetLevel() > 52)) {
+        quest::shout("I will not fight you, but I will banish you!");
+        if($DragonsTarget->IsBot()) {
+            $BotsClient = $DragonsTarget->GetBotOwner();
+            $BotsClient->MovePC(30, -7021.9, 2020.1, -59.8, 190.1);
+        }
+        elsif($DragonsTarget->IsClient()) {
+            quest::movepc(30, -7021.9, 2020.1, -59.8, 190.1);
+        }
+    }
+}
+
+# A solo player of level 53+ will not get banished if they just stand there
+# and don't attack, so we need to have EVENT_AGGRO for that condition
+sub EVENT_AGGRO {
+    quest::shout("I will not fight you, but I will banish you!");
+    quest::movepc(30, -7021.9, 2020.1, -59.8, 190.1);
 }
 
 # EOF zone: permafrost ID: 73057 NPC: Lady_Vox
--- quests/soldungb/#Lord_Nagafen.pl
+++ quests/soldungb/#Lord_Nagafen.pl
@@ -1,10 +1,33 @@
-# Attempt to limit players level 53 and over from engaging nagafen and vox.
+# Attempt to limit players level 53 and over from engaging Lord Nagafen and Lady Vox.
 
+# This is for bots and pets and players that do physical attack damage
 sub EVENT_ATTACK {
-   if($ulevel >= 53) {
-      quest::shout("I will not fight you, but I will banish you!");
-      quest::movepc(27,1400.5,983.0,128.6, 197.3);
-   }
+    # Dragon attacker
+    $DragonsTarget = $entity_list->GetMobID($userid);
+
+    # Check if the attacker is a Pet and get the owner if true
+    $DragonsTargetPetOwner = $entity_list->GetMobID($DragonsTarget->GetOwnerID());
+    if($DragonsTargetPetOwner) {
+        $DragonsTarget = $DragonsTargetPetOwner;
+    }
+
+    if($DragonsTarget && ($DragonsTarget->GetLevel() > 52)) {
+        quest::shout("I will not fight you, but I will banish you!");
+        if($DragonsTarget->IsBot()) {
+            $BotsClient = $DragonsTarget->GetBotOwner();
+            $BotsClient->MovePC(27, 535.3, 955.9, 57.4, 228.8);
+        }
+        elsif($DragonsTarget->IsClient()) {
+            quest::movepc(27, 535.3, 955.9, 57.4, 228.8);
+        }
+    }
+}
+
+# A solo player of level 53+ will not get banished if they just stand there
+# and don't attack, so we need to have EVENT_AGGRO for that condition
+sub EVENT_AGGRO {
+    quest::shout("I will not fight you, but I will banish you!");
+    quest::movepc(27, 535.3, 955.9, 57.4, 228.8);
 }
 
 # EOF zone: soldungb ID: 32040 NPC: #Lord_Nagafen
