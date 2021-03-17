0ff702b357762155d2533c49e923fa446409b4f5
Angelox <axclassic@users.noreply.github.com>
2021-03-17 19:57:40 -0400

    A batch file for copy Adiles Thrall scripts to all starter zones - you should only update his script in templates!

---
+++ quests/templates/CopyAdiles.bat
@@ -0,0 +1,35 @@
+REM This is a Windows version for copy Aediles_Thrall.pl
+REM to all the starter zones he spawns in.
+REM Changes made to the Aediles_Thrall.pl should be done 
+REM in templates folder then copied over with this 
+REM batch file 
+copy/y Aediles_Thrall.pl ..\akanon
+copy/y Aediles_Thrall.pl ..\bazaar
+copy/y Aediles_Thrall.pl ..\cabeast
+copy/y Aediles_Thrall.pl ..\cabwest
+copy/y Aediles_Thrall.pl ..\erudnint
+copy/y Aediles_Thrall.pl ..\felwithea
+copy/y Aediles_Thrall.pl ..\felwitheb
+copy/y Aediles_Thrall.pl ..\freporte
+copy/y Aediles_Thrall.pl ..\freportw
+copy/y Aediles_Thrall.pl ..\gfaydark
+copy/y Aediles_Thrall.pl ..\grobb
+copy/y Aediles_Thrall.pl ..\gukta
+copy/y Aediles_Thrall.pl ..\halas
+copy/y Aediles_Thrall.pl ..\kaladima
+copy/y Aediles_Thrall.pl ..\kaladimb
+copy/y Aediles_Thrall.pl ..\kerraridge
+copy/y Aediles_Thrall.pl ..\neriaka
+copy/y Aediles_Thrall.pl ..\neriakc
+copy/y Aediles_Thrall.pl ..\oggok
+copy/y Aediles_Thrall.pl ..\paineel
+copy/y Aediles_Thrall.pl ..\poknowledge
+copy/y Aediles_Thrall.pl ..\qeynos
+copy/y Aediles_Thrall.pl ..\qeynos2
+copy/y Aediles_Thrall.pl ..\qrg
+copy/y Aediles_Thrall.pl ..\rathemtn
+copy/y Aediles_Thrall.pl ..\rivervale
+copy/y Aediles_Thrall.pl ..\shadeweaver
+copy/y Aediles_Thrall.pl ..\sharvahl
+copy/y Aediles_Thrall.pl ..\swampofnohope
+copy/y Aediles_Thrall.pl ..\tutorialb
---
+++ quests/templates/DeleteAdiles.bat
@@ -0,0 +1,34 @@
+REM This is a Windows version for deleting Aediles Thrall script
+REM in all the starter zones he spawns in.
+REM But NOT in templates.
+REM Only to be used if we ever figure out the  Aediles Thrall bug
+del/q ..\akanon\Aediles_Thrall.pl
+del/q ..\bazaar\Aediles_Thrall.pl
+del/q ..\cabeast\Aediles_Thrall.pl
+del/q ..\cabwest\Aediles_Thrall.pl
+del/q ..\erudnint\Aediles_Thrall.pl
+del/q ..\felwithea\Aediles_Thrall.pl
+del/q ..\felwitheb\Aediles_Thrall.pl
+del/q ..\freporte\Aediles_Thrall.pl
+del/q ..\freportw\Aediles_Thrall.pl
+del/q ..\gfaydark\Aediles_Thrall.pl
+del/q ..\grobb\Aediles_Thrall.pl
+del/q ..\gukta\Aediles_Thrall.pl
+del/q ..\halas\Aediles_Thrall.pl
+del/q ..\kaladima\Aediles_Thrall.pl
+del/q ..\kaladimb\Aediles_Thrall.pl
+del/q ..\kerraridge\Aediles_Thrall.pl
+del/q ..\neriaka\Aediles_Thrall.pl
+del/q ..\neriakc\Aediles_Thrall.pl
+del/q ..\oggok\Aediles_Thrall.pl
+del/q ..\paineel\Aediles_Thrall.pl
+del/q ..\poknowledge\Aediles_Thrall.pl
+del/q ..\qeynos\Aediles_Thrall.pl
+del/q ..\qeynos2\Aediles_Thrall.pl
+del/q ..\qrg\Aediles_Thrall.pl
+del/q ..\rathemtn\Aediles_Thrall.pl
+del/q ..\rivervale\Aediles_Thrall.pl
+del/q ..\shadeweaver\Aediles_Thrall.pl
+del/q ..\sharvahl\Aediles_Thrall.pl
+del/q ..\swampofnohope\Aediles_Thrall.pl
+del/q ..\tutorialb\Aediles_Thrall.pl
