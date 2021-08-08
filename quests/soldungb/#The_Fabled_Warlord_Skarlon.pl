9c38e7e58d042618f576bf8b44505f305a738c30
Congdar <congdar@hotmail.com>
2021-08-07 18:42:46 -0700

    SolB Fabled fixes

--- quests/soldungb/#The_Fabled_Warlord_Skarlon.pl
+++ quests/soldungb/#The_Fabled_Warlord_Skarlon.pl
@@ -1,5 +1,22 @@
 # Fabled depop
 
+# This is for bots and pets and players that do physical attack damage
+sub EVENT_ATTACK {
+    # Dragon attacker
+    $DragonsTarget = $entity_list->GetMobID($userid);
+
+    # Check if the attacker is a Pet and get the owner if true
+    $DragonsTargetPetOwner = $entity_list->GetMobID($DragonsTarget->GetOwnerID());
+    if($DragonsTargetPetOwner) {
+        $DragonsTarget = $DragonsTargetPetOwner;
+    }
+
+    if($DragonsTarget && ($DragonsTarget->GetLevel() < 52)) {
+        quest::shout("We shall meet again $name! When you are a worthy opponent!!");
+        quest::depop();
+    }
+}
+
 sub EVENT_AGGRO {
    if($ulevel < 52) {
       quest::shout("We shall meet again $name! When you are a worthy opponent!!");
