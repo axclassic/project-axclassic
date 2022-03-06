4bc79135d5eeec1db32a1aabe23e5d4fd015cf78
Angelox <angelox22@gmail.com>
2022-03-06 18:12:13 -0500

    Tweaks to Translocators

--- quests/freporte/#Translocator_Setikan.pl
+++ quests/freporte/#Translocator_Setikan.pl
@@ -1,5 +1,14 @@
-sub EVENT_SAY { 
-if ($text=~/Hail/i){quest::say("Hello there. There seems to be some strange problems with the boats in this area. The Academy of Arcane Sciences has sent a small team of us to investigate them. If you need to [travel to OOT] in the meantime, I can transport you to my companion there."); }
-if ($text=~/travel to oot/i){
-quest::selfcast("2279"); } 
-} 
+##Translocator_Setikan.pl
+#Zone: Freeport
+
+sub EVENT_SAY{
+  my $tears = quest::saylink("travel to Ocean of Tears", 1);
+  if ($text=~/Hail/i){
+  $client->Message(14,"Hello there, $name. There seem to be some strange problems with the boats in this area. The Academy of Arcane Sciences has sent a small team of us to investigate them. If you need to $tears in the meantime, I can transport you to my companion there");
+  }
+  if($text=~/travel to Ocean of Tears/i)
+  {
+  $client->Message(14,"Off you go!");   
+  quest::selfcast(2279);
+  }
+}
--- quests/oot/#Translocator_Narrik.pl
+++ quests/oot/#Translocator_Narrik.pl
@@ -1,5 +1,10 @@
-sub EVENT_SAY { 
-if ($text=~/Hail/i){quest::say("Hello there. There seems to be some strange problems with the boats in this area. The Academy of Arcane Sciences has sent a small team of us to investigate them. If you need to [travel to Freeport] or [travel to Butcherblock] in the meantime, I can transport you to my companion there."); }
+##Translocator_Narrik.pl
+#Zone: OoT
+
+sub EVENT_SAY {
+my $freeport = quest::saylink("travel to freeport", 1);
+my $butcher = quest::saylink("travel to butcherblock", 1);
+if ($text=~/Hail/i){quest::say("Hello there. There seems to be some strange problems with the boats in this area. The Academy of Arcane Sciences has sent a small team of us to investigate them. If you need to $freeport or $butcher in the meantime, I can transport you to my companion there."); }
 if ($text=~/travel to freeport/i){quest::selfcast("2283"); }
 if ($text=~/travel to butcherblock/i){quest::selfcast("2281"); } 
 } 
