a9be053b36da0c5ab169182ac1c72395feac4ce8
Congdar <congdar@hotmail.com>
2022-06-12 18:18:56 -0700

    Fix Ry`Gorr Avenger spawn

---
+++ quests/eastwastes/116165.pl
@@ -0,0 +1,11 @@
+#Chief Ry'Gorr non-ring
+
+sub EVENT_DEATH {
+    quest::spawn2(116557,0,0,$npc->GetX(),$npc->GetY(),$npc->GetZ(),0);
+    quest::spawn2(116557,0,0,$npc->GetX(),$npc->GetY(),$npc->GetZ(),0);
+    quest::spawn2(116557,0,0,$npc->GetX(),$npc->GetY(),$npc->GetZ(),0);
+    quest::spawn2(116557,0,0,$npc->GetX(),$npc->GetY(),$npc->GetZ(),0);
+    quest::spawn2(116557,0,0,$npc->GetX(),$npc->GetY(),$npc->GetZ(),0);
+    quest::spawn2(116557,0,0,$npc->GetX(),$npc->GetY(),$npc->GetZ(),0);
+}
+
