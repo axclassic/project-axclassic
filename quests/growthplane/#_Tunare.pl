852975e20a3ffbb0716122afe8dfaf6e2351af0d
Congdar <congdar@hotmail.com>
2022-07-22 19:13:56 -0700

    fix Tunare spawn

--- quests/growthplane/#_Tunare.pl
+++ quests/growthplane/#_Tunare.pl
@@ -1,12 +1,13 @@
-# respawns true Tunare on death
-# Angelox
+# #_Tunare (127001)
+
+#respawns true Tunare on death
+#Angelox
 
 sub EVENT_DEATH{
-if ($tunares==2){
-  quest::spawn2(127098,0,0,-227.5,1608.6,-53.7,173.0); #Spawn Real Tunare
-  quest::delglobal("tuna");
-  quest::setglobal("tuna",3,3,"F");
-  $tuna=undef;
+    quest::spawn2(127098,0,0,-227.5,1608.6,-53.7,173.0); #Spawn Real Tunare
+    quest::delglobal("tuna");
+    quest::setglobal("tuna",3,3,"F");
+    $tuna=undef;
 }
-}
-# EOF zone: GrowthPlane NPC: Fake Tunare
+#EOF zone: GrowthPlane NPC: Fake Tunare
+
--- quests/growthplane/Not_Found.pl
+++
@@ -1,6 +0,0 @@
-sub EVENT_SAY{
-if($text=~/Hail/i){
-quest::say("Greetings. my friend. it is not often we see strangers coming to this plane. I fear that our growing infestation problem has kept us busy of late. The time for battle grows near yet we still do not have the offensive magics necessary to deter our enemies."); }
-}
-#END of FILE Zone:growthplane  ID:Not_Found -- Ancient_Totem 
-
