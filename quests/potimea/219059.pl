dbb8c30eb30525ff34a018c1cc84758010664883
Congdar <congdar@hotmail.com>
2022-09-29 19:10:14 -0700

    more timea

--- quests/potimea/219059.pl
+++ quests/potimea/219059.pl
@@ -1,5 +1,16 @@
 # #The_Elemental_Construct (219059)
 
+my @now_raid;
+
+sub EVENT_COMBAT {
+    if($npc->IsEngaged()) {
+        @now_raid = $npc->GetHateList();
+	}
+    else {
+        @now_raid = undef;
+	}
+}
+
 sub EVENT_DEATH {
     my $Undead = $entity_list->GetMobByNpcTypeID(219060);
     my $Animated = $entity_list->GetMobByNpcTypeID(219061);
@@ -8,9 +19,15 @@ sub EVENT_DEATH {
     if(!$Undead && !$Animated && !$Golem && !$Guardian) {
         quest::ze(14, "A booming echo bellows from across the sky, 'Congratulations! You feel an overwhelming urge to pass through the portals. Make haste before it's too late...'");
     }
-    my $QuestGlobalValue = $client->GetQGlobal($name.".potimea_portal_event_timer");
-    if(!$QuestGlobalValue) {
-        quest::targlobal($name.".potimea_portal_event_timer", "1", "H1", 0, $client->CharacterID(), 0);
+
+    foreach(@now_raid) {
+        if($_->GetEnt()->IsClient()) {
+            my $QuestGlobalValue = $_->GetEnt()->GetQGlobal($name.".potimea_portal_event_timer");
+            if(!$QuestGlobalValue) {
+                quest::targlobal($name.".potimea_portal_event_timer", "1", "H1", 0, $client->CharacterID(), 0);
+                $QuestGlobalValue = undef;
+            }
+        }
     }
 }
 
--- quests/potimea/219061.pl
+++ quests/potimea/219061.pl
@@ -1,5 +1,16 @@
 # #The_Animated_Construct (219061)
 
+my @now_raid;
+
+sub EVENT_COMBAT {
+    if($npc->IsEngaged()) {
+        @now_raid = $npc->GetHateList();
+	}
+    else {
+        @now_raid = undef;
+	}
+}
+
 sub EVENT_DEATH {
     my $Elemental = $entity_list->GetMobByNpcTypeID(219059);
     my $Undead = $entity_list->GetMobByNpcTypeID(219060);
@@ -8,9 +19,15 @@ sub EVENT_DEATH {
     if(!$Elemenal && !$Undead && !$Golem && !$Guardian) {
         quest::ze(14, "A booming echo bellows from across the sky, 'Congratulations! You feel an overwhelming urge to pass through the portals. Make haste before it's too late...'");
     }
-    my $QuestGlobalValue = $client->GetQGlobal($name.".potimea_portal_event_timer");
-    if(!$QuestGlobalValue) {
-        quest::targlobal($name.".potimea_portal_event_timer", "1", "H1", 0, $client->CharacterID(), 0);
+
+    foreach(@now_raid) {
+        if($_->GetEnt()->IsClient()) {
+            my $QuestGlobalValue = $_->GetEnt()->GetQGlobal($name.".potimea_portal_event_timer");
+            if(!$QuestGlobalValue) {
+                quest::targlobal($name.".potimea_portal_event_timer", "1", "H1", 0, $client->CharacterID(), 0);
+                $QuestGlobalValue = undef;
+            }
+        }
     }
 }
 
